a9e9e23dc410fa92efdeabe2c5532b4f67604a0e
Daniel Robbins <drobbins@gentoo.org>
2003-10-25 04:45:09 +0000

    more weirdness
    git-svn-id: svn+ssh://svn.gentoo.org/var/svnroot/catalyst/trunk@20 d1e1f19c-881f-0410-ab34-b69fee027534

--- arch/x86.py
+++ arch/x86.py
@@ -1,31 +1,33 @@
-class generic_target:
-	def __init__(self):
-		self.settings={}
+import builder
 
-class base_x86(generic_target):
+# This module defines the various "builder" classes for the various x86
+# sub-arches. For example, we have a class to handle building of Pentium 4
+# sub-arches, one for i686, etc. We also have a function called register
+# that's called from the main catalyst program, which the main catalyst
+# program uses to become informed of the various sub-arches supported by
+# this module, as well as which classes should be used to build each 
+# particular sub-architecture.
+
+class generic_x86(builder.generic):
+	"abstract base class for all x86 builders"
 	def __init__(self):
 		self.settings["mainarch"]="x86"
 
-class arch_x86(base_x86):
+class arch_x86(generic_x86):
+	"builder class for generic x86 (486+)"
 	def __init__(self):
 		base_x86.__init__(self)
 		self.settings["CFLAGS"]="-O2 -mcpu=i686 -fomit-frame-pointer"
 
-class arch_pentium4(base_x86):
+class arch_pentium4(generic_x86):
+	"builder class for Pentium 4"
 	def __init__(self):
 		base_x86.__init__(self)
 		self.settings["CFLAGS"]="-O2 -mcpu=i686 -fomit-frame-pointer"
 		self.settings["CHOST"]="i686-pc-linux-gnu"
 		self.settings["HOSTUSE"]=["mmx","sse"]
 
-class x86_factory:
-	def __init__(self):
-		self.subarchdict={"x86":arch_x86,"pentium4":arch_pentium4}
-	def get_target(self,target):
-		if self.subarchdict.has_key(target):
-			return self.subarchdict[target]()
-	def is_target(self,target):
-		return target in self.subarchdict.keys()
-
-
+def register(foo):
+	"Inform main catalyst program of the contents of this plugin."
+	foo.update({"pentium4":arch_pentium4,"x86":arch_x86})
 		
