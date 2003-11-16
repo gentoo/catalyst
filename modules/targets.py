8b22743771be3874a2ef04e0b0da3b0866f434e5
Daniel Robbins <drobbins@gentoo.org>
2003-11-16 06:19:38 +0000

    oops
    git-svn-id: svn+ssh://svn.gentoo.org/var/svnroot/catalyst/trunk@87 d1e1f19c-881f-0410-ab34-b69fee027534

--- modules/targets.py
+++ modules/targets.py
@@ -115,7 +115,7 @@ class generic_stage_target(generic_target):
 		self.mount_safety_check()
 		cmd("rm -rf "+self.settings["chroot_path"],"Could not remove existing directory: "+self.settings["chroot_path"])
 		os.makedirs(self.settings["chroot_path"])
-		if self.settings.has_key["PKGCACHE"]:	
+		if self.settings.has_key("PKGCACHE"):	
 			if not os.path.exists(self.settings["pkgcache_path"]):
 				os.makedirs(self.settings["pkgcache_path"])
 		
