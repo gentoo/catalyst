77dfcfd4f8d24e8048ce8c9e545f56082f4d4df8
Jason Wever <weeve@gentoo.org>
2003-11-29 20:01:08 +0000

    Added arch plugins for sparc and sparc64.
    git-svn-id: svn+ssh://svn.gentoo.org/var/svnroot/catalyst/trunk@92 d1e1f19c-881f-0410-ab34-b69fee027534

---
+++ arch/sparc.py
@@ -0,0 +1,23 @@
+# Distributed under the GNU General Public License version 2
+# Copyright 2003-2004 Gentoo Technologies, Inc.
+
+import builder,os
+from catalyst_support import *
+
+class generic_sparc(builder.generic):
+	"abstract base class for all sparc builders"
+	def __init__(self,myspec):
+		builder.generic.__init__(self,myspec)
+		self.settings["mainarch"]="sparc"
+		self.settings["CHROOT"]="chroot"
+
+class arch_sparc(generic_sparc):
+	"builder class for generic sparc (sun4cdm)"
+	def __init__(self,myspec):
+		generic_sparc.__init__(self,myspec)
+		self.settings["CFLAGS"]=" -O2"
+		self.settings["CHOST"]="sparc-unknown-linux-gnu"
+
+def register(foo):
+	"Inform main catalyst program of the contents of this plugin."
+	foo.update({"sparc":arch_sparc})
---
+++ arch/sparc64.py
@@ -0,0 +1,23 @@
+# Distributed under the GNU General Public License version 2
+# Copyright 2003-2004 Gentoo Technologies, Inc.
+
+import builder,os
+from catalyst_support import *
+
+class generic_sparc64(builder.generic):
+	"abstract base class for all sparc64 builders"
+	def __init__(self,myspec):
+		builder.generic.__init__(self,myspec)
+		self.settings["mainarch"]="sparc64"
+		self.settings["CHROOT"]="chroot"
+
+class arch_sparc64(generic_sparc64):
+	"builder class for generic sparc64 (sun4u)"
+	def __init__(self,myspec):
+		generic_sparc64.__init__(self,myspec)
+		self.settings["CFLAGS"]=" -O3 -mcpu=ultrasparc"
+		self.settings["CHOST"]="sparc-unknown-linux-gnu"
+
+def register(foo):
+	"Inform main catalyst program of the contents of this plugin."
+	foo.update({"sparc64":arch_sparc64})
