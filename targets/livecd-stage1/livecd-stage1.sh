e0e965614bc6ea606bff90006a2b51ff84f12003
Daniel Robbins <drobbins@gentoo.org>
2004-01-12 06:35:05 +0000

    expansion fixor?
    git-svn-id: svn+ssh://svn.gentoo.org/var/svnroot/catalyst/trunk@142 d1e1f19c-881f-0410-ab34-b69fee027534

--- targets/livecd-stage1/livecd-stage1.sh
+++ targets/livecd-stage1/livecd-stage1.sh
@@ -1,6 +1,6 @@
 # Copyright 1999-2003 Gentoo Technologies, Inc.
 # Distributed under the terms of the GNU General Public License v2
-# $Header: /var/cvsroot/gentoo/src/catalyst/targets/livecd-stage1/Attic/livecd-stage1.sh,v 1.3 2004/01/12 06:20:33 drobbins Exp $
+# $Header: /var/cvsroot/gentoo/src/catalyst/targets/livecd-stage1/Attic/livecd-stage1.sh,v 1.4 2004/01/12 06:35:05 drobbins Exp $
 
 case $1 in
 enter)
@@ -20,7 +20,7 @@ run)
 	USE="build" emerge portage
 	#turn off auto-use:
 	export USE_ORDER="env:conf:defaults"	
-	for x in $*
+	for x in "$@" 
 	do
 		if [ -n "${clst_PKGCACHE}" ]
 		then
