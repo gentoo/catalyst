64b81b9f5a5905ce34faed77fe4a3297938bc53f
Pieter van den Abeele <pvdabeel@gentoo.org>
2004-03-11 01:56:06 +0000

    Added power target, cleanup CFLAGS
    git-svn-id: svn+ssh://svn.gentoo.org/var/svnroot/catalyst/trunk@311 d1e1f19c-881f-0410-ab34-b69fee027534

--- arch/ppc.py
+++ arch/ppc.py
@@ -10,7 +10,7 @@ class generic_ppc(builder.generic):
 		self.settings["mainarch"]="ppc"
 		self.settings["CHROOT"]="chroot"
 		self.settings["CHOST"]="powerpc-unknown-linux-gnu"
-		
+
 
 class arch_power_ppc(generic_ppc):
 	"builder class for generic powerpc/power"
@@ -23,28 +23,32 @@ class arch_ppc(generic_ppc):
 	def __init__(self,myspec):
 		generic_ppc.__init__(self,myspec)
 		self.settings["CFLAGS"]="-O3 -mcpu=powerpc"
-	
+
+class arch_power(generic_ppc):
+	"builder class for generic power"
+	def __init__(self,myspec):
+		generic_ppc.__init__(self,myspec)
+		self.settings["CFLAGS"]="-O3 -mcpu=power"
+
 class arch_g3(generic_ppc):
 	def __init__(self,myspec):
 		generic_ppc.__init__(self,myspec)
-		self.settings["CFLAGS"]="-O3 -mcpu=750"
+		self.settings["CFLAGS"]="-O3 -mcpu=G3"
 
 class arch_g4(generic_ppc):
 	def __init__(self,myspec):
 		generic_ppc.__init__(self,myspec)
-		self.settings["CFLAGS"]="-O2 -mcpu=7400 -maltivec -mabi=altivec"
+		self.settings["CFLAGS"]="-O3 -mcpu=G4 -maltivec -mabi=altivec"
 		self.settings["HOSTUSE"]=["altivec"]
 
 class arch_g5(generic_ppc):
-	"builder class for ppc970 32bit mode"
 	def __init__(self,myspec):
 		generic_ppc.__init__(self,myspec)
-		self.settings["CFLAGS"]="-O2 -mcpu=970 -maltivec -mabi=altivec"
+		self.settings["CFLAGS"]="-O3 -mcpu=G5 -maltivec -mabi=altivec"
 		self.settings["HOSTUSE"]=["altivec"]
-
-
+		
 def register(foo):
 	"Inform main catalyst program of the contents of this plugin."
 	#power/ppc can't be used as a subarch name as it has a "/" in it and is used in filenames
-	foo.update({"ppc":arch_ppc,"power-ppc":arch_power_ppc,"g3":arch_g3,"g4":arch_g4,"g5":arch_g5})
+	foo.update({"ppc":arch_ppc,"power":arch_power,"power-ppc":arch_power_ppc,"g3":arch_g3,"g4":arch_g4,"g5":arch_g5})
 
