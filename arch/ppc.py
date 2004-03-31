b010c1bdd4719c6ef21a2a7780b28dbc57a39a0b
Pieter van den Abeele <pvdabeel@gentoo.org>
2004-03-31 12:49:58 +0000

    Introduces -fno-strict-aliasing (temporarily), -mcpu=G3,G4,G5 usage, experimental nptl builds. Requires gcc-3.3.3 hammer snapshot.
    git-svn-id: svn+ssh://svn.gentoo.org/var/svnroot/catalyst/trunk@328 d1e1f19c-881f-0410-ab34-b69fee027534

--- arch/ppc.py
+++ arch/ppc.py
@@ -11,44 +11,48 @@ class generic_ppc(builder.generic):
 		self.settings["CHROOT"]="chroot"
 		self.settings["CHOST"]="powerpc-unknown-linux-gnu"
 
-
 class arch_power_ppc(generic_ppc):
 	"builder class for generic powerpc/power"
 	def __init__(self,myspec):
 		generic_ppc.__init__(self,myspec)
-		self.settings["CFLAGS"]="-O3 -mcpu=common"
+		self.settings["CFLAGS"]="-O3 -mcpu=common -fno-strict-aliasing"
 
 class arch_ppc(generic_ppc):
 	"builder class for generic powerpc"
 	def __init__(self,myspec):
 		generic_ppc.__init__(self,myspec)
-		self.settings["CFLAGS"]="-O3 -mcpu=powerpc"
+		self.settings["CFLAGS"]="-O3 -mcpu=powerpc -fno-strict-aliasing"
 
 class arch_power(generic_ppc):
 	"builder class for generic power"
 	def __init__(self,myspec):
 		generic_ppc.__init__(self,myspec)
-		self.settings["CFLAGS"]="-O3 -mcpu=power"
+		self.settings["CFLAGS"]="-O3 -mcpu=power -fno-strict-aliasing"
 
 class arch_g3(generic_ppc):
 	def __init__(self,myspec):
 		generic_ppc.__init__(self,myspec)
-		self.settings["CFLAGS"]="-O3 -mcpu=G3"
+		self.settings["CFLAGS"]="-O2 -mcpu=G3 -fno-strict-aliasing"
 
 class arch_g4(generic_ppc):
 	def __init__(self,myspec):
 		generic_ppc.__init__(self,myspec)
-		self.settings["CFLAGS"]="-O3 -mcpu=G4 -maltivec -mabi=altivec"
+		self.settings["CFLAGS"]="-O2 -mcpu=G4 -maltivec -mabi=altivec -fno-strict-aliasing"
 		self.settings["HOSTUSE"]=["altivec"]
 
 class arch_g5(generic_ppc):
 	def __init__(self,myspec):
 		generic_ppc.__init__(self,myspec)
-		self.settings["CFLAGS"]="-O3 -mcpu=G5 -maltivec -mabi=altivec"
+		self.settings["CFLAGS"]="-O2 -mcpu=G5 -maltivec -mabi=altivec -fno-strict-aliasing"
 		self.settings["HOSTUSE"]=["altivec"]
-		
+
+class arch_experimental(generic_ppc):
+	def __init__(self,myspec):
+		generic_ppc.__init__(self,myspec)
+		self.settings["CFLAGS"]="-O3 -mcpu=7450 -maltivec -mabi=altivec -fno-strict-aliasing"
+		self.settings["HOSTUSE"]=["altivec nptl"]
+	
 def register(foo):
 	"Inform main catalyst program of the contents of this plugin."
-	#power/ppc can't be used as a subarch name as it has a "/" in it and is used in filenames
-	foo.update({"ppc":arch_ppc,"power":arch_power,"power-ppc":arch_power_ppc,"g3":arch_g3,"g4":arch_g4,"g5":arch_g5})
+	foo.update({"experimental":arch_experimental,"ppc":arch_ppc,"power":arch_power,"power-ppc":arch_power_ppc,"g3":arch_g3,"g4":arch_g4,"g5":arch_g5})
 
