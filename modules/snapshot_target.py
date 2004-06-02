b81856d03df39fe7ae1d7bf1b681b26c2a65f1d4
John P. Davis <zhen@gentoo.org>
2004-06-02 06:35:29 +0000

    see changelog
    git-svn-id: svn+ssh://svn.gentoo.org/var/svnroot/catalyst/trunk@385 d1e1f19c-881f-0410-ab34-b69fee027534

--- modules/snapshot_target.py
+++ modules/snapshot_target.py
@@ -1,6 +1,6 @@
 # Distributed under the GNU General Public License version 2
 # Copyright 2003-2004 Gentoo Technologies, Inc.
-# $Header: /var/cvsroot/gentoo/src/catalyst/modules/snapshot_target.py,v 1.1 2004/05/17 01:56:19 zhen Exp $
+# $Header: /var/cvsroot/gentoo/src/catalyst/modules/snapshot_target.py,v 1.2 2004/06/02 06:35:29 zhen Exp $
 
 """
 Builder class for snapshots.
@@ -14,7 +14,7 @@ from stat import *
 class snapshot_target(generic_target):
 	def __init__(self,myspec,addlargs):
 		self.required_values=["version_stamp","target"]
-		self.valid_values=["version_stamp","target"]
+		self.valid_values=["version_stamp","target","portdir_overlay"]
 		generic_target.__init__(self,myspec,addlargs)
 		self.settings=myspec
 		self.settings["target_subpath"]="portage-"+self.settings["version_stamp"]
@@ -31,20 +31,21 @@ class snapshot_target(generic_target):
 		self.setup()
 		print "Creating Portage tree snapshot "+self.settings["version_stamp"]+" from "+self.settings["portdir"]+"..."
 		mytmp=self.settings["tmp_path"]
-		if os.path.exists(mytmp):
-			cmd("rm -rf "+mytmp,"Could not remove existing directory: "+mytmp)
-		os.makedirs(mytmp)
-		cmd("rsync -a --exclude /packages/ --exclude /distfiles/ --exclude CVS/ "+self.settings["portdir"]+"/ "+mytmp+"/portage/","Snapshot failure")
+		if not os.path.exists(mytmp):
+			os.makedirs(mytmp)
+		
+		cmd("rsync -a --delete --exclude /packages/ --exclude /distfiles/ --exclude CVS/ "+self.settings["portdir"]+"/ "+mytmp+"/portage/","Snapshot failure")
+		if self.settings.has_key("portdir_overlay"):
+			cmd("rsync -a --exclude /packages/ --exclude /distfiles/ --exclude CVS/ "+self.settings["portdir_overlay"]+"/ "+mytmp+"/portage/","Snapshot/ overlay addition failure")
+			
 		print "Compressing Portage snapshot tarball..."
 		cmd("tar cjf "+self.settings["snapshot_path"]+" -C "+mytmp+" portage","Snapshot creation failure")
 		self.cleanup()
+		print "snapshot: complete!"
 
 	def cleanup(self):
-		print "Cleaning up temporary snapshot directory..."
-		#Be a good citizen and clean up after ourselves
-		cmd("rm -rf "+self.settings["tmp_path"],"Snapshot cleanup failure")
+		print "Cleaning up..."
 			
 def register(foo):
 	foo.update({"snapshot":snapshot_target})
 	return foo
-	
