dfe81683ae398df0af7f4cd81c187fd0522fa4a2
John P. Davis <zhen@gentoo.org>
2004-10-19 04:09:13 +0000

    patch from vapier@gentoo.org for bug #68080
    git-svn-id: svn+ssh://svn.gentoo.org/var/svnroot/catalyst/trunk@471 d1e1f19c-881f-0410-ab34-b69fee027534

--- arch/ia64.py
+++ arch/ia64.py
@@ -1,6 +1,6 @@
 # Copyright 1999-2004 Gentoo Foundation
 # Distributed under the terms of the GNU General Public License v2
-# $Header: /var/cvsroot/gentoo/src/catalyst/arch/ia64.py,v 1.3 2004/10/15 02:36:00 zhen Exp $
+# $Header: /var/cvsroot/gentoo/src/catalyst/arch/ia64.py,v 1.4 2004/10/19 04:09:13 zhen Exp $
 
 import builder,os
 from catalyst_support import *
@@ -11,7 +11,8 @@ class arch_ia64(builder.generic):
 		builder.generic.__init__(self,myspec)
 		self.settings["mainarch"]="ia64"
 		self.settings["CHROOT"]="chroot"
-		self.settings["CFLAGS"]="-O2"
+		self.settings["CFLAGS"]="-O2 -pipe"
+		self.settings["CFLAGS"]="-O2 -pipe"
 		self.settings["CHOST"]="ia64-unknown-linux-gnu"
 
 def register(foo):
