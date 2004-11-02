9bf48a2cc4c2c0b0b5b4790b282e67b6bcdc51da
Chris Gianelloni <wolf31o2@gentoo.org>
2004-11-02 23:23:16 +0000

    Ssshhh... I've added my super-secret pre-kmerge.sh sed replacement so genkernel will only add the 1024x768 version of the gensplash image to the bzImage, which added with the livecd-stage2 removal of the unused splash images, makes for a significantly smaller (54MB v. 50MB) LiveCD.
    git-svn-id: svn+ssh://svn.gentoo.org/var/svnroot/catalyst/trunk@478 d1e1f19c-881f-0410-ab34-b69fee027534

--- livecd/runscript-support/pre-kmerge.sh
+++ livecd/runscript-support/pre-kmerge.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 # Copyright 1999-2004 Gentoo Foundation
 # Distributed under the terms of the GNU General Public License v2
-# $Header: /var/cvsroot/gentoo/src/catalyst/livecd/runscript-support/Attic/pre-kmerge.sh,v 1.6 2004/10/28 15:05:36 wolf31o2 Exp $
+# $Header: /var/cvsroot/gentoo/src/catalyst/livecd/runscript-support/Attic/pre-kmerge.sh,v 1.7 2004/11/02 23:23:16 wolf31o2 Exp $
 
 /usr/sbin/env-update
 source /etc/profile
@@ -15,4 +15,4 @@ install -d /usr/portage/packages/gk_binaries
 rm -f /usr/src/linux
 
 sed -i 's/uchi-hcd/uhci-hcd/' /usr/share/genkernel/x86/modules_load
-
+sed -i 's/initramfs_data.cpio.gz /initramfs_data.cpio.gz -r 1024x768 /' /usr/share/genkernel/genkernel
