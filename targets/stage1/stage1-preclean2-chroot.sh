e5fd83d0dc4166d62d9df1701dc049730f60736f
Chris Gianelloni <wolf31o2@gentoo.org>
2004-12-06 22:25:10 +0000

    Added patch from vapier and closing bug #73556.
    git-svn-id: svn+ssh://svn.gentoo.org/var/svnroot/catalyst/trunk@485 d1e1f19c-881f-0410-ab34-b69fee027534

--- targets/stage1/stage1-preclean2-chroot.sh
+++ targets/stage1/stage1-preclean2-chroot.sh
@@ -1,15 +1,14 @@
 #!/bin/bash
 # Copyright 1999-2004 Gentoo Foundation
 # Distributed under the terms of the GNU General Public License v2
-# $Header: /var/cvsroot/gentoo/src/catalyst/targets/stage1/Attic/stage1-preclean2-chroot.sh,v 1.4 2004/10/15 02:46:58 zhen Exp $
+# $Header: /var/cvsroot/gentoo/src/catalyst/targets/stage1/Attic/stage1-preclean2-chroot.sh,v 1.5 2004/12/06 22:25:10 wolf31o2 Exp $
 		
 #now, some finishing touches to initialize gcc-config....
 unset ROOT
 
 if [ -e /usr/bin/gcc-config ]
 then
-	mythang=$( cd /etc/env.d/gcc; ls ${clst_CHOST}-* )
-	echo $mythang; sleep 20
+	mythang=$( cd /etc/env.d/gcc; ls ${clst_CHOST}-* | head -n 1 )
 	gcc-config ${mythang}; /usr/sbin/env-update; source /etc/profile
 fi
 
