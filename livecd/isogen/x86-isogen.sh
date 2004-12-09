bdfcefa0594a00ba5c1c554f13965174364728b9
Chris Gianelloni <wolf31o2@gentoo.org>
2004-12-09 14:13:02 +0000

    Removed -no-emul-boot from x86-isogen.sh to keep the ISO being made from possibly not booting on really old systems.
    git-svn-id: svn+ssh://svn.gentoo.org/var/svnroot/catalyst/trunk@488 d1e1f19c-881f-0410-ab34-b69fee027534

--- livecd/isogen/x86-isogen.sh
+++ livecd/isogen/x86-isogen.sh
@@ -1,4 +1,2 @@
 #!/bin/bash
-mkisofs -J -R -l -o ../gentoo.iso -b isolinux/isolinux.bin -c isolinux/boot.cat -no-emul-boot -boot-load-size 4 -boot-info-table -z .
-
-
+mkisofs -J -R -l -o ../gentoo.iso -b isolinux/isolinux.bin -c isolinux/boot.cat -boot-load-size 4 -boot-info-table -z .
