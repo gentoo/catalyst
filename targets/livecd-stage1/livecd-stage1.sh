1ea9f026fa1f49abef5175f300c23e939c598ea2
Chris Gianelloni <wolf31o2@gentoo.org>
2004-12-16 15:26:22 +0000

    Added environmental.motd.txt for gentoo-release-environmental livecd/type.
    git-svn-id: svn+ssh://svn.gentoo.org/var/svnroot/catalyst/trunk@496 d1e1f19c-881f-0410-ab34-b69fee027534

--- targets/livecd-stage1/livecd-stage1.sh
+++ targets/livecd-stage1/livecd-stage1.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 # Copyright 1999-2004 Gentoo Foundation
 # Distributed under the terms of the GNU General Public License v2
-# $Header: /var/cvsroot/gentoo/src/catalyst/targets/livecd-stage1/Attic/livecd-stage1.sh,v 1.13 2004/10/15 02:46:58 zhen Exp $
+# $Header: /var/cvsroot/gentoo/src/catalyst/targets/livecd-stage1/Attic/livecd-stage1.sh,v 1.14 2004/12/16 15:26:22 wolf31o2 Exp $
 
 case $1 in
 	enter)
@@ -9,26 +9,27 @@ case $1 in
 	;;
 	run)
 		shift
-	
+
 		cp ${clst_sharedir}/targets/livecd-stage1/livecd-stage1-chroot.sh ${clst_chroot_path}/tmp
 		clst_packages="$*" ${clst_CHROOT} ${clst_chroot_path} /tmp/livecd-stage1-chroot.sh || exit 1
 		rm -f ${clst_chroot_path}/tmp/livecd-stage1-chroot.sh
 	;;
 
 	preclean)
-        #cp ${clst_sharedir}/targets/livecd-stage1/livecd-stage1-preclean-chroot.sh ${clst_chroot_path}/tmp
-        #${clst_CHROOT} ${clst_chroot_path} /tmp/livecd-stage1-preclean-chroot.sh || exit 1
+		killall -9 gconfd-2
+		#cp ${clst_sharedir}/targets/livecd-stage1/livecd-stage1-preclean-chroot.sh ${clst_chroot_path}/tmp
+		#${clst_CHROOT} ${clst_chroot_path} /tmp/livecd-stage1-preclean-chroot.sh || exit 1
 		#rm -f ${clst_chroot_path}/tmp/livecd-stage1-preclean-chroot.sh
 		exit 0
-    ;;
+	;;
 
-    clean)
-        exit 0
-    ;;
+	clean)
+		exit 0
+	;;
 
-    *)
-        exit 1
-    ;;
+	*)
+		exit 1
+	;;
 
 esac
 exit 0
