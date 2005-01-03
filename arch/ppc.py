f909361a70f6fc7ac5d91d6c5915b68ea683cdc4
John P. Davis <zhen@gentoo.org>
2005-01-03 21:09:09 +0000

    new PPC arch file from pvdabeel@gentoo.org
    git-svn-id: svn+ssh://svn.gentoo.org/var/svnroot/catalyst/trunk@506 d1e1f19c-881f-0410-ab34-b69fee027534

--- arch/ppc.py
+++ arch/ppc.py
@@ -1,6 +1,6 @@
 # Copyright 1999-2004 Gentoo Foundation
 # Distributed under the terms of the GNU General Public License v2
-# $Header: /var/cvsroot/gentoo/src/catalyst/arch/ppc.py,v 1.11 2004/10/15 02:36:00 zhen Exp $
+# $Header: /var/cvsroot/gentoo/src/catalyst/arch/ppc.py,v 1.12 2005/01/03 21:09:09 zhen Exp $
 
 import os,builder
 from catalyst_support import *
@@ -27,44 +27,38 @@ class arch_power_ppc(generic_ppc):
 	"builder class for generic powerpc/power"
 	def __init__(self,myspec):
 		generic_ppc.__init__(self,myspec)
-		self.settings["CFLAGS"]="-O3 -mcpu=common"
+		self.settings["CFLAGS"]="-O2 -mtune=common -fno-strict-aliasing -pipe"
 
 class arch_ppc(generic_ppc):
 	"builder class for generic powerpc"
 	def __init__(self,myspec):
 		generic_ppc.__init__(self,myspec)
-		self.settings["CFLAGS"]="-O3 -mcpu=powerpc"
+		self.settings["CFLAGS"]="-O2 -mtune=powerpc -fno-strict-aliasing -pipe"
 
 class arch_power(generic_ppc):
 	"builder class for generic power"
 	def __init__(self,myspec):
 		generic_ppc.__init__(self,myspec)
-		self.settings["CFLAGS"]="-O3 -mcpu=power"
+		self.settings["CFLAGS"]="-O2 -mtune=power -fno-strict-aliasing -pipe"
 
 class arch_g3(generic_ppc):
 	def __init__(self,myspec):
 		generic_ppc.__init__(self,myspec)
-		self.settings["CFLAGS"]="-O3 -mcpu=G3 -fno-strict-aliasing -pipe"
+		self.settings["CFLAGS"]="-O2 -mtune=G3 -fno-strict-aliasing -pipe"
 
 class arch_g4(generic_ppc):
 	def __init__(self,myspec):
 		generic_ppc.__init__(self,myspec)
-		self.settings["CFLAGS"]="-O3 -mcpu=G4 -maltivec -mabi=altivec -fno-strict-aliasing -pipe"
+		self.settings["CFLAGS"]="-O2 -mtune=G4 -maltivec -mabi=altivec -fno-strict-aliasing -pipe"
 		self.settings["HOSTUSE"]=["altivec"]
 
 class arch_g5(generic_ppc):
 	def __init__(self,myspec):
 		generic_ppc.__init__(self,myspec)
-		self.settings["CFLAGS"]="-O2 -mcpu=G5 -maltivec -mabi=altivec -fno-strict-aliasing -pipe"
+		self.settings["CFLAGS"]="-O2 -mtune=G5 -maltivec -mabi=altivec -fno-strict-aliasing -pipe"
 		self.settings["HOSTUSE"]=["altivec"]
 
-class arch_experimental(generic_ppc):
-	def __init__(self,myspec):
-		generic_ppc.__init__(self,myspec)
-		self.settings["CFLAGS"]="-O3 -mcpu=7450 -maltivec -mabi=altivec -fno-strict-aliasing"
-		self.settings["HOSTUSE"]=["altivec"]
-	
 def register(foo):
 	"Inform main catalyst program of the contents of this plugin."
-	foo.update({"experimental":arch_experimental,"ppc":arch_ppc,"power":arch_power,"power-ppc":arch_power_ppc,"g3":arch_g3,"g4":arch_g4,"g5":arch_g5})
+	foo.update({"ppc":arch_ppc,"power":arch_power,"power-ppc":arch_power_ppc,"g3":arch_g3,"g4":arch_g4,"g5":arch_g5})
 
