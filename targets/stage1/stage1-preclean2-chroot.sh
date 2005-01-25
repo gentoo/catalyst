e01716362da5be9f80313b921a9945512a14b7b9
Chris Gianelloni <wolf31o2@gentoo.org>
2005-01-25 22:54:00 +0000

    Added code to clean up /var/db/pkg, while still keeping the CONTENTS, COUNTER and ebuilds. This should keep a stage1 useable, while still keeping its size small.
    git-svn-id: svn+ssh://svn.gentoo.org/var/svnroot/catalyst/trunk@526 d1e1f19c-881f-0410-ab34-b69fee027534

--- targets/stage1/stage1-preclean2-chroot.sh
+++ targets/stage1/stage1-preclean2-chroot.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 # Copyright 1999-2004 Gentoo Foundation
 # Distributed under the terms of the GNU General Public License v2
-# $Header: /var/cvsroot/gentoo/src/catalyst/targets/stage1/Attic/stage1-preclean2-chroot.sh,v 1.5 2004/12/06 22:25:10 wolf31o2 Exp $
+# $Header: /var/cvsroot/gentoo/src/catalyst/targets/stage1/Attic/stage1-preclean2-chroot.sh,v 1.6 2005/01/25 22:54:00 wolf31o2 Exp $
 		
-#now, some finishing touches to initialize gcc-config....
+# now, some finishing touches to initialize gcc-config....
 unset ROOT
 
 if [ -e /usr/bin/gcc-config ]
@@ -12,10 +12,13 @@ then
 	gcc-config ${mythang}; /usr/sbin/env-update; source /etc/profile
 fi
 
-#stage1 is not going to have anything in zoneinfo, so save our Factory timezone
+# stage1 is not going to have anything in zoneinfo, so save our Factory timezone
 if [ -d /usr/share/zoneinfo ] ; then
 	rm -f /etc/localtime
 	cp /usr/share/zoneinfo/Factory /etc/localtime
 else
 	echo UTC > /etc/TZ
 fi
+
+# this cleans out /var/db, but leaves behind files portage needs for removal
+find /var/db/pkg -type f | grep -v '\(COUNTER\|CONTENTS\|ebuild\)' -exec rm -f '{}' \;
