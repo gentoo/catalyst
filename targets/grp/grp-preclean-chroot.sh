efa9e148ac2fc37de1872d50573408f0b7a7f112
Chris Gianelloni <wolf31o2@gentoo.org>
2005-01-28 19:36:49 +0000

    Re-enabled preclean in grp and added gconfd-2 killing.
    git-svn-id: svn+ssh://svn.gentoo.org/var/svnroot/catalyst/trunk@532 d1e1f19c-881f-0410-ab34-b69fee027534

--- targets/grp/grp-preclean-chroot.sh
+++ targets/grp/grp-preclean-chroot.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 # Copyright 1999-2004 Gentoo Foundation
 # Distributed under the terms of the GNU General Public License v2
-# $Header: /var/cvsroot/gentoo/src/catalyst/targets/grp/grp-preclean-chroot.sh,v 1.4 2004/10/15 02:46:58 zhen Exp $
+# $Header: /var/cvsroot/gentoo/src/catalyst/targets/grp/grp-preclean-chroot.sh,v 1.5 2005/01/28 19:36:49 wolf31o2 Exp $
 
 /usr/sbin/env-update
 source /etc/profile
+
+killall -9 gconfd-2
--- targets/grp/grp.sh
+++ targets/grp/grp.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 # Copyright 1999-2004 Gentoo Foundation
 # Distributed under the terms of the GNU General Public License v2
-# $Header: /var/cvsroot/gentoo/src/catalyst/targets/grp/Attic/grp.sh,v 1.18 2004/10/15 02:46:58 zhen Exp $
+# $Header: /var/cvsroot/gentoo/src/catalyst/targets/grp/Attic/grp.sh,v 1.19 2005/01/28 19:36:49 wolf31o2 Exp $
 
 case $1 in
 	enter)
@@ -20,9 +20,9 @@ case $1 in
 	;;
 
 	preclean)
-		#cp ${clst_sharedir}/targets/grp/grp-preclean-chroot.sh ${clst_chroot_path}/tmp
-		#${clst_CHROOT} ${clst_chroot_path} /tmp/grp-preclean-chroot.sh || exit 1
-		#rm -f ${clst_chroot_path}/tmp/grp-preclean-chroot.sh
+		cp ${clst_sharedir}/targets/grp/grp-preclean-chroot.sh ${clst_chroot_path}/tmp
+		${clst_CHROOT} ${clst_chroot_path} /tmp/grp-preclean-chroot.sh || exit 1
+		rm -f ${clst_chroot_path}/tmp/grp-preclean-chroot.sh
 		exit 0
 	;;
 
