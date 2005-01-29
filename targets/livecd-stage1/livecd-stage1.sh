4205263473d7a43af50fdf43d32b829b9c81c50c
Chris Gianelloni <wolf31o2@gentoo.org>
2005-01-29 04:07:00 +0000

    Added killall for gconfd-2 back into livecd-stage1.sh
    git-svn-id: svn+ssh://svn.gentoo.org/var/svnroot/catalyst/trunk@537 d1e1f19c-881f-0410-ab34-b69fee027534

--- targets/livecd-stage1/livecd-stage1.sh
+++ targets/livecd-stage1/livecd-stage1.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 # Copyright 1999-2004 Gentoo Foundation
 # Distributed under the terms of the GNU General Public License v2
-# $Header: /var/cvsroot/gentoo/src/catalyst/targets/livecd-stage1/Attic/livecd-stage1.sh,v 1.16 2005/01/28 18:37:23 wolf31o2 Exp $
+# $Header: /var/cvsroot/gentoo/src/catalyst/targets/livecd-stage1/Attic/livecd-stage1.sh,v 1.17 2005/01/29 04:07:00 wolf31o2 Exp $
 
 case $1 in
 	enter)
@@ -16,6 +16,7 @@ case $1 in
 	;;
 
 	preclean)
+		killall -9 gconfd-2
 		cp ${clst_sharedir}/targets/livecd-stage1/livecd-stage1-preclean-chroot.sh ${clst_chroot_path}/tmp
 		${clst_CHROOT} ${clst_chroot_path} /tmp/livecd-stage1-preclean-chroot.sh || exit 1
 		rm -f ${clst_chroot_path}/tmp/livecd-stage1-preclean-chroot.sh
