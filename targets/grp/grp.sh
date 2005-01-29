d9ecc9d0896ffc5facaa758d3d5f41f243657a41
Chris Gianelloni <wolf31o2@gentoo.org>
2005-01-29 14:54:31 +0000

    Moved killall -9 gconfd-2 to execute outside chroot.
    git-svn-id: svn+ssh://svn.gentoo.org/var/svnroot/catalyst/trunk@541 d1e1f19c-881f-0410-ab34-b69fee027534

--- targets/grp/grp.sh
+++ targets/grp/grp.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 # Copyright 1999-2004 Gentoo Foundation
 # Distributed under the terms of the GNU General Public License v2
-# $Header: /var/cvsroot/gentoo/src/catalyst/targets/grp/Attic/grp.sh,v 1.19 2005/01/28 19:36:49 wolf31o2 Exp $
+# $Header: /var/cvsroot/gentoo/src/catalyst/targets/grp/Attic/grp.sh,v 1.20 2005/01/29 14:54:31 wolf31o2 Exp $
 
 case $1 in
 	enter)
@@ -20,6 +20,7 @@ case $1 in
 	;;
 
 	preclean)
+		killall -9 gconfd-2
 		cp ${clst_sharedir}/targets/grp/grp-preclean-chroot.sh ${clst_chroot_path}/tmp
 		${clst_CHROOT} ${clst_chroot_path} /tmp/grp-preclean-chroot.sh || exit 1
 		rm -f ${clst_chroot_path}/tmp/grp-preclean-chroot.sh
--- targets/livecd-stage1/livecd-stage1-preclean-chroot.sh
+++ targets/livecd-stage1/livecd-stage1-preclean-chroot.sh
@@ -1,9 +1,7 @@
 #!/bin/bash
 # Copyright 1999-2004 Gentoo Foundation
 # Distributed under the terms of the GNU General Public License v2
-# $Header: /var/cvsroot/gentoo/src/catalyst/targets/livecd-stage1/livecd-stage1-preclean-chroot.sh,v 1.5 2005/01/28 18:37:23 wolf31o2 Exp $
+# $Header: /var/cvsroot/gentoo/src/catalyst/targets/livecd-stage1/livecd-stage1-preclean-chroot.sh,v 1.6 2005/01/29 14:54:31 wolf31o2 Exp $
 
 /usr/sbin/env-update
 source /etc/profile
-
-killall -9 gconfd-2
