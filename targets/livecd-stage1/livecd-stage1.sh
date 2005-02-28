a4bbfdf05ce3fc1e6b79323cc54dee84a2931e99
Chris Gianelloni <wolf31o2@gentoo.org>
2005-02-28 23:40:38 +0000

    -n, not -z
    git-svn-id: svn+ssh://svn.gentoo.org/var/svnroot/catalyst/trunk@548 d1e1f19c-881f-0410-ab34-b69fee027534

--- targets/livecd-stage1/livecd-stage1.sh
+++ targets/livecd-stage1/livecd-stage1.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 # Copyright 1999-2004 Gentoo Foundation
 # Distributed under the terms of the GNU General Public License v2
-# $Header: /var/cvsroot/gentoo/src/catalyst/targets/livecd-stage1/Attic/livecd-stage1.sh,v 1.19 2005/02/28 23:21:09 wolf31o2 Exp $
+# $Header: /var/cvsroot/gentoo/src/catalyst/targets/livecd-stage1/Attic/livecd-stage1.sh,v 1.20 2005/02/28 23:40:38 wolf31o2 Exp $
 
 case $1 in
 	enter)
@@ -17,7 +17,7 @@ case $1 in
 
 	preclean)
 		GCONFD_RUNNING="`pidof gconfd-2`"
-		if [ -z "${GCONFD_RUNNING}" ]
+		if [ -n "${GCONFD_RUNNING}" ]
 		then
 			killall -9 gconfd-2
 		fi
