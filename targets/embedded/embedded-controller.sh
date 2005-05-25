a0b89049ccd98b7472c5c706f25bda27a45072f0
Chris Gianelloni <wolf31o2@gentoo.org>
2005-05-25 19:28:59 +0000

    Actually modify the embedded target this time.
    git-svn-id: svn+ssh://svn.gentoo.org/var/svnroot/catalyst/trunk@664 d1e1f19c-881f-0410-ab34-b69fee027534

--- targets/embedded/embedded-controller.sh
+++ targets/embedded/embedded-controller.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 # Copyright 1999-2003 Gentoo Technologies, Inc.
 # Distributed under the terms of the GNU General Public License v2
-# $Header: /var/cvsroot/gentoo/src/catalyst/targets/embedded/embedded-controller.sh,v 1.3 2005/04/27 17:44:58 rocket Exp $
+# $Header: /var/cvsroot/gentoo/src/catalyst/targets/embedded/embedded-controller.sh,v 1.4 2005/05/25 19:28:59 wolf31o2 Exp $
 
 . ${clst_sharedir}/targets/support/functions.sh
 . ${clst_sharedir}/targets/support/filesystem-functions.sh
@@ -32,8 +32,14 @@ case $1 in
 	kernel)
 		shift
 		export clst_kname="$1"
+		# if we have our own linuxrc, copy it in
+		if [ -n "${clst_livecd_linuxrc}" ]
+		then
+			cp -a ${clst_livecd_linuxrc} ${clst_chroot_path}/tmp/linuxrc
+		fi
 		exec_in_chroot ${clst_sharedir}/targets/support/pre-kmerge.sh
 		exec_in_chroot ${clst_sharedir}/targets/support/kmerge.sh
+		rm -f ${clst_chroot_path}/tmp/linuxrc
 		exec_in_chroot ${clst_sharedir}/targets/support/post-kmerge.sh
 	
 	;;
