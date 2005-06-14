76bcbd1674e893ab24893c380b0c7499a644564d
Chris Gianelloni <wolf31o2@gentoo.org>
2005-06-14 19:43:26 +0000

    Added a -p bootstrap when catalyst is called with -V (verbose).
    git-svn-id: svn+ssh://svn.gentoo.org/var/svnroot/catalyst/trunk@690 d1e1f19c-881f-0410-ab34-b69fee027534

--- targets/stage2/stage2-chroot.sh
+++ targets/stage2/stage2-chroot.sh
@@ -1,7 +1,7 @@
-#!/bin/sh
+#!/bin/bash
 # Copyright 1999-2004 Gentoo Foundation
 # Distributed under the terms of the GNU General Public License v2
-# $Header: /var/cvsroot/gentoo/src/catalyst/targets/stage2/stage2-chroot.sh,v 1.11 2005/04/04 17:48:33 rocket Exp $
+# $Header: /var/cvsroot/gentoo/src/catalyst/targets/stage2/stage2-chroot.sh,v 1.12 2005/06/14 19:43:26 wolf31o2 Exp $
 
 . /tmp/chroot-functions.sh
 
@@ -14,6 +14,18 @@ setup_myemergeopts
 ## setup the environment
 export FEATURES="${clst_myfeatures}"
 GRP_STAGE23_USE="$(portageq envvar GRP_STAGE23_USE)"
+
+if [ "${clst_VERBOSE}" ]
+then
+	/usr/portage/scripts/bootstrap.sh -p ${bootstrap_opts}
+	echo "Press any key within 15 seconds to pause the build..."
+	read -s -t 15 -n 1
+	if [ $? -eq 0 ]
+	then
+		echo "Press any key to continue..."
+		read -s -n 1
+	fi
+fi
 
 ## START BUILD
 /usr/portage/scripts/bootstrap.sh ${bootstrap_opts} || exit 1
