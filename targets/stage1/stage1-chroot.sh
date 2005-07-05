fbfd7caa6597632fb8a86141a5f0c36bd72220b7
Chris Gianelloni <wolf31o2@gentoo.org>
2005-07-05 17:55:30 +0000

    Added profile sanity check for bug #97867.
    git-svn-id: svn+ssh://svn.gentoo.org/var/svnroot/catalyst/trunk@738 d1e1f19c-881f-0410-ab34-b69fee027534

--- targets/stage1/stage1-chroot.sh
+++ targets/stage1/stage1-chroot.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 # Copyright 1999-2004 Gentoo Foundation
 # Distributed under the terms of the GNU General Public License v2
-# $Header: /var/cvsroot/gentoo/src/catalyst/targets/stage1/stage1-chroot.sh,v 1.32 2005/04/29 21:31:32 rocket Exp $
+# $Header: /var/cvsroot/gentoo/src/catalyst/targets/stage1/stage1-chroot.sh,v 1.33 2005/07/05 17:55:30 wolf31o2 Exp $
 	
 
 . /tmp/chroot-functions.sh
@@ -19,6 +19,15 @@ export clst_buildpkgs="$(/tmp/build.py)"
 export STAGE1_USE="$(portageq envvar STAGE1_USE)"
 export USE="-* build ${STAGE1_USE}"
 export FEATURES="${clst_myfeatures}"
+
+## Sanity check profile
+if [[ -z ${clst_buildpkgs} ]]
+then
+	echo "Your profile seems to be broken."
+	echo "Could not build a list of build packages."
+	echo "Double check your /etc/make.profile link and the 'packages' files."
+	exit 1
+fi
 
 ## START BUILD
 run_emerge "--noreplace ${clst_buildpkgs}"
