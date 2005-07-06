61081abbcda3817a0adfef891125786c3ba3a23a
Chris Gianelloni <wolf31o2@gentoo.org>
2005-07-06 22:07:46 +0000

    Changed to use linux32 for ppc32 support when build host is ppc64.
    git-svn-id: svn+ssh://svn.gentoo.org/var/svnroot/catalyst/trunk@758 d1e1f19c-881f-0410-ab34-b69fee027534

--- arch/ppc.py
+++ arch/ppc.py
@@ -1,6 +1,6 @@
 # Copyright 1999-2005 Gentoo Foundation
 # Distributed under the terms of the GNU General Public License v2
-# $Header: /var/cvsroot/gentoo/src/catalyst/arch/ppc.py,v 1.13 2005/07/05 21:53:41 wolf31o2 Exp $
+# $Header: /var/cvsroot/gentoo/src/catalyst/arch/ppc.py,v 1.14 2005/07/06 22:07:46 wolf31o2 Exp $
 
 import os,builder
 from catalyst_support import *
@@ -17,9 +17,9 @@ class generic_ppc(builder.generic):
 		self.settings["mainarch"]="ppc"
 		self.settings["CHOST"]="powerpc-unknown-linux-gnu"
 		if self.settings["hostarch"]=="ppc64":
-			if not os.path.exists("/usr/bin/powerpc32"):
-				raise CatalystError,"required /usr/bin/setarch executable not found."
-			self.settings["CHROOT"]="/usr/bin/powerpc32 chroot"
+			if not os.path.exists("/usr/bin/linux32"):
+				raise CatalystError,"required /usr/bin/linux32 executable not found."
+			self.settings["CHROOT"]="/usr/bin/linux32 chroot"
 		else:   
 			self.settings["CHROOT"]="chroot"
 
