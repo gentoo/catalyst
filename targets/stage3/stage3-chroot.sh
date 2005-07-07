c32220c2466030fdaa7ce7c56c764b24c5aba480
Chris Gianelloni <wolf31o2@gentoo.org>
2005-07-07 22:19:08 +0000

    Fixing USE for stage3.
    git-svn-id: svn+ssh://svn.gentoo.org/var/svnroot/catalyst/trunk@773 d1e1f19c-881f-0410-ab34-b69fee027534

--- targets/stage3/stage3-chroot.sh
+++ targets/stage3/stage3-chroot.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 # Copyright 1999-2005 Gentoo Foundation
 # Distributed under the terms of the GNU General Public License v2
-# $Header: /var/cvsroot/gentoo/src/catalyst/targets/stage3/stage3-chroot.sh,v 1.21 2005/07/07 17:21:08 rocket Exp $
+# $Header: /var/cvsroot/gentoo/src/catalyst/targets/stage3/stage3-chroot.sh,v 1.22 2005/07/07 22:19:08 wolf31o2 Exp $
 
 . /tmp/chroot-functions.sh
 check_portage_version
@@ -15,7 +15,7 @@ setup_myemergeopts
 # setup the build environment
 export FEATURES="${clst_myfeatures}"
 export CONFIG_PROTECT="-*"
-export USE="-* ${clst_HOSTUSE}"
+export USE="${USE} ${clst_HOSTUSE}"
 
 
 ## START BUILD
