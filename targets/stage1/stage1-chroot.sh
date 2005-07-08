6234453ea5ed1fd3a3c8bc48834deef9535d427b
Chris Gianelloni <wolf31o2@gentoo.org>
2005-07-08 15:07:25 +0000

    Fixed quoting in stage1 profile check.
    git-svn-id: svn+ssh://svn.gentoo.org/var/svnroot/catalyst/trunk@777 d1e1f19c-881f-0410-ab34-b69fee027534

--- targets/stage1/stage1-chroot.sh
+++ targets/stage1/stage1-chroot.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 # Copyright 1999-2005 Gentoo Foundation
 # Distributed under the terms of the GNU General Public License v2
-# $Header: /var/cvsroot/gentoo/src/catalyst/targets/stage1/stage1-chroot.sh,v 1.34 2005/07/05 21:53:41 wolf31o2 Exp $
+# $Header: /var/cvsroot/gentoo/src/catalyst/targets/stage1/stage1-chroot.sh,v 1.35 2005/07/08 15:07:25 wolf31o2 Exp $
 	
 
 . /tmp/chroot-functions.sh
@@ -21,7 +21,7 @@ export USE="-* build ${STAGE1_USE}"
 export FEATURES="${clst_myfeatures}"
 
 ## Sanity check profile
-if [[ -z ${clst_buildpkgs} ]]
+if [ -z "${clst_buildpkgs}" ]
 then
 	echo "Your profile seems to be broken."
 	echo "Could not build a list of build packages."
