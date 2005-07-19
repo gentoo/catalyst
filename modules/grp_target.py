51a3fc8cdae6b597f3876d511159da6bb1ef4fee
Eric Edgar <rocket@gentoo.org>
2005-07-19 21:44:14 +0000

    Fix grp so that grp/use is not required anymore
    git-svn-id: svn+ssh://svn.gentoo.org/var/svnroot/catalyst/trunk@804 d1e1f19c-881f-0410-ab34-b69fee027534

--- modules/grp_target.py
+++ modules/grp_target.py
@@ -1,6 +1,6 @@
 # Copyright 1999-2005 Gentoo Foundation
 # Distributed under the terms of the GNU General Public License v2
-# $Header: /var/cvsroot/gentoo/src/catalyst/modules/grp_target.py,v 1.12 2005/07/07 19:57:17 wolf31o2 Exp $
+# $Header: /var/cvsroot/gentoo/src/catalyst/modules/grp_target.py,v 1.13 2005/07/19 21:44:14 rocket Exp $
 
 """
 The builder class for GRP (Gentoo Reference Platform) builds.
@@ -19,12 +19,13 @@ class grp_target(generic_stage_target):
 		if not addlargs.has_key("grp"):
 			raise CatalystError,"Required value \"grp\" not specified in spec."
 		
-		self.required_values.extend(["grp","grp/use"])
+		self.required_values.extend(["grp"])
 		if type(addlargs["grp"])==types.StringType:
 			addlargs["grp"]=[addlargs["grp"]]
-
-		if type(addlargs["grp/use"])==types.StringType:
-			addlargs["grp/use"]=[addlargs["grp/use"]]
+		
+		if addlargs.has_key("grp/use"):
+		    if type(addlargs["grp/use"])==types.StringType:
+			    addlargs["grp/use"]=[addlargs["grp/use"]]
 			
 		for x in addlargs["grp"]:
 			self.required_values.append("grp/"+x+"/packages")
@@ -55,6 +56,8 @@ class grp_target(generic_stage_target):
 	    generic_stage_target.set_use(self)
 	    if self.settings.has_key("use"):
 	    	self.settings["use"].append("bindist")
+	    else:
+	    	self.settings["use"]=["bindist"]
 
 	def set_mounts(self):
 	    self.mounts.append("/tmp/grp")
