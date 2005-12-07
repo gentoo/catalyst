9044f7b61534f47e2d37c0ad201a5710cd3aa4d4
Chris Gianelloni <wolf31o2@gentoo.org>
2005-12-07 21:57:59 +0000

    Removed profiles hack from livecd-stage2's unmerge, since it isn't used anymore and probably should have been removed a long time ago when the new code was put into place in livecdfs-update.sh and livecd-local.start instead.
    git-svn-id: svn+ssh://svn.gentoo.org/var/svnroot/catalyst/trunk@944 d1e1f19c-881f-0410-ab34-b69fee027534

--- targets/livecd-stage2/unmerge.sh
+++ targets/livecd-stage2/unmerge.sh
@@ -1,15 +1,9 @@
 # Copyright 1999-2005 Gentoo Foundation
 # Distributed under the terms of the GNU General Public License v2
-# $Header: /var/cvsroot/gentoo/src/catalyst/targets/livecd-stage2/Attic/unmerge.sh,v 1.9 2005/07/05 21:53:41 wolf31o2 Exp $
+# $Header: /var/cvsroot/gentoo/src/catalyst/targets/livecd-stage2/Attic/unmerge.sh,v 1.10 2005/12/07 21:57:59 wolf31o2 Exp $
 
 ${clst_CHROOT} ${clst_chroot_path} /bin/bash << EOF
 	EMERGE_WARNING_DELAY=0 emerge -C $*
 EOF
-
-if [ -d /usr/livecd/profiles ]
-then
-	mkdir -p /usr/portage/profiles
-	ln -sf /usr/livecd/profiles /usr/portage/profiles
-fi
 
 exit 0
