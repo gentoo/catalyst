dd337a3b717ca839f346a6b086fe654882287581
Chris Gianelloni <wolf31o2@gentoo.org>
2005-12-20 16:28:03 +0000

    Updated sh support from Mike Frysinger <vapier@gentoo.org> and closing bug #115866.
    git-svn-id: svn+ssh://svn.gentoo.org/var/svnroot/catalyst/trunk@986 d1e1f19c-881f-0410-ab34-b69fee027534

--- arch/sh.py
+++ arch/sh.py
@@ -1,6 +1,6 @@
-# Copyright 1999-2004 Gentoo Foundation
+'# Copyright 1999-2004 Gentoo Foundation
 # Distributed under the terms of the GNU General Public License v2
-# $Header: /var/cvsroot/gentoo/src/catalyst/arch/sh.py,v 1.1 2005/09/12 15:31:57 wolf31o2 Exp $
+# $Header: /var/cvsroot/gentoo/src/catalyst/arch/sh.py,v 1.2 2005/12/20 16:28:03 wolf31o2 Exp $
 
 import builder,os
 from catalyst_support import *
@@ -19,6 +19,20 @@ class generic_sheb(builder.generic):
 		self.settings["mainarch"]="sh"
 		self.settings["CHROOT"]="chroot"
 
+class arch_sh(generic_sh):
+	"Builder class for SH [Little-endian]"
+	def __init__(self,myspec):
+		generic_sh.__init__(self,myspec)
+		self.settings["CFLAGS"]="-O2"
+		self.settings["CHOST"]="sh-unknown-linux-gnu"
+
+class arch_sh2(generic_sh):
+	"Builder class for SH-2 [Little-endian]"
+	def __init__(self,myspec):
+		generic_sh.__init__(self,myspec)
+		self.settings["CFLAGS"]="-O2 -m2"
+		self.settings["CHOST"]="sh2-unknown-linux-gnu"
+
 class arch_sh3(generic_sh):
 	"Builder class for SH-3 [Little-endian]"
 	def __init__(self,myspec):
@@ -33,27 +47,43 @@ class arch_sh4(generic_sh):
 		self.settings["CFLAGS"]="-O2 -m4"
 		self.settings["CHOST"]="sh4-unknown-linux-gnu"
 
+class arch_sheb(generic_sheb):
+	"Builder class for SH [Big-endian]"
+	def __init__(self,myspec):
+		generic_sheb.__init__(self,myspec)
+		self.settings["CFLAGS"]="-O2"
+		self.settings["CHOST"]="sheb-unknown-linux-gnu"
+
+class arch_sh2eb(generic_sheb):
+	"Builder class for SH-2 [Big-endian]"
+	def __init__(self,myspec):
+		generic_sheb.__init__(self,myspec)
+		self.settings["CFLAGS"]="-O2 -m2"
+		self.settings["CHOST"]="sh2eb-unknown-linux-gnu"
+
 class arch_sh3eb(generic_sheb):
 	"Builder class for SH-3 [Big-endian]"
 	def __init__(self,myspec):
 		generic_sheb.__init__(self,myspec)
-		self.settings["CFLAGS"]="-O2 -m3 -mb"
+		self.settings["CFLAGS"]="-O2 -m3"
 		self.settings["CHOST"]="sh3eb-unknown-linux-gnu"
 
 class arch_sh4eb(generic_sheb):
 	"Builder class for SH-4 [Big-endian]"
 	def __init__(self,myspec):
 		generic_sheb.__init__(self,myspec)
-		self.settings["CFLAGS"]="-O2 -m4 -mb"
+		self.settings["CFLAGS"]="-O2 -m4"
 		self.settings["CHOST"]="sh4eb-unknown-linux-gnu"
 
 def register(foo):
 	"Inform main catalyst program of the contents of this plugin."
 	foo.update({
-			"sh"	:arch_sh3,
+			"sh"	:arch_sh,
+			"sh2"	:arch_sh2,
 			"sh3"	:arch_sh3,
 			"sh4"	:arch_sh4,
-			"sheb"	:arch_sh3eb,
+			"sheb"	:arch_sheb,
+			"sh2eb" :arch_sh2eb,
 			"sh3eb"	:arch_sh3eb,
 			"sh4eb"	:arch_sh4eb
 	})
--- modules/generic_stage_target.py
+++ modules/generic_stage_target.py
@@ -1,6 +1,6 @@
 # Copyright 1999-2005 Gentoo Foundation
 # Distributed under the terms of the GNU General Public License v2
-# $Header: /var/cvsroot/gentoo/src/catalyst/modules/generic_stage_target.py,v 1.102 2005/12/19 22:18:54 rocket Exp $
+# $Header: /var/cvsroot/gentoo/src/catalyst/modules/generic_stage_target.py,v 1.103 2005/12/20 16:28:03 wolf31o2 Exp $
 
 """
 This class does all of the chroot setup, copying of files, etc. It is
@@ -52,8 +52,10 @@ class generic_stage_target(generic_target):
 				"ia64" : "ia64",
 				"alpha" : "alpha",
 				"sparc" : "sparc",
+				"sh2" : "sh",
 				"sh3" : "sh",
 				"sh4" : "sh",
+				"sh2eb" : "sh",
 				"sh3eb" : "sh",
 				"sh4eb" : "sh",
 				"s390" : "s390",
