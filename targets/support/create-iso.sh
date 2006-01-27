4cac0543782d269c3948ab61a7bc771fa257341f
Chris Gianelloni <wolf31o2@gentoo.org>
2006-01-27 22:57:16 +0000

    Fixing HFS bless on PPC64.  This is catalyst 2.0_rc26.
    git-svn-id: svn+ssh://svn.gentoo.org/var/svnroot/catalyst/trunk@1052 d1e1f19c-881f-0410-ab34-b69fee027534

--- targets/support/create-iso.sh
+++ targets/support/create-iso.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 # Copyright 1999-2005 Gentoo Foundation
 # Distributed under the terms of the GNU General Public License v2
-# $Header: /var/cvsroot/gentoo/src/catalyst/targets/support/create-iso.sh,v 1.25 2006/01/27 22:49:10 rocket Exp $
+# $Header: /var/cvsroot/gentoo/src/catalyst/targets/support/create-iso.sh,v 1.26 2006/01/27 22:57:16 wolf31o2 Exp $
 
 . ${clst_sharedir}/targets/support/functions.sh
 . ${clst_sharedir}/targets/support/filesystem-functions.sh
@@ -234,27 +234,27 @@ case ${clst_mainarch} in
 			zisofs)
 				echo "Running mkisofs to create iso image...."
 				echo "mkisofs -J -r -U -z -chrp-boot -netatalk -hfs -probe \
-					-map ${clst_target_path}/boot/map.hfs -part -no-desktop \
+					-map ${clst_target_path}boot/map.hfs -part -no-desktop \
 					-hfs-volid \"${clst_iso_volume_id}\" -hfs-bless \
-					${clst_target_path}/boot -V \"${clst_iso_volume_id}\" -o \
+					${clst_target_path}boot -V \"${clst_iso_volume_id}\" -o \
 					${1} ${clst_target_path}"
 				mkisofs -J -r -U -z -chrp-boot -netatalk -hfs -probe -map \
-					${clst_target_path}/boot/map.hfs -part -no-desktop \
+					${clst_target_path}boot/map.hfs -part -no-desktop \
 					-hfs-volid "${clst_iso_volume_id}" -hfs-bless \
-					${clst_target_path}/boot -V "${clst_iso_volume_id}" -o \
+					${clst_target_path}boot -V "${clst_iso_volume_id}" -o \
 					${1} ${clst_target_path} || die "Cannot make ISO image"
 			;;
 			*)
 				echo "Running mkisofs to create iso image...."
 				echo "mkisofs -J -r -U -chrp-boot -netatalk -hfs -probe -map \
-					${clst_target_path}/boot/map.hfs -part -no-desktop \
+					${clst_target_path}boot/map.hfs -part -no-desktop \
 					-hfs-volid \"${clst_iso_volume_id}\" -hfs-bless \
-					${clst_target_path}/boot -V \"${clst_iso_volume_id}\" -o \
+					${clst_target_path}boot -V \"${clst_iso_volume_id}\" -o \
 					${1} ${clst_target_path}"
 				mkisofs -J -r -U -chrp-boot -netatalk -hfs -probe -map \
-					${clst_target_path}/boot/map.hfs -part -no-desktop \
+					${clst_target_path}boot/map.hfs -part -no-desktop \
 					-hfs-volid "${clst_iso_volume_id}" -hfs-bless \
-					${clst_target_path}/boot -V "${clst_iso_volume_id}" -o \
+					${clst_target_path}boot -V "${clst_iso_volume_id}" -o \
 					${1} ${clst_target_path} || die "Cannot make ISO image"
 			;;
 		esac
