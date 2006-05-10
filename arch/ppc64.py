73eb26b2e72783f3b9ae8ee4033d1e4997048ca4
Chris Gianelloni <wolf31o2@gentoo.org>
2006-05-10 13:23:00 +0000

    Added 970, power3, power4, and power5 sub-arches for ppc64.
    git-svn-id: svn+ssh://svn.gentoo.org/var/svnroot/catalyst/trunk@1132 d1e1f19c-881f-0410-ab34-b69fee027534

--- arch/ppc64.py
+++ arch/ppc64.py
@@ -1,6 +1,6 @@
 # Copyright 1999-2005 Gentoo Foundation
 # Distributed under the terms of the GNU General Public License v2
-# $Header: /var/cvsroot/gentoo/src/catalyst/arch/ppc64.py,v 1.4 2006/01/03 14:55:11 wolf31o2 Exp $
+# $Header: /var/cvsroot/gentoo/src/catalyst/arch/ppc64.py,v 1.5 2006/05/10 13:23:00 wolf31o2 Exp $
 
 import builder
 
@@ -12,13 +12,43 @@ class generic_ppc64(builder.generic):
 		self.settings["CHROOT"]="chroot"
 
 class arch_ppc64(generic_ppc64):
-	"builder class for generic ppc64 (G5/Power4/Power4+)"
+	"builder class for generic ppc64 (G5/Power4/Power5)"
 	def __init__(self,myspec):
 		generic_ppc64.__init__(self,myspec)
 		self.settings["CFLAGS"]="-O2 -pipe"
+		self.settings["CXXFLAGS"]="-O2 -pipe"
 		self.settings["CHOST"]="powerpc64-unknown-linux-gnu"
+
+class arch_970(generic_ppc64):
+	"builder class for G5 under ppc64"
+	def __init__(self,myspec):
+		generic_ppc64.__init__(self,myspec)
+		self.settings["CFLAGS"]="-O2 -pipe -mcpu=970 -mtune=970 -mabi=altivec"
+		self.settings["CXXFLAGS"]="-O2 -pipe -mcpu=970 -mtune=970 -mabi=altivec"
+		self.settings["HOSTUSE"]=["altivec"]
+
+class arch_power3(generic_ppc64):
+	"builder class for power3 under ppc64"
+	def __init__(self,myspec):
+		generic_ppc64.__init__(self,myspec)
+		self.settings["CFLAGS"]="-O2 -pipe -mcpu=power3 -mtune=power3"
+		self.settings["CXXFLAGS"]="-O2 -pipe -mcpu=power3 -mtune=power3"
+
+class arch_power4(generic_ppc64):
+	"builder class for power4 under ppc64"
+	def __init__(self,myspec):
+		generic_ppc64.__init__(self,myspec)
+		self.settings["CFLAGS"]="-O2 -pipe -mcpu=power4 -mtune=power4"
+		self.settings["CXXFLAGS"]="-O2 -pipe -mcpu=power4 -mtune=power4"
+
+class arch_power5(generic_ppc64):
+	"builder class for power5 under ppc64"
+	def __init__(self,myspec):
+		generic_ppc64.__init__(self,myspec)
+		self.settings["CFLAGS"]="-O2 -pipe -mcpu=power5 -mtune=power5"
+		self.settings["CXXFLAGS"]="-O2 -pipe -mcpu=power5 -mtune=power5"
 
 def register(foo):
 	"Inform main catalyst program of the contents of this plugin."
-	foo.update({"ppc64":arch_ppc64})
+	foo.update({"ppc64":arch_ppc64,"970":arch_970,"power3":arch_power3,"power4":arch_power4,"power5":arch_power5})
 
