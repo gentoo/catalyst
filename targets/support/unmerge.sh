8ceac5496e1bdb66e623c7c27a99ae4e8986f22d
Chris Gianelloni <wolf31o2@gentoo.org>
2006-06-20 19:32:31 +0000

    Change our unmerge from being a single unmerge to a loop, to work around an unexpected change in portage 2.1's unmerge processing.
    git-svn-id: svn+ssh://svn.gentoo.org/var/svnroot/catalyst/trunk@1150 d1e1f19c-881f-0410-ab34-b69fee027534

--- targets/support/unmerge.sh
+++ targets/support/unmerge.sh
@@ -1,11 +1,19 @@
 # Copyright 1999-2005 Gentoo Foundation
 # Distributed under the terms of the GNU General Public License v2
-# $Header: /var/cvsroot/gentoo/src/catalyst/targets/support/unmerge.sh,v 1.3 2005/12/19 16:52:01 wolf31o2 Exp $
+# $Header: /var/cvsroot/gentoo/src/catalyst/targets/support/unmerge.sh,v 1.4 2006/06/20 19:32:31 wolf31o2 Exp $
 
 
 source /tmp/chroot-functions.sh
 
 update_env_settings
 
-run_emerge -C "${clst_packages}"
+# Add a fun loop, so we have to init portage every single time, causing it to
+# run much slower to work around unexpected changes in portage 2.1 with respect
+# to how it processes unmerge for non-existent packages.  A word to other
+# developers out there.  Don't change behavior that people rely on without
+# documenting the change.  It really sucks when you do.
+for package in ${clst_packages}
+do
+	run_emerge -C "${package}"
+done
 exit 0
