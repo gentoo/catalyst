552d49da0b80aec7eb7b98200b44b6da8adc9dfa
Chris Gianelloni <wolf31o2@gentoo.org>
2007-04-10 21:30:45 +0000

    Added s390x (64-bit) support via a patch from Mike Frysinger <vapier@gentoo.org> for bug #173002.
    git-svn-id: svn+ssh://svn.gentoo.org/var/svnroot/catalyst/trunk@1226 d1e1f19c-881f-0410-ab34-b69fee027534

--- arch/s390.py
+++ arch/s390.py
@@ -8,6 +8,12 @@ class generic_s390(builder.generic):
 		builder.generic.__init__(self,myspec)
 		self.settings["CHROOT"]="chroot"
 
+class generic_s390x(builder.generic):
+	"abstract base class for all s390x builders"
+	def __init__(self,myspec):
+		builder.generic.__init__(self,myspec)
+		self.settings["CHROOT"]="chroot"
+
 class arch_s390(generic_s390):
 	"builder class for generic s390"
 	def __init__(self,myspec):
@@ -15,6 +21,13 @@ class arch_s390(generic_s390):
 		self.settings["CFLAGS"]="-O2 -pipe"
 		self.settings["CHOST"]="s390-ibm-linux-gnu"
 
+class arch_s390x(generic_s390x):
+	"builder class for generic s390x"
+	def __init__(self,myspec):
+		generic_s390x.__init__(self,myspec)
+		self.settings["CFLAGS"]="-O2 -pipe"
+		self.settings["CHOST"]="s390x-ibm-linux-gnu"
+
 def register():
 	"Inform main catalyst program of the contents of this plugin."
-	return ({"s390":arch_s390}, ("s390", ))
+	return ({"s390":arch_s390,"s390x":arch_s390x}, ("s390", "s390x"))
