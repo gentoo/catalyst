74447085f59039fb0d7362b32bb2ac62182d3de2
Chris Gianelloni <wolf31o2@gentoo.org>
2007-11-13 21:34:21 +0000

    Removing cleaning of python encodings for bug #64890.
    git-svn-id: svn+ssh://svn.gentoo.org/var/svnroot/catalyst/trunk@1255 d1e1f19c-881f-0410-ab34-b69fee027534

--- modules/stage1_target.py
+++ modules/stage1_target.py
@@ -24,13 +24,11 @@ class stage1_target(generic_stage_target):
 	def set_cleanables(self):
 		generic_stage_target.set_cleanables(self)
 		self.settings["cleanables"].extend(["/usr/share/gettext",\
-		"/usr/lib/python2.2/test", "/usr/lib/python2.2/encodings",\
-		"/usr/lib/python2.2/email", "/usr/lib/python2.2/lib-tk",\
-		"/usr/lib/python2.3/test", "/usr/lib/python2.3/encodings",\
+		"/usr/lib/python2.2/test", "/usr/lib/python2.2/email",\
+		"/usr/lib/python2.2/lib-tk", "/usr/lib/python2.3/test",\
 		"/usr/lib/python2.3/email", "/usr/lib/python2.3/lib-tk",\
-		"/usr/lib/python2.4/test", "/usr/lib/python2.4/encodings",\
-		"/usr/lib/python2.4/email", "/usr/lib/python2.4/lib-tk",\
-		"/usr/share/zoneinfo"])
+		"/usr/lib/python2.4/test", "/usr/lib/python2.4/email",\
+		"/usr/lib/python2.4/lib-tk", "/usr/share/zoneinfo"])
 
 	# XXX: How do these override_foo() functions differ from the ones in generic_stage_target and why aren't they in stage3_target?
 
