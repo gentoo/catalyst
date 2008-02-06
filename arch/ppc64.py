33d0469d15bda8bd926fe2c740e2f64d67745961
Chris Gianelloni <wolf31o2@gentoo.org>
2008-02-06 22:45:22 +0000

    Added a patch from Markus Rothe <corsair@gentoo.org> to add power6 and cell subarches to ppc64 for bug #208860 and removing redundant CXXFLAGS settings, since catalyst sets CXXFLAGS=CFLAGS by default.
    git-svn-id: svn+ssh://svn.gentoo.org/var/svnroot/catalyst/trunk@1277 d1e1f19c-881f-0410-ab34-b69fee027534

--- arch/ppc64.py
+++ arch/ppc64.py
@@ -8,19 +8,24 @@ class generic_ppc64(builder.generic):
 		self.settings["CHROOT"]="chroot"
 
 class arch_ppc64(generic_ppc64):
-	"builder class for generic ppc64 (G5/Power4/Power5)"
+	"builder class for generic ppc64"
 	def __init__(self,myspec):
 		generic_ppc64.__init__(self,myspec)
 		self.settings["CFLAGS"]="-O2 -pipe"
-		self.settings["CXXFLAGS"]="-O2 -pipe"
 		self.settings["CHOST"]="powerpc64-unknown-linux-gnu"
 
 class arch_970(arch_ppc64):
-	"builder class for G5 under ppc64"
+	"builder class for 970 aka G5 under ppc64"
 	def __init__(self,myspec):
 		arch_ppc64.__init__(self,myspec)
-		self.settings["CFLAGS"]="-O2 -pipe -mcpu=970 -mtune=970 -mabi=altivec"
-		self.settings["CXXFLAGS"]="-O2 -pipe -mcpu=970 -mtune=970 -mabi=altivec"
+		self.settings["CFLAGS"]="-O2 -pipe -mcpu=970 -mtune=970"
+		self.settings["HOSTUSE"]=["altivec"]
+
+class arch_cell(arch_ppc64):
+	"builder class for cell under ppc64"
+	def __init__(self,myspec):
+		arch_ppc64.__init__(self,myspec)
+		self.settings["CFLAGS"]="-O2 -pipe -mcpu=cell -mtune=cell"
 		self.settings["HOSTUSE"]=["altivec"]
 
 class arch_power3(arch_ppc64):
@@ -28,25 +33,36 @@ class arch_power3(arch_ppc64):
 	def __init__(self,myspec):
 		arch_ppc64.__init__(self,myspec)
 		self.settings["CFLAGS"]="-O2 -pipe -mcpu=power3 -mtune=power3"
-		self.settings["CXXFLAGS"]="-O2 -pipe -mcpu=power3 -mtune=power3"
 
 class arch_power4(arch_ppc64):
 	"builder class for power4 under ppc64"
 	def __init__(self,myspec):
 		arch_ppc64.__init__(self,myspec)
 		self.settings["CFLAGS"]="-O2 -pipe -mcpu=power4 -mtune=power4"
-		self.settings["CXXFLAGS"]="-O2 -pipe -mcpu=power4 -mtune=power4"
 
 class arch_power5(arch_ppc64):
 	"builder class for power5 under ppc64"
 	def __init__(self,myspec):
 		arch_ppc64.__init__(self,myspec)
 		self.settings["CFLAGS"]="-O2 -pipe -mcpu=power5 -mtune=power5"
-		self.settings["CXXFLAGS"]="-O2 -pipe -mcpu=power5 -mtune=power5"
+
+class arch_power6(arch_ppc64):
+	"builder class for power6 under ppc64"
+	def __init__(self,myspec):
+		arch_ppc64.__init__(self,myspec)
+		self.settings["CFLAGS"]="-O2 -pipe -mcpu=power6 -mtune=power6"
+		self.settings["HOSTUSE"]=["altivec"]
 
 def register():
 	"Inform main catalyst program of the contents of this plugin."
-	return ({"ppc64":arch_ppc64,"970":arch_970,"power3":arch_power3,"power4":arch_power4,"power5":arch_power5},
-	("ppc64","powerpc64"))
+	return ({
+		"ppc64":	arch_ppc64,
+		"970":		arch_970,
+		"cell":		arch_cell,
+		"power3":	arch_power3,
+		"power4":	arch_power4,
+		"power5":	arch_power5,
+		"power6":	arch_power6
+	}, ("ppc64","powerpc64"))
 
 
