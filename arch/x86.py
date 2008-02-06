fd1f4302036a3ff8ed7730c3ab58f83b6876f5fa
Chris Gianelloni <wolf31o2@gentoo.org>
2008-02-06 22:08:38 +0000

    Added prescott to the subarches for x86.
    git-svn-id: svn+ssh://svn.gentoo.org/var/svnroot/catalyst/trunk@1275 d1e1f19c-881f-0410-ab34-b69fee027534

--- arch/x86.py
+++ arch/x86.py
@@ -59,19 +59,12 @@ class arch_athlon(generic_x86):
 		self.settings["HOSTUSE"]=["mmx","3dnow"]
 
 class arch_athlon_xp(generic_x86):
-	#this handles XP and MP processors
+	# This handles XP and MP processors
 	def __init__(self,myspec):
 		generic_x86.__init__(self,myspec)
 		self.settings["CFLAGS"]="-O2 -march=athlon-xp -pipe"
 		self.settings["CHOST"]="i686-pc-linux-gnu"
 		self.settings["HOSTUSE"]=["mmx","3dnow","sse"]
-
-class arch_pentium4(generic_x86):
-	def __init__(self,myspec):
-		generic_x86.__init__(self,myspec)
-		self.settings["CFLAGS"]="-O2 -march=pentium4 -pipe"
-		self.settings["CHOST"]="i686-pc-linux-gnu"
-		self.settings["HOSTUSE"]=["mmx","sse"]
 
 class arch_pentium3(generic_x86):
 	def __init__(self,myspec):
@@ -80,9 +73,37 @@ class arch_pentium3(generic_x86):
 		self.settings["CHOST"]="i686-pc-linux-gnu"
 		self.settings["HOSTUSE"]=["mmx","sse"]
 
+class arch_pentium4(generic_x86):
+	def __init__(self,myspec):
+		generic_x86.__init__(self,myspec)
+		self.settings["CFLAGS"]="-O2 -march=pentium4 -pipe"
+		self.settings["CHOST"]="i686-pc-linux-gnu"
+		self.settings["HOSTUSE"]=["mmx","sse"]
+
+class arch_prescott(generic_x86):
+	def __init__(self,myspec):
+		generic_x86.__init__(self,myspec)
+		self.settings["CFLAGS"]="-O2 -march=prescott -pipe"
+		self.settings["CHOST"]="i686-pc-linux-gnu"
+		self.settings["HOSTUSE"]=["mmx","sse","sse2"]
+
+#class arch_nocona(generic_x86):
+#	def __init__(self,myspec):
+#		generic_x86.__init__(self,myspec)
+#		self.settings["CFLAGS"]="-O2 -march=nocona -pipe"
+#		self.settings["CHOST"]="i686-pc-linux-gnu"
+#		self.settings["HOSTUSE"]=["mmx","sse","sse2"]
+
+#class arch_core2(generic_x86):
+#	def __init__(self,myspec):
+#		generic_x86.__init__(self,myspec)
+#		self.settings["CFLAGS"]="-O2 -march=core2 -pipe"
+#		self.settings["CHOST"]="i686-pc-linux-gnu"
+#		self.settings["HOSTUSE"]=["mmx","sse","sse2"]
+
 def register():
 	"Inform main catalyst program of the contents of this plugin."
 	return ({"pentium4":arch_pentium4,"x86":arch_x86,"i386":arch_i386,"i486":arch_i486,"i586":arch_i586,"i686":arch_i686,"athlon":arch_athlon,
-	"athlon-xp":arch_athlon_xp,"athlon-mp":arch_athlon_xp,"pentium3":arch_pentium3,"pentium-mmx":arch_pentium_mmx},
+	"athlon-xp":arch_athlon_xp,"athlon-mp":arch_athlon_xp,"pentium3":arch_pentium3,"pentium-mmx":arch_pentium_mmx,"prescott":arch_prescott},
 	('i386', 'i486', 'i586', 'i686'))
 
