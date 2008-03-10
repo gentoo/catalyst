ca72b0dc962529eb2a7045c501709271418f5217
Andrew Gaffney <agaffney@gentoo.org>
2008-03-10 20:50:24 +0000

    Call setup_myemergeopts after changing clst_FETCH
    git-svn-id: svn+ssh://svn.gentoo.org/var/svnroot/catalyst/trunk@1368 d1e1f19c-881f-0410-ab34-b69fee027534

--- targets/grp/grp-chroot.sh
+++ targets/grp/grp-chroot.sh
@@ -11,6 +11,9 @@ export PKGDIR="/tmp/grp/${clst_grp_target}"
 if [ "${clst_grp_type}" != "pkgset" ]
 then
 	export clst_FETCH=1
+	# This is necessary since we're setting the above variable and the emerge
+	# opts have already been set
+	setup_myemergeopts
 fi
 
 run_merge --noreplace ${clst_grp_packages} || exit 1
