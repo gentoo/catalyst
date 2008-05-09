a17894138e1bee56a2d77b214e280555898e6dce
Chris Gianelloni <wolf31o2@gentoo.org>
2008-05-09 01:49:44 +0000

    We don't want to run setup_myfeatures in preclean, but still want to update the environment and show our debug information.
    git-svn-id: svn+ssh://svn.gentoo.org/var/svnroot/catalyst/trunk@1406 d1e1f19c-881f-0410-ab34-b69fee027534

--- targets/stage1/stage1-preclean-chroot.sh
+++ targets/stage1/stage1-preclean-chroot.sh
@@ -1,6 +1,11 @@
 #!/bin/bash
 
+export RUN_DEFAULT_FUNCS="no"
+
 source /tmp/chroot-functions.sh
+
+update_env_settings
+show_debug
 
 # Now, some finishing touches to initialize gcc-config....
 unset ROOT
--- targets/stage3/stage3-preclean-chroot.sh
+++ targets/stage3/stage3-preclean-chroot.sh
@@ -1,6 +1,11 @@
 #!/bin/bash
 
+export RUN_DEFAULT_FUNCS="no"
+
 source /tmp/chroot-functions.sh
+
+update_env_settings
+show_debug
 
 cleanup_stages
 
