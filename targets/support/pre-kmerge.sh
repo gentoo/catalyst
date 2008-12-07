d67cca245469d5f3ba02607faf578cfa399caf69
Andrew Gaffney <agaffney@gentoo.org>
2008-12-06 22:01:50 -0600

    Remove evil hacks that copy gk files around for netboot2

--- targets/support/pre-kmerge.sh
+++ targets/support/pre-kmerge.sh
@@ -4,22 +4,3 @@ source /tmp/chroot-functions.sh
 
 run_merge --oneshot genkernel
 install -d /tmp/kerncache
-
-case ${clst_target} in
-	netboot2)
-		# Set the netboot builddate/hostname in linuxrc and copy to proper arch
-		# directory in genkernel
-		sed -e "s/@@MYDATE@@/$(date '+%Y%m%d')/g" \
-		    -e "s/@@RELVER@@/${clst_version_stamp}/g" \
-			/usr/share/genkernel/netboot/linuxrc.x \
-			> /usr/share/genkernel/${clst_hostarch}/linuxrc
-
-		echo ">>> Copying support files to ${clst_root_path} ..."
-		cp -pPRf /usr/share/genkernel/netboot/misc/* \
-			${clst_merge_path}
-
-		echo ">>> Copying busybox config ..."
-		cp -f /tmp/busy-config \
-			/usr/share/genkernel/${clst_hostarch}/busy-config
-	;;
-esac
