860b35a22e5b0d026ffcd39ee5ea01815871b381
Andrew Gaffney <agaffney@gentoo.org>
2009-01-10 18:51:05 -0600

    Forgot the sh arch module

--- modules/catalyst/arch/sh.py
+++ modules/catalyst/arch/sh.py
@@ -2,6 +2,19 @@
 import builder,os
 from catalyst_support import *
 
+__subarch_map = {
+	"sh"	:arch_sh,
+	"sh2"	:arch_sh2,
+	"sh3"	:arch_sh3,
+	"sh4"	:arch_sh4,
+	"sheb"	:arch_sheb,
+	"sh2eb" :arch_sh2eb,
+	"sh3eb"	:arch_sh3eb,
+	"sh4eb"	:arch_sh4eb
+}
+
+__machine_map = ("sh2","sh3","sh4","sh2eb","sh3eb","sh4eb")
+
 class generic_sh(builder.generic):
 	"Abstract base class for all sh builders [Little-endian]"
 	def __init__(self,myspec):
@@ -69,16 +82,3 @@ class arch_sh4eb(generic_sheb):
 		generic_sheb.__init__(self,myspec)
 		self.settings["CFLAGS"]="-O2 -m4 -pipe"
 		self.settings["CHOST"]="sh4eb-unknown-linux-gnu"
-
-def register():
-	"Inform main catalyst program of the contents of this plugin."
-	return ({
-			"sh"	:arch_sh,
-			"sh2"	:arch_sh2,
-			"sh3"	:arch_sh3,
-			"sh4"	:arch_sh4,
-			"sheb"	:arch_sheb,
-			"sh2eb" :arch_sh2eb,
-			"sh3eb"	:arch_sh3eb,
-			"sh4eb"	:arch_sh4eb
-	}, ("sh2","sh3","sh4","sh2eb","sh3eb","sh4eb"))
