6faed8e5d6e0db52f5006105405295d7850d547d
Andrew Gaffney <agaffney@gentoo.org>
2009-01-10 19:08:54 -0600

    add note about module loading method

--- modules/catalyst/util.py
+++ modules/catalyst/util.py
@@ -15,6 +15,8 @@ def print_traceback():
 
 def load_module(name):
 	try:
+		# I'm not sure if it's better to use imp.load_module() for this, but
+		# it seems to work just fine this way, and it's easier.
 		exec("import " + name)
 		return sys.modules[name]
 	except Exception:
