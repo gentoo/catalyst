f491afaeeff097e975365e4c7771b4220ea21e6c
Andrew Gaffney <agaffney@gentoo.org>
2009-01-12 16:04:37 -0600

    Add base singleton class definition

--- modules/catalyst/__init__.py
+++ modules/catalyst/__init__.py
@@ -15,3 +15,16 @@ error = catalyst.error
 spawn = catalyst.spawn
 target = catalyst.target
 config = catalyst.config
+
+"""
+This class implements a proper singleton. This is useful for having a "global"
+var with config and spec values instead of passing them around between objects
+and functions
+"""
+class Singleton(object):
+
+	def __new__(type):
+		if not '_the_instance' in type.__dict__:
+			type._the_instance = object.__new__(type)
+		return type._the_instance
+
