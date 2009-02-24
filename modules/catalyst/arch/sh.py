e89cd6c6fc1a07d32a058ffaa4648106a71b5069
Andrew Gaffney <agaffney@gentoo.org>
2009-02-24 07:30:03 -0600

    Additional sh patch from gentoo bug #255793

--- modules/catalyst/arch/sh.py
+++ modules/catalyst/arch/sh.py
@@ -27,6 +27,13 @@ class arch_sh2(generic_sh):
 		self.settings["CFLAGS"]="-O2 -m2 -pipe"
 		self.settings["CHOST"]="sh2-unknown-linux-gnu"
 
+class arch_sh2a(generic_sh):
+	"Builder class for SH-2A [Little-endian]"
+	def __init__(self,myspec):
+		generic_sh.__init__(self,myspec)
+		self.settings["CFLAGS"]="-O2 -m2a -pipe"
+		self.settings["CHOST"]="sh2a-unknown-linux-gnu"
+
 class arch_sh3(generic_sh):
 	"Builder class for SH-3 [Little-endian]"
 	def __init__(self,myspec):
@@ -42,7 +49,7 @@ class arch_sh4(generic_sh):
 		self.settings["CHOST"]="sh4-unknown-linux-gnu"
 
 class arch_sh4a(generic_sh):
-	"Builder class for SH-4a [Little-endian]"
+	"Builder class for SH-4A [Little-endian]"
 	def __init__(self,myspec):
 		generic_sh.__init__(self,myspec)
 		self.settings["CFLAGS"]="-O2 -m4a -pipe"
@@ -56,7 +63,7 @@ class arch_sheb(generic_sheb):
 		self.settings["CHOST"]="sheb-unknown-linux-gnu"
 
 class arch_sh4aeb(generic_sheb):
-	"Builder class for SH-4a [Big-endian]"
+	"Builder class for SH-4A [Big-endian]"
 	def __init__(self,myspec):
 		generic_sheb.__init__(self,myspec)
 		self.settings["CFLAGS"]="-O2 -m4a -pipe"
@@ -68,6 +75,13 @@ class arch_sh2eb(generic_sheb):
 		generic_sheb.__init__(self,myspec)
 		self.settings["CFLAGS"]="-O2 -m2 -pipe"
 		self.settings["CHOST"]="sh2eb-unknown-linux-gnu"
+
+class arch_sh2aeb(generic_sheb):
+	"Builder class for SH-2A [Big-endian]"
+	def __init__(self,myspec):
+		generic_sheb.__init__(self,myspec)
+		self.settings["CFLAGS"]="-O2 -m2a -pipe"
+		self.settings["CHOST"]="sh2aeb-unknown-linux-gnu"
 
 class arch_sh3eb(generic_sheb):
 	"Builder class for SH-3 [Big-endian]"
@@ -86,14 +100,16 @@ class arch_sh4eb(generic_sheb):
 _subarch_map = {
 	"sh"	:arch_sh,
 	"sh2"	:arch_sh2,
+	"sh2a"	:arch_sh2a,
 	"sh3"	:arch_sh3,
 	"sh4"	:arch_sh4,
 	"sh4a"	:arch_sh4a,
 	"sheb"	:arch_sheb,
+	"sh2aeb":arch_sh2aeb,
 	"sh2eb" :arch_sh2eb,
 	"sh3eb"	:arch_sh3eb,
 	"sh4eb"	:arch_sh4eb
 }
 
-_machine_map = ("sh2","sh3","sh4","sh4a","sh2eb","sh3eb","sh4eb","sh4aeb")
+_machine_map = ("sh2","sh2a","sh3","sh4","sh4a","sh2eb","sh2aeb","sh3eb","sh4eb","sh4aeb")
 
