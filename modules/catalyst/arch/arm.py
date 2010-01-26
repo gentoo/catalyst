423613d4d63f3a603ff416edd814c78b4f729c55
Andrew Gaffney <agaffney@gentoo.org>
2010-01-25 21:18:43 -0600

    Apply patch from armin76 for Gentoo bug #299498

--- modules/catalyst/arch/arm.py
+++ modules/catalyst/arch/arm.py
@@ -33,105 +33,84 @@ class arch_armv4l(generic_arm):
 	def __init__(self,myspec):
 		generic_arm.__init__(self,myspec)
 		self.settings["CHOST"]="armv4l-unknown-linux-gnu"
-		self.settings["CFLAGS"]="-Os -march=armv4 -pipe"
+		self.settings["CFLAGS"]+=" -march=armv4"
 
 class arch_armv4tl(generic_arm):
 	"Builder class for armv4tl target"
 	def __init__(self,myspec):
 		generic_arm.__init__(self,myspec)
 		self.settings["CHOST"]="armv4tl-softfloat-linux-gnueabi"
-		self.settings["CFLAGS"]="-Os -march=armv4t -pipe"
-
-class arch_armv5l(generic_arm):
-	"Builder class for armv5l target"
-	def __init__(self,myspec):
-		generic_arm.__init__(self,myspec)
-		self.settings["CHOST"]="armv5l-softfloat-linux-gnueabi"
-		self.settings["CFLAGS"]="-Os -march=armv5 -pipe"
+		self.settings["CFLAGS"]+=" -march=armv4t"
 
 class arch_armv5tl(generic_arm):
 	"Builder class for armv5tl target"
 	def __init__(self,myspec):
 		generic_arm.__init__(self,myspec)
 		self.settings["CHOST"]="armv5tl-softfloat-linux-gnueabi"
-		self.settings["CFLAGS"]="-Os -march=armv5t -pipe"
+		self.settings["CFLAGS"]+=" -march=armv5t"
 
 class arch_armv5tel(generic_arm):
 	"Builder class for armv5tel target"
 	def __init__(self,myspec):
 		generic_arm.__init__(self,myspec)
 		self.settings["CHOST"]="armv5tel-softfloat-linux-gnueabi"
-		self.settings["CFLAGS"]="-Os -march=armv5te -pipe"
+		self.settings["CFLAGS"]+=" -march=armv5te"
 
 class arch_armv5tejl(generic_arm):
 	"Builder class for armv5tejl target"
 	def __init__(self,myspec):
 		generic_arm.__init__(self,myspec)
 		self.settings["CHOST"]="armv5tejl-softfloat-linux-gnueabi"
-		self.settings["CFLAGS"]="-Os -march=armv5te -pipe"
+		self.settings["CFLAGS"]+=" -march=armv5te"
 
-class arch_armv6l(generic_arm):
-	"Builder class for armv6l target"
+class arch_armv6j(generic_arm):
+	"Builder class for armv6j target"
 	def __init__(self,myspec):
 		generic_arm.__init__(self,myspec)
-		self.settings["CHOST"]="armv6l-softloat-linux-gnueabi"
-		self.settings["CFLAGS"]="-Os -march=armv6 -pipe"
+		self.settings["CHOST"]="armv6j-unknown-linux-gnueabi"
+		self.settings["CFLAGS"]+=" -march=armv6j -mfpu=vfp -mfloat-abi=softfp"
 
-class arch_armv6jl(generic_arm):
-	"Builder class for armv6jl target"
+class arch_armv6t2(generic_arm):
+	"Builder class for armv6t2 target"
 	def __init__(self,myspec):
 		generic_arm.__init__(self,myspec)
-		self.settings["CHOST"]="armv6jl-softloat-linux-gnueabi"
-		self.settings["CFLAGS"]="-Os -march=armv6j -pipe"
+		self.settings["CHOST"]="armv6t2-unknown-linux-gnueabi"
+		self.settings["CFLAGS"]=" -march=armv6t2 -mfpu=vfp -mfloat-abi=softfp"
 
-class arch_armv6t2l(generic_arm):
-	"Builder class for armv6t2l target"
+class arch_armv6z(generic_arm):
+	"Builder class for armv6z target"
 	def __init__(self,myspec):
 		generic_arm.__init__(self,myspec)
-		self.settings["CHOST"]="armv6t2l-softloat-linux-gnueabi"
-		self.settings["CFLAGS"]="-Os -march=armv6t2 -pipe"
+		self.settings["CHOST"]="armv6z-unknown-linux-gnueabi"
+		self.settings["CFLAGS"]+=" -march=armv6z -mfpu=vfp -mfloat-abi=softfp"
 
-class arch_armv6zl(generic_arm):
-	"Builder class for armv6zl target"
+class arch_armv6zk(generic_arm):
+	"Builder class for armv6zk target"
 	def __init__(self,myspec):
 		generic_arm.__init__(self,myspec)
-		self.settings["CHOST"]="armv6zl-softloat-linux-gnueabi"
-		self.settings["CFLAGS"]="-Os -march=armv6z -pipe"
+		self.settings["CHOST"]="armv6zk-unknown-linux-gnueabi"
+		self.settings["CFLAGS"]+=" -march=armv6zk -mfpu=vfp -mfloat-abi=softfp"
 
-class arch_armv6zkl(generic_arm):
-	"Builder class for armv6zkl target"
+class arch_armv7a(generic_arm):
+	"Builder class for armv7a target"
 	def __init__(self,myspec):
 		generic_arm.__init__(self,myspec)
-		self.settings["CHOST"]="armv6zkl-softloat-linux-gnueabi"
-		self.settings["CFLAGS"]="-Os -march=armv6zk -pipe"
+		self.settings["CHOST"]="armv7a-unknown-linux-gnueabi"
+		self.settings["CFLAGS"]+=" -march=armv7-a -mfpu=vfp -mfloat-abi=softfp"
 
-class arch_armv7l(generic_arm):
-	"Builder class for armv7l target"
+class arch_armv7r(generic_arm):
+	"Builder class for armv7r target"
 	def __init__(self,myspec):
 		generic_arm.__init__(self,myspec)
-		self.settings["CHOST"]="armv7l-softfloat-linux-gnueabi"
-		self.settings["CFLAGS"]="-Os -march=armv7 -pipe"
+		self.settings["CHOST"]="armv7rl-unknown-linux-gnueabi"
+		self.settings["CFLAGS"]+=" -march=armv7-r -mfpu=vfp -mfloat-abi=softfp"
 
-class arch_armv7al(generic_arm):
-	"Builder class for armv7al target"
+class arch_armv7m(generic_arm):
+	"Builder class for armv7m target"
 	def __init__(self,myspec):
 		generic_arm.__init__(self,myspec)
-		self.settings["CHOST"]="armv7al-softfloat-linux-gnueabi"
-		self.settings["CFLAGS"]="-Os -march=armv7-a -pipe"
-
-class arch_armv7rl(generic_arm):
-	"Builder class for armv7rl target"
-	def __init__(self,myspec):
-		generic_arm.__init__(self,myspec)
-		self.settings["CHOST"]="armv7ml-softfloat-linux-gnueabi"
-		self.settings["CFLAGS"]="-Os -march=armv7-r -pipe"
-
-class arch_armv7ml(generic_arm):
-	"Builder class for armv7ml target"
-	def __init__(self,myspec):
-		generic_arm.__init__(self,myspec)
-		self.settings["CHOST"]="armv7ml-softfloat-linux-gnueabi"
-		self.settings["CFLAGS"]="-Os -march=armv7-m -pipe"
+		self.settings["CHOST"]="armv7m-unknown-linux-gnueabi"
+		self.settings["CFLAGS"]+=" -march=armv7-m -mfpu=vfp -mfloat-abi=softfp"
 
 class arch_armv5teb(generic_armeb):
 	"Builder class for armv5teb (XScale) target"
@@ -144,23 +123,20 @@ _subarch_map = {
 	"arm"    : arch_arm,
 	"armv4l" : arch_armv4l,
 	"armv4tl": arch_armv4tl,
-	"armv5l" : arch_armv5l,
 	"armv5tl": arch_armv5tl,
-	"armv5teb" : arch_armv5teb,
 	"armv5tel": arch_armv5tel,
 	"armv5tejl": arch_armv5tejl,
-	"armv6l" : arch_armv6l,
-	"armv6jl" : arch_armv6jl,
-	"armv6t2l" : arch_armv6t2l,
-	"armv6zl" : arch_armv6zl,
-	"armv6zkl" : arch_armv6zkl,
-	"armv7l" : arch_armv7l,
-	"armv7al" : arch_armv7al,
-	"armv7rl" : arch_armv7rl,
-	"armv7ml" : arch_armv7ml,
-	"armeb"  : arch_armeb
+	"armv6j" : arch_armv6j,
+	"armv6t2" : arch_armv6t2,
+	"armv6z" : arch_armv6z,
+	"armv6zk" : arch_armv6zk,
+	"armv7a" : arch_armv7a,
+	"armv7r" : arch_armv7r,
+	"armv7m" : arch_armv7m,
+	"armeb"  : arch_armeb,
+	"armv5teb" : arch_armv5teb
 }
 
-_machine_map = ("arm", "armv4l", "armv4tl", "armv5l", "armv5tl", "armv5tel", "armv5tejl", "armv6l", "armv7l", "armeb", "armv5teb")
+_machine_map = ("arm", "armv4l", "armv4tl", "armv5tl", "armv5tel", "armv5tejl", "armv6l", "armv7l", "armeb", "armv5teb")
 
 # vim: ts=4 sw=4 sta noet sts=4 ai
