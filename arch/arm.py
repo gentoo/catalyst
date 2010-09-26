0d0f313452a8552c324b78ee4de40ce4c70fb0c4
Raúl Porcel <armin76@gentoo.org>
2010-09-26 15:06:03 +0000

    ARM fixes

--- arch/arm.py
+++ arch/arm.py
@@ -70,13 +70,6 @@ class arch_armv6j(generic_arm):
 		self.settings["CHOST"]="armv6j-unknown-linux-gnueabi"
 		self.settings["CFLAGS"]+=" -march=armv6j -mfpu=vfp -mfloat-abi=softfp"
 
-class arch_armv6t2(generic_arm):
-	"Builder class for armv6t2 target"
-	def __init__(self,myspec):
-		generic_arm.__init__(self,myspec)
-		self.settings["CHOST"]="armv6t2-unknown-linux-gnueabi"
-		self.settings["CFLAGS"]=" -march=armv6t2 -mfpu=vfp -mfloat-abi=softfp"
-
 class arch_armv6z(generic_arm):
 	"Builder class for armv6z target"
 	def __init__(self,myspec):
@@ -96,28 +89,14 @@ class arch_armv7a(generic_arm):
 	def __init__(self,myspec):
 		generic_arm.__init__(self,myspec)
 		self.settings["CHOST"]="armv7a-unknown-linux-gnueabi"
-		self.settings["CFLAGS"]+=" -march=armv7-a -mfpu=vfp -mfloat-abi=softfp"
-
-class arch_armv7r(generic_arm):
-	"Builder class for armv7r target"
-	def __init__(self,myspec):
-		generic_arm.__init__(self,myspec)
-		self.settings["CHOST"]="armv7r-unknown-linux-gnueabi"
-		self.settings["CFLAGS"]+=" -march=armv7-r -mfpu=vfp -mfloat-abi=softfp"
-
-class arch_armv7m(generic_arm):
-	"Builder class for armv7m target"
-	def __init__(self,myspec):
-		generic_arm.__init__(self,myspec)
-		self.settings["CHOST"]="armv7m-unknown-linux-gnueabi"
-		self.settings["CFLAGS"]+=" -march=armv7-m -mfpu=vfp -mfloat-abi=softfp"
+		self.settings["CFLAGS"]+=" -march=armv7-a -mfpu=vfpv3-d16 -mfloat-abi=softfp"
 
 class arch_armv7a_hardfp(generic_arm):
 	"Builder class for armv7a hardfloat target, needs >=gcc-4.5"
 	def __init__(self,myspec):
 		generic_arm.__init__(self,myspec)
 		self.settings["CHOST"]="armv7a-hardfloat-linux-gnueabi"
-		self.settings["CFLAGS"]+=" -march=armv7-a -mfpu=vfp -mfloat-abi=hardfp"
+		self.settings["CFLAGS"]+=" -march=armv7-a -mfpu=vfpv3-d16 -mfloat-abi=hardfp"
 
 class arch_armv5teb(generic_armeb):
 	"Builder class for armv5teb (XScale) target"
@@ -136,12 +115,9 @@ def register():
 		"armv5tel": arch_armv5tel,
 		"armv5tejl": arch_armv5tejl,
 		"armv6j" : arch_armv6j,
-		"armv6t2" : arch_armv6t2,
 		"armv6z" : arch_armv6z,
 		"armv6zk" : arch_armv6zk,
 		"armv7a" : arch_armv7a,
-		"armv7r" : arch_armv7r,
-		"armv7m" : arch_armv7m,
 		"armv7a_hardfp" : arch_armv7a_hardfp,
 		"armeb"  : arch_armeb,
 		"armv5teb" : arch_armv5teb
