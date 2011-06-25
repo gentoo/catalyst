c42b0354874ef23fab3e0dbd77d3d43f3613faf0
Matt Turner <mattst88@gentoo.org>
2011-06-25 12:09:28 -0400

    mips.py: Remove mips2 classes
    Linux doesn't even run on mips2

--- modules/catalyst/arch/mips.py
+++ modules/catalyst/arch/mips.py
@@ -21,12 +21,6 @@ class arch_mips1(generic_mips):
 		generic_mips.__init__(self,myspec)
 		self.settings["CFLAGS"]="-O2 -mips1 -mabi=32 -pipe"
 
-class arch_mips2(generic_mips):
-	"Builder class for MIPS II [Big-endian]"
-	def __init__(self,myspec):
-		generic_mips.__init__(self,myspec)
-		self.settings["CFLAGS"]="-O2 -mips2 -mabi=32 -pipe"
-
 class arch_mips3(generic_mips):
 	"Builder class for MIPS III [Big-endian]"
 	def __init__(self,myspec):
@@ -76,12 +70,6 @@ class arch_mipsel1(generic_mipsel):
 	def __init__(self,myspec):
 		generic_mipsel.__init__(self,myspec)
 		self.settings["CFLAGS"]="-O2 -mips1 -mabi=32 -pipe"
-
-class arch_mipsel2(generic_mipsel):
-	"Builder class for all MIPS II [Little-endian]"
-	def __init__(self,myspec):
-		generic_mipsel.__init__(self,myspec)
-		self.settings["CFLAGS"]="-O2 -mips2 -mabi=32 -pipe"
 
 class arch_mipsel3(generic_mipsel):
 	"Builder class for all MIPS III [Little-endian]"
@@ -201,7 +189,6 @@ _subarch_map = {
 	"ip30_n32"		: arch_ip30_n32,
 	"mips"			: arch_mips1,
 	"mips1"			: arch_mips1,
-	"mips2"			: arch_mips2,
 	"mips3"			: arch_mips3,
 	"mips3_n32"		: arch_mips3_n32,
 	"mips3_n64"		: arch_mips3_n64,
@@ -209,7 +196,6 @@ _subarch_map = {
 	"mips4_n32"		: arch_mips4_n32,
 	"mipsel"		: arch_mipsel1,
 	"mipsel1"		: arch_mipsel1,
-	"mipsel2"		: arch_mipsel2,
 	"mipsel3"		: arch_mipsel3,
 	"mipsel3_n32"	: arch_mipsel3_n32,
 	"mipsel4"		: arch_mipsel4,
