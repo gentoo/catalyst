0131a93c7d95f8d27d79463750a1fdf471ba79b1
Rick Farina (Zero_Chaos) <zerochaos@gentoo.org>
2012-10-19 23:27:03 -0400

    prevent cleaning .a files
    The releng specs already remove all the static libs from the system
    We do not need to do it with catalyst as this breaks all other livecds

--- targets/livecd-stage2/livecd-stage2-controller.sh
+++ targets/livecd-stage2/livecd-stage2-controller.sh
@@ -109,13 +109,6 @@ case $1 in
 			# Zap all .pyc and .pyo files
 			find ${clst_chroot_path}/usr/lib* -iname "*.py[co]" -exec rm -f {} \;
 		fi
-		# Cleanup all .a files except libgcc.a, *_nonshared.a and
-		# /usr/lib/portage/bin/*.a
-		find ${clst_chroot_path}usr/lib* -type f -iname "*.a" | \
-			grep -v 'libgcc.a' | \
-			grep -v 'nonshared.a' | \
-			grep -v '/usr/lib/portage/bin/' | \
-			grep -v 'libgcc_eh.a' | xargs rm -f
 		rm -f ${clst_chroot_path}/tmp/packages.txt
 		;;
 
