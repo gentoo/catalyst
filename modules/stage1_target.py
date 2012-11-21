1f604c892da5e0d382976900f16b5f6a5fd51025
Jorge Manuel B. S. Vicetto (jmbsvicetto) <jmbsvicetto@gentoo.org>
2012-11-21 00:08:28 -0100

    Clean-up the dirs we remove on stage1 target (stop cleaning old non-existant python dirs).

--- modules/stage1_target.py
+++ modules/stage1_target.py
@@ -29,12 +29,7 @@ class stage1_target(generic_stage_target):
 	def set_cleanables(self):
 		generic_stage_target.set_cleanables(self)
 		self.settings["cleanables"].extend([\
-		"/usr/lib/python2.2/test", "/usr/lib/python2.2/email",\
-		"/usr/lib/python2.2/lib-tk", "/usr/lib/python2.3/test",\
-		"/usr/lib/python2.3/email", "/usr/lib/python2.3/lib-tk",\
-		"/usr/lib/python2.4/test", "/usr/lib/python2.4/email",\
-		"/usr/lib/python2.4/lib-tk", "/usr/share/zoneinfo",\
-		"/etc/portage/*"])
+		"/usr/share/zoneinfo", "/etc/portage/package*"])
 
 	# XXX: How do these override_foo() functions differ from the ones in generic_stage_target and why aren't they in stage3_target?
 
