e0837622ffcf7177f2b8089c6eabb225c777c71f
Raúl Porcel <armin76@gentoo.org>
2013-04-06 17:07:00 +0000

    Add support for m68k
    port commit for the rewrite branch.
    Brian Dolbec <dolsen@gentoo.org>

---
+++ catalyst/arch/m68k.py
@@ -0,0 +1,21 @@
+
+import os
+from catalyst import builder
+
+class generic_m68k(builder.generic):
+	"abstract base class for all m68k builders"
+	def __init__(self,myspec):
+		builder.generic.__init__(self,myspec)
+		self.settings["CHROOT"]="chroot"
+		self.settings["CFLAGS"]=" -pipe"
+
+class arch_m68k(generic_m68k):
+	"builder class for generic m68k"
+	def __init__(self,myspec):
+		generic_m68k.__init__(self,myspec)
+		self.settings["CFLAGS"]+=" -O2"
+		self.settings["CHOST"]="m68k-unknown-linux-gnu"
+
+def register():
+	"Inform main catalyst program of the contents of this plugin."
+	return ({ "m68k":arch_m68k }, ("m68k", ))
