gentoo/catalyst

commit 6c0a577deaf6cdfb188abe1ce2f263122eaa8935

Fix update_seed use by not using nor building binary packages during the seed update.

targets/support/chroot-functions.sh
@@ -133,7 +133,7 @@ setup_myemergeopts(){
 	then
 		export bootstrap_opts="${bootstrap_opts} -f"
 		export clst_myemergeopts="${clst_myemergeopts} -f"
-	elif [ -n "${clst_PKGCACHE}" ]
+	elif [ -n "${clst_PKGCACHE}" -a -z "${clst_update_seed}" ]
 	then
 		export clst_myemergeopts="${clst_myemergeopts} --usepkg --buildpkg --newuse"
 		export bootstrap_opts="${bootstrap_opts} -r"

targets/stage1/stage1-chroot.sh
@@ -30,12 +30,15 @@ if [ -n "${clst_update_seed}" ]; then
 		if [ -n "${clst_update_seed_command}" ]; then
 			clst_root_path=/ run_merge "--buildpkg=n ${clst_update_seed_command}"
 		else
-			clst_root_path=/ run_merge "--buildpkg=n --update --deep --newuse --onlydeps gcc"
+			clst_root_path=/ run_merge "--update --deep --newuse --complete-graph --rebuild-if-new-ver gcc"
 		fi
 	elif [ "${clst_update_seed}" != "no" ]; then
 		echo "Invalid setting for update_seed: ${clst_update_seed}"
 		exit 1
 	fi
+
+	# reset emerge options for the target
+	clst_update_seed=no setup_myemergeopts
 else
 	echo "Skipping seed stage update..."
 fi

commit 8324fc2ccaa796d6ba06e00d33facc6392d6b811

Fixes a bug inadvertently caused by 6c0a577 where setting update_seed to no would result in dropping the use and making of binpkgs while building stage1, which is undesirable.  The conditional was getting a bit unwieldly, so I added a comment explaining when it matches, which is especially helpful to those who don't know every single test flag.
Tested-By: Rick Farina <zerochaos@gentoo.org>
Tested-By: Brian Dolbec <dolsen@gentoo.org>

targets/support/chroot-functions.sh
@@ -133,7 +133,8 @@ setup_myemergeopts(){
 	then
 		export bootstrap_opts="${bootstrap_opts} -f"
 		export clst_myemergeopts="${clst_myemergeopts} -f"
-	elif [ -n "${clst_PKGCACHE}" -a -z "${clst_update_seed}" ]
+	# if we have PKGCACHE, and either update_seed is empty or 'no', make and use binpkgs
+	elif [ -n "${clst_PKGCACHE}" ] && [ -z "${clst_update_seed}" -o "${clst_update_seed}" = "no" ]
 	then
 		export clst_myemergeopts="${clst_myemergeopts} --usepkg --buildpkg --newuse"
 		export bootstrap_opts="${bootstrap_opts} -r"