gentoo/catalyst

commit 900554b0b067e76d3806e520357d91adbfa4fa0c

Make sure portage updates itself at the start of stage1 - thanks to Zac for noticing the issue.

targets/support/chroot-functions.sh
@@ -165,8 +165,9 @@ setup_pkgmgr(){
 	# We need to merge our package manager with USE="build" set in case it is
 	# portage to avoid frying our /etc/make.conf file.  Otherwise, we could
 	# just let emerge system could merge it.
+	# Use --update or portage won't reinstall the same version.
 	[ -e /etc/make.conf ] && echo 'USE="${USE} build"' >> /etc/make.conf
-	run_merge --oneshot --nodeps sys-apps/portage
+	run_merge --oneshot --nodeps --update sys-apps/portage
 	sed -i '/USE="${USE} build"/d' /etc/make.conf
 }
 

commit cbda895492e4c978b3d7b704538bce62c27a8faa

chroot-functions.sh: Fix a mis-worded comment
Fixes: 900554b0b067e76d3806e520357d91adbfa4fa0c
Author: Jorge Manuel B. S. Vicetto (jmbsvicetto) <jmbsvicetto@gentoo.org> (Mon 19 Dec 2011 11:14:05 PM PST)
subject: Make sure portage updates itself at the start of stage1 - thanks to Zac for noticing the issue.

targets/support/chroot-functions.sh
@@ -169,7 +169,7 @@ setup_pkgmgr(){
 	# We need to merge our package manager with USE="build" set in case it is
 	# portage to avoid frying our /etc/portage/make.conf file.  Otherwise, we could
 	# just let emerge @system could merge it.
-	# Use --update or portage won't reinstall the same version.
+	# Use --update or portage might just waste time/cycles and reinstall the same version.
 	[ -e /etc/portage/make.conf ] && echo 'USE="${USE} build"' >> /etc/portage/make.conf
 	run_merge --oneshot --update sys-apps/portage
 	sed -i '/USE="${USE} build"/d' /etc/portage/make.conf