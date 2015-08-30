8c395e8608549ae910fd09abdcd119e876e8dfdf
Jorge Manuel B. S. Vicetto (jmbsvicetto) <jmbsvicetto@gentoo.org>
2015-08-30 16:40:46 +0000

    We need to preserve bindist for later stages (if set). This will also need to be done to stage4 and netboot targets. This is temporary until we move to catalyst_build_use.
    Signed-off-by: Jorge Manuel B. S. Vicetto (jmbsvicetto) <jmbsvicetto@gentoo.org>

--- targets/stage3/stage3-chroot.sh
+++ targets/stage3/stage3-chroot.sh
@@ -5,4 +5,10 @@ source /tmp/chroot-functions.sh
 ## START BUILD
 setup_pkgmgr
 
+# Set bindist USE flag if clst_BINDIST is set
+[ -e ${clst_make_conf} ] && [ -n "${clst_BINDIST}" ] && echo "USE=\"${USE} bindist\"" >> ${clst_make_conf}
+
 run_merge "-e @system"
+
+# Clean-up USE again
+sed -i "/USE=\"${USE} bindist\"/d" ${clst_make_conf}
