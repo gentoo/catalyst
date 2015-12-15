00d5be7000d1591b0377e98d64e32cdea898c9e1
Brian Dolbec <dolsen@gentoo.org>
2015-12-15 09:02:01 -0800

    targets/stage4.py: Cleanup line continuations and spacing

--- catalyst/targets/stage4.py
+++ catalyst/targets/stage4.py
@@ -13,21 +13,21 @@ class stage4(StageBase):
 	def __init__(self,spec,addlargs):
 		self.required_values=["stage4/packages"]
 		self.valid_values=self.required_values[:]
-		self.valid_values.extend(["stage4/use","boot/kernel",\
-			"stage4/root_overlay","stage4/fsscript",\
-			"stage4/gk_mainargs","splash_theme",\
-			"portage_overlay","stage4/rcadd","stage4/rcdel",\
-			"stage4/linuxrc","stage4/unmerge","stage4/rm","stage4/empty"])
+		self.valid_values.extend(["stage4/use", "boot/kernel",
+			"stage4/root_overlay", "stage4/fsscript",
+			"stage4/gk_mainargs", "splash_theme",
+			"portage_overlay", "stage4/rcadd", "stage4/rcdel",
+			"stage4/linuxrc", "stage4/unmerge", "stage4/rm", "stage4/empty"])
 		StageBase.__init__(self,spec,addlargs)
 
 	def set_cleanables(self):
 		self.settings["cleanables"]=["/var/tmp/*","/tmp/*"]
 
 	def set_action_sequence(self):
-		self.settings["action_sequence"]=["unpack","unpack_snapshot",\
-			"config_profile_link","setup_confdir","portage_overlay",\
-			"bind","chroot_setup","setup_environment","build_packages",\
-			"build_kernel","bootloader","root_overlay","fsscript",\
-			"preclean","rcupdate","unmerge","unbind","remove","empty",\
+		self.settings["action_sequence"] = ["unpack", "unpack_snapshot",
+			"config_profile_link", "setup_confdir", "portage_overlay",
+			"bind", "chroot_setup", "setup_environment", "build_packages",
+			"build_kernel", "bootloader", "root_overlay", "fsscript",
+			"preclean", "rcupdate", "unmerge", "unbind", "remove", "empty",
 			"clean"]
 		self.set_completion_action_sequences()
