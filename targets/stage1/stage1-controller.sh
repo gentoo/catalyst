9b8a61e03e3062335a369cd34146cd26eb675c1b
Rick Farina (Zero_Chaos) <zerochaos@gentoo.org>
2016-09-14 12:00:18 -0400

    quotes to make shellcheck happy

--- targets/stage1/stage1-controller.sh
+++ targets/stage1/stage1-controller.sh
@@ -1,29 +1,29 @@
 #!/bin/bash
 
-source ${clst_shdir}/support/functions.sh
+source "${clst_shdir}/support/functions.sh"
 
-case $1 in
+case "$1" in
 	enter)
 	;;
 
 	run)
-		cp ${clst_shdir}/stage1/build.py ${clst_chroot_path}/tmp
+		cp "${clst_shdir}/stage1/build.py" "${clst_chroot_path}/tmp"
 
 		# Setup "ROOT in chroot" dir
-		install -d ${clst_chroot_path}/${clst_root_path}/etc
-		install -d ${clst_chroot_path}/${clst_root_path}${clst_port_conf}
+		install -d "${clst_chroot_path}/${clst_root_path}/etc"
+		install -d "${clst_chroot_path}/${clst_root_path}${clst_port_conf}"
 
 		# Setup make.conf and make.profile link in "ROOT in chroot":
-		copy_to_chroot ${clst_chroot_path}${clst_make_conf} ${clst_root_path}${clst_port_conf}
+		copy_to_chroot "${clst_chroot_path}${clst_make_conf}" "${clst_root_path}${clst_port_conf}"
 
 		# Enter chroot, execute our build script
 		exec_in_chroot \
-			${clst_shdir}/${clst_target}/${clst_target}-chroot.sh \
+			"${clst_shdir}/${clst_target}/${clst_target}-chroot.sh" \
 			|| exit 1
 	;;
 
 	preclean)
-		exec_in_chroot ${clst_shdir}/${clst_target}/${clst_target}-preclean-chroot.sh || exit 1
+		exec_in_chroot "${clst_shdir}/${clst_target}/${clst_target}-preclean-chroot.sh" || exit 1
 	;;
 
 	clean)
