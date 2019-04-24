cfc08db3e0c5bf8c163b843e8b48e3774e4f2582
Andreas K. Hüttel <dilfridge@gentoo.org>
2019-04-25 00:34:02 +0200

    Add builder for riscv64-unknown-linux-gnu
    Signed-off-by: Andreas K. Hüttel <dilfridge@gentoo.org>
    Signed-off-by: Matt Turner <mattst88@gentoo.org>

---
+++ catalyst/arch/riscv.py
@@ -0,0 +1,13 @@
+
+from catalyst import builder
+
+class arch_riscv(builder.generic):
+	"builder class for riscv"
+	def __init__(self,myspec):
+		builder.generic.__init__(self,myspec)
+		self.settings["COMMON_FLAGS"]="-O2 -pipe"
+		self.settings["CHOST"]="riscv64-unknown-linux-gnu"
+
+def register():
+	"Inform main catalyst program of the contents of this plugin."
+	return ({ "riscv":arch_riscv }, ("rv64","riscv64","riscv"))
