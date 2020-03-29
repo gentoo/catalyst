gentoo/catalyst

commit 8441372c285726de85c71a83035a9e11cc47f037

fix /var/tmp/portage not being forced into a tmpfs
2e91db10 accidently revealed that src tmpfs was being being used when we
meant "this might be a tmpfs, or it might not".  Fix preexisting code to
be more readable and adjust src tmpfs to be what a sane person would
expect

Signed-off-by: Rick Farina (Zero_Chaos) <zerochaos@gentoo.org>

catalyst/base/stagebase.py
@@ -973,13 +973,13 @@ class StageBase(TargetBase, ClearBase, GenBase):
 			if "snapcache" in self.settings["options"] and x == "portdir":
 				self.snapcache_lock.read_lock()
 			_cmd = None
-			if src == "tmpfs":
+			if src == "maybe_tmpfs":
 				if "var_tmpfs_portage" in self.settings:
 					_cmd = ['mount', '-t', 'tmpfs',
 						'-o', 'size=' + self.settings['var_tmpfs_portage'] + 'G',
 						src, target]
-				else:
-					_cmd = ['mount', '-t', 'tmpfs', src, target]
+			elif src == "tmpfs":
+				_cmd = ['mount', '-t', 'tmpfs', src, target]
 			else:
 				if os.uname()[0] == "FreeBSD":
 					if src == "/dev":

catalyst/defaults.py
@@ -99,7 +99,7 @@ SOURCE_MOUNT_DEFAULTS = {
 	"devpts": "/dev/pts",
 	"distdir": DISTDIR[:],
 	"portdir": PORTDIR[:],
-	"port_tmpdir": "tmpfs",
+	"port_tmpdir": "maybe_tmpfs",
 	"proc": "/proc",
 	"shm": "shmfs",
 	"run": "tmpfs",

commit a4f0612d4162d937652c34200e3de0925d113e05

catalyst: Skip "maybe_tmpfs" directory creation
Fixes: 8441372c2857 (fix /var/tmp/portage not being forced into a tmpfs)
Signed-off-by: Matt Turner <mattst88@gentoo.org>

catalyst/base/stagebase.py
@@ -963,7 +963,7 @@ class StageBase(TargetBase, ClearBase, GenBase):
 			ensure_dirs(target, mode=0o755)
 
 			if not os.path.exists(self.mountmap[x]):
-				if self.mountmap[x] not in ["tmpfs", "shmfs"]:
+				if self.mountmap[x] not in ("maybe_tmpfs", "tmpfs", "shmfs"):
 					ensure_dirs(self.mountmap[x], mode=0o755)
 
 			src = self.mountmap[x]