gentoo/catalyst

commit be6d7f0b7f0a36d4bbfa30704dbccf6bd76f5754

use profile for CPU_FLAGS_X86 where appropriate
a number of the CPU_FLAGS_X86 settings appeared to be trying to use something
very much like the default from the profile but missing mmxext.  Due to this,
a number of packages like ffmpeg would be broken, where using the profile is
better.

Modified to remove (comment out) the ones that appeared to be attempting to
use the default (and failing) while leaving in the settings which are intentionally
disabling certain unavailable features per sub-arch.

catalyst/arch/amd64.py
@@ -13,7 +13,8 @@ class arch_amd64(generic_amd64):
 		generic_amd64.__init__(self,myspec)
 		self.settings["CFLAGS"]="-O2 -pipe"
 		self.settings["CHOST"]="x86_64-pc-linux-gnu"
-		self.settings["HOSTUSEEXPAND"]={"CPU_FLAGS_X86":["mmx","sse","sse2"]}
+		#lines like this appears to be trying to set the profile default, better to use the profile
+		#self.settings["HOSTUSEEXPAND"]={"CPU_FLAGS_X86":["mmx","sse","sse2"]}
 
 class arch_nocona(generic_amd64):
 	"improved version of Intel Pentium 4 CPU with 64-bit extensions, MMX, SSE, SSE2 and SSE3 support"
@@ -21,7 +22,7 @@ class arch_nocona(generic_amd64):
 		generic_amd64.__init__(self,myspec)
 		self.settings["CFLAGS"]="-O2 -march=nocona -pipe"
 		self.settings["CHOST"]="x86_64-pc-linux-gnu"
-		self.settings["HOSTUSEEXPAND"]={"CPU_FLAGS_X86":["mmx","sse","sse2"]}
+		#self.settings["HOSTUSEEXPAND"]={"CPU_FLAGS_X86":["mmx","sse","sse2"]}
 
 # Requires gcc 4.3 to use this class
 class arch_core2(generic_amd64):
@@ -30,7 +31,7 @@ class arch_core2(generic_amd64):
 		generic_amd64.__init__(self,myspec)
 		self.settings["CFLAGS"]="-O2 -march=core2 -pipe"
 		self.settings["CHOST"]="x86_64-pc-linux-gnu"
-		self.settings["HOSTUSEEXPAND"]={"CPU_FLAGS_X86":["mmx","sse","sse2","ssse3"]}
+		#self.settings["HOSTUSEEXPAND"]={"CPU_FLAGS_X86":["mmx","sse","sse2","ssse3"]}
 
 class arch_k8(generic_amd64):
 	"generic k8, opteron and athlon64 support"
@@ -38,7 +39,7 @@ class arch_k8(generic_amd64):
 		generic_amd64.__init__(self,myspec)
 		self.settings["CFLAGS"]="-O2 -march=k8 -pipe"
 		self.settings["CHOST"]="x86_64-pc-linux-gnu"
-		self.settings["HOSTUSEEXPAND"]={"CPU_FLAGS_X86":["mmx","sse","sse2","3dnow"]}
+		#self.settings["HOSTUSEEXPAND"]={"CPU_FLAGS_X86":["mmx","sse","sse2","3dnow"]}
 
 class arch_k8_sse3(generic_amd64):
 	"improved versions of k8, opteron and athlon64 with SSE3 support"
@@ -46,7 +47,7 @@ class arch_k8_sse3(generic_amd64):
 		generic_amd64.__init__(self,myspec)
 		self.settings["CFLAGS"]="-O2 -march=k8-sse3 -pipe"
 		self.settings["CHOST"]="x86_64-pc-linux-gnu"
-		self.settings["HOSTUSEEXPAND"]={"CPU_FLAGS_X86":["mmx","sse","sse2","3dnow"]}
+		#self.settings["HOSTUSEEXPAND"]={"CPU_FLAGS_X86":["mmx","sse","sse2","3dnow"]}
 
 class arch_amdfam10(generic_amd64):
 	"AMD Family 10h core based CPUs with x86-64 instruction set support"
@@ -54,7 +55,7 @@ class arch_amdfam10(generic_amd64):
 		generic_amd64.__init__(self,myspec)
 		self.settings["CFLAGS"]="-O2 -march=amdfam10 -pipe"
 		self.settings["CHOST"]="x86_64-pc-linux-gnu"
-		self.settings["HOSTUSEEXPAND"]={"CPU_FLAGS_X86":["mmx","sse","sse2","3dnow"]}
+		#self.settings["HOSTUSEEXPAND"]={"CPU_FLAGS_X86":["mmx","sse","sse2","3dnow"]}
 
 class arch_x32(generic_amd64):
 	"builder class for generic x32 (Intel and AMD)"
@@ -62,7 +63,7 @@ class arch_x32(generic_amd64):
 		generic_amd64.__init__(self,myspec)
 		self.settings["CFLAGS"]="-O2 -pipe"
 		self.settings["CHOST"]="x86_64-pc-linux-gnux32"
-		self.settings["HOSTUSEEXPAND"]={"CPU_FLAGS_X86":["mmx","sse","sse2"]}
+		#self.settings["HOSTUSEEXPAND"]={"CPU_FLAGS_X86":["mmx","sse","sse2"]}
 
 def register():
 	"inform main catalyst program of the contents of this plugin"

catalyst/arch/x86.py
@@ -78,21 +78,22 @@ class arch_pentium4(generic_x86):
 	def __init__(self,myspec):
 		generic_x86.__init__(self,myspec)
 		self.settings["CFLAGS"]="-O2 -march=pentium4 -pipe"
-		self.settings["HOSTUSEEXPAND"]={"CPU_FLAGS_X86":["mmx","sse","sse2"]}
+		#lines like this appears to be trying to set the profile default, better to use the profile
+		#self.settings["HOSTUSEEXPAND"]={"CPU_FLAGS_X86":["mmx","sse","sse2"]}
 
 class arch_pentium_m(generic_x86):
 	"Intel Pentium M CPU with MMX, SSE and SSE2 support"
 	def __init__(self,myspec):
 		generic_x86.__init__(self,myspec)
 		self.settings["CFLAGS"]="-O2 -march=pentium-m -pipe"
-		self.settings["HOSTUSEEXPAND"]={"CPU_FLAGS_X86":["mmx","sse","sse2"]}
+		#self.settings["HOSTUSEEXPAND"]={"CPU_FLAGS_X86":["mmx","sse","sse2"]}
 
 class arch_prescott(generic_x86):
 	"improved version of Intel Pentium 4 CPU with MMX, SSE, SSE2 and SSE3 support"
 	def __init__(self,myspec):
 		generic_x86.__init__(self,myspec)
 		self.settings["CFLAGS"]="-O2 -march=prescott -pipe"
-		self.settings["HOSTUSEEXPAND"]={"CPU_FLAGS_X86":["mmx","sse","sse2"]}
+		#self.settings["HOSTUSEEXPAND"]={"CPU_FLAGS_X86":["mmx","sse","sse2"]}
 		self.settings["CHOST"]="i686-pc-linux-gnu"
 
 class arch_k6(generic_x86):

commit 391ec5099ee6d0c678943b70641d2b3e3c82ced4

catalyst: Enable and fix CPU_FLAGS_X86
In commit be6d7f0b7f0a (use profile for CPU_FLAGS_X86 where appropriate)
we disabled setting CPU_FLAGS_X86 to avoid overriding the profile's
defaults (and in some cases, overriding incorrectly).

While that is undoubtedly the correct thing to do for the generic
classes (e.g., arch_amd64) it does not make sense for the classes
specific to a particular microarchitecture, unless we expect the
defaults of CPU_FLAGS_X86 to change in the future, which seems highly
unlikely.

It also is the wrong thing to do for x86, which does not have a default
CPU_FLAGS_X86.

Fixes: be6d7f0b7f0a (use profile for CPU_FLAGS_X86 where appropriate)
Signed-off-by: Matt Turner <mattst88@gentoo.org>

catalyst/arch/amd64.py
@@ -11,51 +11,47 @@ class arch_amd64(generic_amd64):
 	def __init__(self,myspec):
 		generic_amd64.__init__(self,myspec)
 		self.settings["COMMON_FLAGS"]="-O2 -pipe"
-		#lines like this appears to be trying to set the profile default, better to use the profile
-		#self.settings["HOSTUSEEXPAND"]={"CPU_FLAGS_X86":["mmx","sse","sse2"]}
 
 class arch_nocona(generic_amd64):
 	"improved version of Intel Pentium 4 CPU with 64-bit extensions, MMX, SSE, SSE2 and SSE3 support"
 	def __init__(self,myspec):
 		generic_amd64.__init__(self,myspec)
 		self.settings["COMMON_FLAGS"]="-O2 -march=nocona -pipe"
-		#self.settings["HOSTUSEEXPAND"]={"CPU_FLAGS_X86":["mmx","sse","sse2"]}
+		self.settings["HOSTUSEEXPAND"]={"CPU_FLAGS_X86":["mmx","mmxext","sse","sse2","sse3"]}
 
-# Requires gcc 4.3 to use this class
 class arch_core2(generic_amd64):
 	"Intel Core 2 CPU with 64-bit extensions, MMX, SSE, SSE2, SSE3 and SSSE3 support"
 	def __init__(self,myspec):
 		generic_amd64.__init__(self,myspec)
 		self.settings["COMMON_FLAGS"]="-O2 -march=core2 -pipe"
-		#self.settings["HOSTUSEEXPAND"]={"CPU_FLAGS_X86":["mmx","sse","sse2","ssse3"]}
+		self.settings["HOSTUSEEXPAND"]={"CPU_FLAGS_X86":["mmx","mmxext","sse","sse2","sse3","ssse3"]}
 
 class arch_k8(generic_amd64):
 	"generic k8, opteron and athlon64 support"
 	def __init__(self,myspec):
 		generic_amd64.__init__(self,myspec)
 		self.settings["COMMON_FLAGS"]="-O2 -march=k8 -pipe"
-		#self.settings["HOSTUSEEXPAND"]={"CPU_FLAGS_X86":["mmx","sse","sse2","3dnow"]}
+		self.settings["HOSTUSEEXPAND"]={"CPU_FLAGS_X86":["mmx","mmxext","3dnow","3dnowext","sse","sse2"]}
 
 class arch_k8_sse3(generic_amd64):
 	"improved versions of k8, opteron and athlon64 with SSE3 support"
 	def __init__(self,myspec):
 		generic_amd64.__init__(self,myspec)
 		self.settings["COMMON_FLAGS"]="-O2 -march=k8-sse3 -pipe"
-		#self.settings["HOSTUSEEXPAND"]={"CPU_FLAGS_X86":["mmx","sse","sse2","3dnow"]}
+		self.settings["HOSTUSEEXPAND"]={"CPU_FLAGS_X86":["mmx","mmxext","3dnow","3dnowext","sse","sse2","sse3"]}
 
 class arch_amdfam10(generic_amd64):
 	"AMD Family 10h core based CPUs with x86-64 instruction set support"
 	def __init__(self,myspec):
 		generic_amd64.__init__(self,myspec)
 		self.settings["COMMON_FLAGS"]="-O2 -march=amdfam10 -pipe"
-		#self.settings["HOSTUSEEXPAND"]={"CPU_FLAGS_X86":["mmx","sse","sse2","3dnow"]}
+		self.settings["HOSTUSEEXPAND"]={"CPU_FLAGS_X86":["mmx","mmxext","3dnow","3dnowext","sse","sse2","sse3","sse4a"]}
 
 class arch_x32(generic_amd64):
 	"builder class for generic x32 (Intel and AMD)"
 	def __init__(self,myspec):
 		generic_amd64.__init__(self,myspec)
 		self.settings["COMMON_FLAGS"]="-O2 -pipe"
-		#self.settings["HOSTUSEEXPAND"]={"CPU_FLAGS_X86":["mmx","sse","sse2"]}
 
 def register():
 	"inform main catalyst program of the contents of this plugin"

catalyst/arch/x86.py
@@ -63,29 +63,28 @@ class arch_pentium3(generic_x86):
 	def __init__(self,myspec):
 		generic_x86.__init__(self,myspec)
 		self.settings["COMMON_FLAGS"]="-O2 -march=pentium3 -pipe"
-		self.settings["HOSTUSEEXPAND"]={"CPU_FLAGS_X86":["mmx","sse"]}
+		self.settings["HOSTUSEEXPAND"]={"CPU_FLAGS_X86":["mmx","mmxext","sse"]}
 
 class arch_pentium4(generic_x86):
 	"Intel Pentium 4 CPU with MMX, SSE and SSE2 support"
 	def __init__(self,myspec):
 		generic_x86.__init__(self,myspec)
 		self.settings["COMMON_FLAGS"]="-O2 -march=pentium4 -pipe"
-		#lines like this appears to be trying to set the profile default, better to use the profile
-		#self.settings["HOSTUSEEXPAND"]={"CPU_FLAGS_X86":["mmx","sse","sse2"]}
+		self.settings["HOSTUSEEXPAND"]={"CPU_FLAGS_X86":["mmx","mmxext","sse","sse2"]}
 
 class arch_pentium_m(generic_x86):
 	"Intel Pentium M CPU with MMX, SSE and SSE2 support"
 	def __init__(self,myspec):
 		generic_x86.__init__(self,myspec)
 		self.settings["COMMON_FLAGS"]="-O2 -march=pentium-m -pipe"
-		#self.settings["HOSTUSEEXPAND"]={"CPU_FLAGS_X86":["mmx","sse","sse2"]}
+		self.settings["HOSTUSEEXPAND"]={"CPU_FLAGS_X86":["mmx","mmxext","sse","sse2"]}
 
 class arch_prescott(generic_x86):
 	"improved version of Intel Pentium 4 CPU with MMX, SSE, SSE2 and SSE3 support"
 	def __init__(self,myspec):
 		generic_x86.__init__(self,myspec)
 		self.settings["COMMON_FLAGS"]="-O2 -march=prescott -pipe"
-		#self.settings["HOSTUSEEXPAND"]={"CPU_FLAGS_X86":["mmx","sse","sse2"]}
+		self.settings["HOSTUSEEXPAND"]={"CPU_FLAGS_X86":["mmx","mmxext","sse","sse2","sse3"]}
 
 class arch_k6(generic_x86):
 	"AMD K6 CPU with MMX support"
@@ -106,14 +105,14 @@ class arch_athlon(generic_x86):
 	def __init__(self,myspec):
 		generic_x86.__init__(self,myspec)
 		self.settings["COMMON_FLAGS"]="-O2 -march=athlon -pipe"
-		self.settings["HOSTUSEEXPAND"]={"CPU_FLAGS_X86":["mmx","3dnow"]}
+		self.settings["HOSTUSEEXPAND"]={"CPU_FLAGS_X86":["mmx","mmxext","3dnow","3dnowext"]}
 
 class arch_athlon_xp(generic_x86):
 	"improved AMD Athlon CPU with MMX, 3DNow!, Enhanced 3DNow! and full SSE support"
 	def __init__(self,myspec):
 		generic_x86.__init__(self,myspec)
 		self.settings["COMMON_FLAGS"]="-O2 -march=athlon-xp -pipe"
-		self.settings["HOSTUSEEXPAND"]={"CPU_FLAGS_X86":["mmx","3dnow","sse"]}
+		self.settings["HOSTUSEEXPAND"]={"CPU_FLAGS_X86":["mmx","mmxext","3dnow","3dnowext","sse"]}
 
 def register():
 	"Inform main catalyst program of the contents of this plugin."