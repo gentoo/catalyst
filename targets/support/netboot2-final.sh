gentoo/catalyst

commit 23cd0ed7dc8ab4483bd34810d52309e90de5723a

netboot2.sh: Fix missing slashes in paths

targets/support/netboot2-final.sh
@@ -4,20 +4,20 @@ source ${clst_shdir}/support/functions.sh
 source ${clst_shdir}/support/filesystem-functions.sh
 
 
-extract_kernels ${clst_target_path}boot
+extract_kernels ${clst_target_path}/boot
 
-# Move kernel binaries to ${clst_target_path}kernels, and
-# move everything else to ${clst_target_path}kernels/misc
-mkdir ${clst_target_path}kernels
-mkdir ${clst_target_path}kernels/misc
+# Move kernel binaries to ${clst_target_path}/kernels, and
+# move everything else to ${clst_target_path}/kernels/misc
+mkdir ${clst_target_path}/kernels
+mkdir ${clst_target_path}/kernels/misc
 
 for x in ${clst_boot_kernel}; do
-	mv ${clst_target_path}boot/${x} ${clst_target_path}kernels
-	mv ${clst_target_path}boot/${x}.igz ${clst_target_path}kernels/misc
-	mv ${clst_target_path}boot/System.map* ${clst_target_path}kernels/misc/System.map-${x}
+	mv ${clst_target_path}/boot/${x} ${clst_target_path}/kernels
+	mv ${clst_target_path}/boot/${x}.igz ${clst_target_path}/kernels/misc
+	mv ${clst_target_path}/boot/System-${x}.map ${clst_target_path}/kernels/misc
 done
 
-rmdir ${clst_target_path}boot
+rmdir ${clst_target_path}/boot
 
 # Any post-processing necessary for each architecture can be done here.  This
 # may include things like sparc's elftoaout, x86's PXE boot, etc.
@@ -51,7 +51,7 @@ case ${clst_hostarch} in
 		fi
 		for x in ${clst_boot_kernel}; do
 			elftoaout ${clst_target_path}/kernels/${x} -o ${clst_target_path}/${x}-a.out
-			${piggyback} ${clst_target_path}/${x}-a.out ${clst_target_path}/kernels/misc/System-${x}.map ${clst_target_path}/kernels/misc/${x}.igz
+			${piggyback} ${clst_target_path}/${x}-a.out ${clst_target_path}/kernels/misc/System.map-${x} ${clst_target_path}/kernels/misc/${x}.igz
 		done
 		;;
 	ia64)

commit 46322490b543105aa561933d678368082ccc096d

targets: Fix expected System.map name
This fixes a regression that broke netboot2 on SPARC.

Fixes: 23cd0ed7dc8a (netboot2.sh: Fix missing slashes in paths)
Signed-off-by: Matt Turner <mattst88@gentoo.org>

targets/support/netboot2-final.sh
@@ -50,7 +50,7 @@ case ${clst_hostarch} in
 		fi
 		for x in ${clst_boot_kernel}; do
 			elftoaout ${clst_target_path}/kernels/${x} -o ${clst_target_path}/${x}-a.out
-			${piggyback} ${clst_target_path}/${x}-a.out ${clst_target_path}/kernels/misc/System.map-${x} ${clst_target_path}/kernels/misc/${x}.igz
+			${piggyback} ${clst_target_path}/${x}-a.out ${clst_target_path}/kernels/misc/System-${x}.map ${clst_target_path}/kernels/misc/${x}.igz
 		done
 		;;
 	ia64)