gentoo/catalyst

commit b28b3ce7a4aca0cd79236133fbe69b8769c0c194

targets: Use mkdir -p
Signed-off-by: Matt Turner <mattst88@gentoo.org>

targets/support/bootloader-setup.sh
@@ -291,9 +291,7 @@ case ${clst_hostarch} in
 		# CD image, and then pass these components to the
 		# `sgibootcd` tool which outputs a final CD image
 		scratch="${1}"
-		[ ! -d "${scratch}/kernels" ] && mkdir ${scratch}/kernels
-		[ ! -d "${scratch}/kernels/misc" ] && mkdir ${scratch}/kernels/misc
-		[ ! -d "${scratch}/arcload" ] && mkdir ${scratch}/arcload
+		mkdir -p mkdir ${scratch}/{kernels/misc,arcload}
 		echo "" > ${scratch}/arc.cf
 
 		# Move kernel binaries to ${scratch}/kernels, and

commit e41b9e40c8d20fab3be2b9a1c071cefbf37341c0

targets: Fix copy-n-paste mistake
Fixes: b28b3ce7a4ac (targets: Use mkdir -p)
Signed-off-by: Matt Turner <mattst88@gentoo.org>

targets/support/bootloader-setup.sh
@@ -267,7 +267,7 @@ case ${clst_hostarch} in
 		# CD image, and then pass these components to the
 		# `sgibootcd` tool which outputs a final CD image
 		scratch="${1}"
-		mkdir -p mkdir ${scratch}/{kernels/misc,arcload}
+		mkdir -p ${scratch}/{kernels/misc,arcload}
 		echo "" > ${scratch}/arc.cf
 
 		# Move kernel binaries to ${scratch}/kernels, and