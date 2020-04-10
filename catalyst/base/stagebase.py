gentoo/catalyst

commit c3b572d0ed5ebf29017da5e894e98fe9ab150126

catalyst: Automatically specify --b2sum if <target>/verify is set
Signed-off-by: Matt Turner <mattst88@gentoo.org>

catalyst/base/stagebase.py
@@ -672,10 +672,18 @@ class StageBase(TargetBase, ClearBase, GenBase):
 							[addlargs["boot/kernel/" + x + "/packages"]]
 
 	def set_build_kernel_vars(self):
-		if self.settings["spec_prefix"] + "/gk_mainargs" in self.settings:
-			self.settings["gk_mainargs"] = \
-				self.settings[self.settings["spec_prefix"] + "/gk_mainargs"]
-			del self.settings[self.settings["spec_prefix"] + "/gk_mainargs"]
+		prefix = self.settings["spec_prefix"]
+
+		gk_mainargs = prefix + "/gk_mainargs"
+		if gk_mainargs in self.settings:
+			self.settings["gk_mainargs"] = self.settings[gk_mainargs]
+			del self.settings[gk_mainargs]
+
+		# Ask genkernel to include b2sum if <target>/verify is set
+		verify = prefix + "/verify"
+		if verify in self.settings:
+			assert self.settings[verify] == "blake2"
+			self.settings.setdefault("gk_mainargs", "").append(" --b2sum")
 
 	def kill_chroot_pids(self):
 		log.info('Checking for processes running in chroot and killing them.')

commit 4b430dc36fdb7fdecb768f617b67fab1baacb9cd

catalyst: Fix appending --b2sum
I fixed this locally on my testing machine and failed to sync the
changes back.

Fixes: c3b572d0ed5e (catalyst: Automatically specify --b2sum ...)
Signed-off-by: Matt Turner <mattst88@gentoo.org>

catalyst/base/stagebase.py
@@ -683,7 +683,8 @@ class StageBase(TargetBase, ClearBase, GenBase):
 		verify = prefix + "/verify"
 		if verify in self.settings:
 			assert self.settings[verify] == "blake2"
-			self.settings.setdefault("gk_mainargs", "").append(" --b2sum")
+			self.settings.setdefault("gk_mainargs", "")
+			self.settings["gk_mainargs"] += " --b2sum"
 
 	def kill_chroot_pids(self):
 		log.info('Checking for processes running in chroot and killing them.')