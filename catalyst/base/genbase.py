46e6f497dcd0c21cd5197e5d9cf1ef6e196af10a
Matt Turner <mattst88@gentoo.org>
2020-04-16 22:37:42 -0700

    catalyst: Generate array outside of loop
    I'm going to change all of this code anyway, but I had to make this a
    separate commit since I was so flabbergasted by the absurdity of it.
    After digging through the history across multiple renames, I found that
    this mistake was introduced in 2006.
    
    Fixes: d128d2a554b0 (digests function uses raw output from hash function now)
    Signed-off-by: Matt Turner <mattst88@gentoo.org>

--- catalyst/base/genbase.py
+++ catalyst/base/genbase.py
@@ -22,7 +22,7 @@ class GenBase():
                     keys = {}
                     for i in self.settings["contents"].split():
                         keys[i] = 1
-                        array = sorted(keys.keys())
+                    array = sorted(keys.keys())
                     for j in array:
                         contents = contents_map.contents(path, j,
                                                          verbose=self.settings["VERBOSE"])
@@ -40,7 +40,7 @@ class GenBase():
                     keys = {}
                     for i in self.settings["digests"].split():
                         keys[i] = 1
-                        array = sorted(keys.keys())
+                    array = sorted(keys.keys())
                     for f in [path, path + '.CONTENTS']:
                         if os.path.exists(f):
                             if "all" in array:
