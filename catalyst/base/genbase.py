gentoo/catalyst

commit bb21b8615e64cb31fa9aa9d533ef328dc1374e45

catalyst: gzip the .CONTENTS file
Other algorithms give better compression ratios, but the difference is
not meaningful for a 2MiB text file. In my testing bzip2 gave a better
compression ratio of 15:1 vs gzip's 11:1, but that ends up being only a
size difference of 50KiB (136 vs 187) which is only an additional 2.5%
savings from the uncompressed input.

Choose gzip because transparent decompression is widely supported by web
servers and clients.

Closes: https://bugs.gentoo.org/630284
Signed-off-by: Matt Turner <mattst88@gentoo.org>

catalyst/base/genbase.py
@@ -2,6 +2,7 @@
 import hashlib
 import io
 import os
+import gzip
 
 class GenBase():
     """
@@ -28,7 +29,7 @@ class GenBase():
     def gen_contents_file(self, path):
         c = self.settings['contents_map']
 
-        with io.open(path + '.CONTENTS', 'w', encoding='utf-8') as file:
+        with gzip.open(path + '.CONTENTS.gz', 'w', encoding='utf-8') as file:
             file.write(c.contents(path, '', verbose=self.settings['VERBOSE']))
 
     def gen_digest_file(self, path):
@@ -36,6 +37,6 @@ class GenBase():
             return
 
         with io.open(path + '.DIGESTS', 'w', encoding='utf-8') as file:
-            for f in [path, path + '.CONTENTS']:
+            for f in [path, path + '.CONTENTS.gz']:
                 for i in self.settings['digests']:
                     file.write(self.generate_hash(f, name=i))

catalyst/support.py
@@ -65,7 +65,7 @@ def file_check(filepath, extensions=None, strict=True):
     files = glob.glob("%s.*" % filepath)
     # remove any false positive files
     files = [x for x in files if not x.endswith(
-        ".CONTENTS") and not x.endswith(".DIGESTS")]
+        ".CONTENTS") and not x.endswith(".CONTENTS.gz") and not x.endswith(".DIGESTS")]
     if len(files) == 1:
         return files[0]
     if len(files) > 1 and strict:

commit 060df92357aed97af955aa36caf47e8518b01e05

catalyst: Open CONTENTS file in text mode
Fixes: bb21b8615e64 (catalyst: gzip the .CONTENTS file)
Signed-off-by: Matt Turner <mattst88@gentoo.org>

catalyst/base/genbase.py
@@ -29,7 +29,7 @@ class GenBase():
     def gen_contents_file(self, path):
         c = self.settings['contents_map']
 
-        with gzip.open(path + '.CONTENTS.gz', 'w', encoding='utf-8') as file:
+        with gzip.open(path + '.CONTENTS.gz', 'wt', encoding='utf-8') as file:
             file.write(c.contents(path, '', verbose=self.settings['VERBOSE']))
 
     def gen_digest_file(self, path):