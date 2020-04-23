gentoo/catalyst

commit ddc1b50d47ffde9b9bbceb83ce0b5a6cb7045e1b

catalyst: Simplify config_profile_link()
I see no use in making this a resume point since making a symlink is a
cheap operation.

Signed-off-by: Matt Turner <mattst88@gentoo.org>

catalyst/base/stagebase.py
@@ -3,6 +3,9 @@ import os
 import platform
 import shutil
 import sys
+
+from pathlib import Path
+
 import toml
 
 from snakeoil import fileutils
@@ -902,24 +905,16 @@ class StageBase(TargetBase, ClearBase, GenBase):
                 self.snapcache_lock.unlock()
 
     def config_profile_link(self):
-        if "autoresume" in self.settings["options"] \
-                and self.resume.is_enabled("config_profile_link"):
-            log.notice(
-                'Resume point detected, skipping config_profile_link operation...')
-        else:
-            # TODO: zmedico and I discussed making this a directory and pushing
-            # in a parent file, as well as other user-specified configuration.
-            log.info('Configuring profile link...')
-            clear_path(self.settings['chroot_path'] +
-                       self.settings['port_conf'] + '/make.profile')
-            ensure_dirs(self.settings['chroot_path'] +
-                        self.settings['port_conf'])
-            cmd(['ln', '-sf',
-                 '../..' + self.settings['portdir'] +
-                 '/profiles/' + self.settings['target_profile'],
-                 self.settings['chroot_path'] + self.settings['port_conf'] + '/make.profile'],
-                env=self.env)
-            self.resume.enable("config_profile_link")
+        log.info('Configuring profile link...')
+        make_profile = Path(self.settings['chroot_path'],
+                            self.settings['port_conf'],
+                            'make.profile')
+        make_profile.unlink()
+        make_profile.symlink_to(Path('../..',
+                                     self.settings['portdir'],
+                                     'profiles',
+                                     self.settings['target_profile']),
+                                target_is_directory=True)
 
     def setup_confdir(self):
         if "autoresume" in self.settings["options"] \

commit 992cf27d631b064a612c203169f1bc2446e6c6fc

catalyst: Fix Path() usage
When an Path argument has a leading /, all previous arguments are
ignored. This is surprising behavior to me...

Fixes: ddc1b50d47ff (catalyst: Simplify config_profile_link())
Signed-off-by: Matt Turner <mattst88@gentoo.org>

catalyst/base/stagebase.py
@@ -906,12 +906,10 @@ class StageBase(TargetBase, ClearBase, GenBase):
 
     def config_profile_link(self):
         log.info('Configuring profile link...')
-        make_profile = Path(self.settings['chroot_path'],
-                            self.settings['port_conf'],
+        make_profile = Path(self.settings['chroot_path'] + self.settings['port_conf'],
                             'make.profile')
         make_profile.unlink()
-        make_profile.symlink_to(Path('../..',
-                                     self.settings['portdir'],
+        make_profile.symlink_to(Path('../..' + self.settings['portdir'],
                                      'profiles',
                                      self.settings['target_profile']),
                                 target_is_directory=True)