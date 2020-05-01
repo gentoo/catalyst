df366b28049fd58f4371942cb53279f03b771855
Matt Turner <mattst88@gentoo.org>
2020-04-30 23:40:26 -0700

    targets: Inline filesystem-functions.sh
    Inline create_squashfs and create_jffs2 since they're called only from
    one place (in a different file).
    
    Signed-off-by: Matt Turner <mattst88@gentoo.org>

--- targets/support/target_image_setup.sh
+++ targets/support/target_image_setup.sh
@@ -1,25 +1,17 @@
 #!/bin/bash
 
 source ${clst_shdir}/support/functions.sh
-source ${clst_shdir}/support/filesystem-functions.sh
 
-# Make the directory if it doesnt exist
-mkdir -p $1
+mkdir -p "${1}"
 
-loopret=1
+echo "Creating ${clst_fstype} filesystem"
 case ${clst_fstype} in
 	squashfs)
-		create_squashfs $1
-		loopret=$?
+		gensquashfs -D "${clst_destpath}" -q ${clst_fsops} "${1}/image.squashfs" \
+			|| die "Failed to create squashfs filesystem"
 	;;
 	jffs2)
-		create_jffs2 $1
-		loopret=$?
+		mkfs.jffs2 --root="${clst_destpath}" --output="${1}/image.jffs" "${clst_fsops}" \
+			|| die "Failed to create jffs2 filesystem"
 	;;
 esac
-
-if [ ${loopret} = "1" ]
-then
-	die "Filesystem not setup"
-fi
-exit $loopret
