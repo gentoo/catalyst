f189619d3c588dfcb2d70fd31f843c00f212cb56
Matt Turner <mattst88@gentoo.org>
2020-05-15 20:20:01 -0700

    catalyst: Remove remnants of options="preserve_libs"
    This option has been broken since commit e3114e65ffda (Some options
    cleanup, unifying their use, reducing redundancy.) in 2013 which was
    first in catalyst 3.0.0.
    
    I think the feature was a kludge, and that if or when we find the need
    for something similar that we instead just run emerge @preserved-rebuild
    instead.
    
    Signed-off-by: Matt Turner <mattst88@gentoo.org>

--- targets/support/depclean.sh
+++ targets/support/depclean.sh
@@ -2,8 +2,6 @@
 
 source /tmp/chroot-functions.sh
 
-# If the user enabled PRESERVE_LIBS in options, tell portage to preserve them.
-[ -n "${clst_PRESERVE_LIBS}" ] && FEATURES="${clst_myfeatures} preserve-libs"
 if [ "${clst_livecd_depclean}" = "keepbdeps" ]; then
 	run_merge --depclean --with-bdeps=y
 else
--- targets/support/unmerge.sh
+++ targets/support/unmerge.sh
@@ -2,8 +2,6 @@
 
 source /tmp/chroot-functions.sh
 
-# If the user enabled PRESERVE_LIBS in options, tell portage to preserve them.
-[ -n "${clst_PRESERVE_LIBS}" ] && FEATURES="${clst_myfeatures} preserve-libs"
 run_merge -C ${clst_packages}
 
 exit 0
