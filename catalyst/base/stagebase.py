gentoo/catalyst

commit 99e9ceabe0532f5fb1b12ab55d35097b5be3cfa8

DeComp bug fixes

catalyst/base/stagebase.py
@@ -10,7 +10,7 @@ from stat import ST_UID, ST_GID, ST_MODE
 # for convienience
 pjoin = os.path.join
 
-from decomp.compress import CompressMap
+from DeComp.compress import CompressMap
 
 from catalyst.defaults import (SOURCE_MOUNT_DEFAULTS, TARGET_MOUNT_DEFAULTS,
 	PORT_LOGDIR_CLEAN)
@@ -38,10 +38,11 @@ class StageBase(TargetBase, ClearBase, GenBase):
 		self.required_values.extend(["version_stamp","target","subarch",\
 			"rel_type","profile","snapshot","source_subpath"])
 
-		self.valid_values.extend(["version_stamp","target","subarch",\
-			"rel_type","profile","snapshot","source_subpath","portage_confdir",\
-			"cflags","cxxflags","fcflags","fflags","ldflags","cbuild","hostuse","portage_overlay",\
-			"distcc_hosts","makeopts","pkgcache_path","kerncache_path"])
+		self.valid_values.extend(["version_stamp","target","subarch",
+			"rel_type","profile","snapshot","source_subpath","portage_confdir",
+			"cflags","cxxflags","fcflags","fflags","ldflags","cbuild","hostuse","portage_overlay",
+			"distcc_hosts","makeopts","pkgcache_path","kerncache_path",
+			"compression_mode", "decompression_mode"])
 
 		self.set_valid_build_kernel_vars(addlargs)
 		TargetBase.__init__(self, myspec, addlargs)
@@ -1371,6 +1372,9 @@ class StageBase(TargetBase, ClearBase, GenBase):
 			# lists in arch/amd64.py and friends.  If we wanted self.settigs[var]
 			# of any depth, we should make this function recursive.
 			elif type(self.settings[x]) == types.DictType:
+				if x in ["compress_definitions",
+					"decompress_definitions"]:
+					continue
 				self.env[varname] = string.join(self.settings[x].keys())
 				for y in self.settings[x].keys():
 					varname2 = "clst_"+string.replace(y,"/","_")

catalyst/base/genbase.py
@@ -24,7 +24,7 @@ class GenBase(object):
 					array=keys.keys()
 					array.sort()
 				for j in array:
-					contents = contents_map.generate_contents(file, j,
+					contents = contents_map.contents(file, j,
 						verbose="VERBOSE" in self.settings)
 					if contents:
 						myf.write(contents)

catalyst/main.py
@@ -13,11 +13,12 @@ import os.path
 
 __selfpath__ = os.path.abspath(os.path.dirname(__file__))
 
-from decomp.compress import COMPRESS_DEFINITIONS, DECOMPRESS_DEFINITIONS
+from DeComp.definitions import (COMPRESS_DEFINITIONS, DECOMPRESS_DEFINITIONS,
+	CONTENTS_DEFINITIONS)
+from DeComp.contents import ContentsMap
 
 import catalyst.config
 import catalyst.util
-from catalyst.contents import ContentsMap, CONTENTS_DEFINITIONS
 from catalyst.defaults import confdefaults, option_messages
 from catalyst.hash_utils import HashMap, HASH_DEFINITIONS
 from catalyst.lock import LockInUse

catalyst/targets/snapshot.py
@@ -6,18 +6,21 @@ import os
 import shutil
 from stat import ST_UID, ST_GID, ST_MODE
 
+from DeComp.compress import CompressMap
 
 from catalyst.support import normpath, cmd
 from catalyst.base.targetbase import TargetBase
 from catalyst.base.genbase import GenBase
 from catalyst.fileops import ensure_dirs
+
+
 class snapshot(TargetBase, GenBase):
 	"""
 	Builder class for snapshots.
 	"""
 	def __init__(self,myspec,addlargs):
 		self.required_values=["version_stamp","target"]
-		self.valid_values=["version_stamp","target"]
+		self.valid_values=["version_stamp","target", "compression_mode"]
 
 		TargetBase.__init__(self, myspec, addlargs)
 		GenBase.__init__(self,myspec)
@@ -26,7 +29,7 @@ class snapshot(TargetBase, GenBase):
 		st=self.settings["storedir"]
 		self.settings["snapshot_path"] = normpath(st + "/snapshots/"
 			+ self.settings["snapshot_name"]
-			+ self.settings["version_stamp"] + ".tar.bz2")
+			+ self.settings["version_stamp"])
 		self.settings["tmp_path"]=normpath(st+"/tmp/"+self.settings["target_subpath"])
 
 	def setup(self):
@@ -51,18 +54,31 @@ class snapshot(TargetBase, GenBase):
 		mytmp=self.settings["tmp_path"]
 		ensure_dirs(mytmp)
 
+		target_snapshot = self.settings["portdir"] + "/ " + mytmp + "/%s/" % self.settings["repo_name"]
 		cmd("rsync -a --delete --exclude /packages/ --exclude /distfiles/ " +
 			"--exclude /local/ --exclude CVS/ --exclude .svn --filter=H_**/files/digest-* " +
-			self.settings["portdir"] + "/ " + mytmp + "/%s/" % self.settings["repo_name"],
+			target_snapshot,
 			"Snapshot failure", env=self.env)
 
 		print "Compressing Portage snapshot tarball..."
-		cmd("tar -I lbzip2 -cf " + self.settings["snapshot_path"] + " -C " +
-			mytmp + " " + self.settings["repo_name"],
-			"Snapshot creation failure",env=self.env)
-
-		self.gen_contents_file(self.settings["snapshot_path"])
-		self.gen_digest_file(self.settings["snapshot_path"])
+		compressor = CompressMap(self.settings["compress_definitions"],
+			env=self.env, default_mode=self.settings['compression_mode'])
+		infodict = compressor.create_infodict(
+			source=self.settings["repo_name"],
+			destination=self.settings["snapshot_path"],
+			basedir=mytmp,
+			filename=self.settings["snapshot_path"],
+			mode=self.settings["compression_mode"],
+			auto_extension=True
+			)
+		if not compressor.compress(infodict):
+			print("Snapshot compression failure")
+		else:
+			filename = '.'.join([self.settings["snapshot_path"],
+				compressor.extension(self.settings["compression_mode"])])
+			print("COMPRESSOR success!!!! filename", filename)
+			self.gen_contents_file(filename)
+			self.gen_digest_file(filename)
 
 		self.cleanup()
 		print "snapshot: complete!"

commit 3e1784975ab40fda968fe4f36a57e2cfb8edd695

catalyst: Remove unused decompression_mode spec option
Never used.

Fixes: 99e9ceabe053 (DeComp bug fixes)
Signed-off-by: Matt Turner <mattst88@gentoo.org>

catalyst/base/stagebase.py
@@ -49,7 +49,6 @@ class StageBase(TargetBase, ClearBase, GenBase):
             "common_flags",
             "compression_mode",
             "cxxflags",
-            "decompression_mode",
             "distcc_hosts",
             "fcflags",
             "fflags",