gentoo/catalyst

commit 1c7687f60b557d3221661c1a5e25a3dd286cb569

update-modules doesn't exists anymore.

targets/support/kmerge.sh
@@ -245,8 +245,6 @@ SUB=`grep ^SUBLEVEL\ \= /usr/src/linux/Makefile | awk '{ print $3 };'`
 EXV=`grep ^EXTRAVERSION\ \= /usr/src/linux/Makefile | sed -e "s/EXTRAVERSION =//" -e "s/ //g"`
 clst_fudgeuname=${VER}.${PAT}.${SUB}${EXV}
 
-/sbin/update-modules --assume-kernel=${clst_fudgeuname}
-
 unset USE
 echo ${clst_kernel_use} > /tmp/kerncache/${clst_kname}/${clst_kname}-${clst_version_stamp}.USE
 

commit 5e1d4cfcac80ac6582acaa8b8e2b110ce5815734

targets: Delete some more dead code
Dead since 2015.

Fixes: 1c7687f6 ("update-modules doesn't exists anymore.")
Signed-off-by: Matt Turner <mattst88@gentoo.org>

targets/support/kmerge.sh
@@ -247,13 +247,6 @@ make_destpath
 genkernel_compile
 
 sed -i "/USE=\"\${USE} ${kernel_use} \"/d" ${clst_make_conf}
-# grep out the kernel version so that we can do our modules magic
-VER=`grep ^VERSION\ \= /usr/src/linux/Makefile | awk '{ print $3 };'`
-PAT=`grep ^PATCHLEVEL\ \= /usr/src/linux/Makefile | awk '{ print $3 };'`
-SUB=`grep ^SUBLEVEL\ \= /usr/src/linux/Makefile | awk '{ print $3 };'`
-EXV=`grep ^EXTRAVERSION\ \= /usr/src/linux/Makefile | sed -e "s/EXTRAVERSION =//" -e "s/ //g"`
-clst_fudgeuname=${VER}.${PAT}.${SUB}${EXV}
-
 unset USE
 
 if [ -n "${clst_KERNCACHE}" ]