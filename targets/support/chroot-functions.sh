gentoo/catalyst

commit b79d06a3700ea725481d149906a9c66fa681a5ab

Remove temporary package listing code, since --verbose is forced on with --debug
git-svn-id: svn+ssh://svn.gentoo.org/var/svnroot/catalyst/trunk@1322 d1e1f19c-881f-0410-ab34-b69fee027534

targets/support/chroot-functions.sh
@@ -304,21 +304,6 @@ show_debug() {
 		echo "KERNEL_ABI:            $(portageq envvar KERNEL_ABI)"
 		echo "MULTILIB_ABIS:         $(portageq envvar MULTILIB_ABIS)"
 		echo
-		### XXX: This is temporary until we make --debug force-enable --verbose
-		if [ -n "${clst_buildpkgs}" ]
-		then
-			PACKAGES=${clst_buildpkgs}
-		elif [ -n "${clst_packages}" ]
-		then
-			PACKAGES=${clst_packages}
-		fi
-		if [ -n "${PACKAGES}" ]
-		then
-			echo "Packages:"
-			echo "${PACKAGES}"
-			echo
-		fi
-		### XXX: end of section to remove
 	fi
 }
 

commit 9d167d45f011940b15c7bba09dd04fd2ca0235ae

targets: Remove unnecessary unset PACKAGES
Dead code since 2008.

Fixes: b79d06a3 ("Remove temporary package listing code, ...")
Signed-off-by: Matt Turner <mattst88@gentoo.org>

targets/support/chroot-functions.sh
@@ -270,7 +270,6 @@ run_merge() {
 show_debug() {
 	if [ -n "${clst_DEBUG}" ]
 	then
-		unset PACKAGES
 		echo "DEBUG:"
 		echo "Profile/target info:"
 		echo "Profile inheritance:"