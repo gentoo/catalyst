gentoo/catalyst

commit 0343a18ec34a9d9ef03d917afb5d24cd908752b5

Fix merging kernel without kerncache enabled.
This is the proper fix for building kernels without kerncache.
It prevents the creation of kerncache specific files and does not check for them.
Build with kerncache require the later patch to work.

targets/support/kmerge.sh
@@ -109,7 +109,7 @@ genkernel_compile(){
 	else
 		genkernel ${GK_ARGS} || exit 1
 	fi
-	if [ -e /var/tmp/${clst_kname}.config ]
+	if [ -n "${clst_KERNCACHE}" -a -e /var/tmp/${clst_kname}.config ]
 	then
 		md5sum /var/tmp/${clst_kname}.config | awk '{print $1}' > \
 			/tmp/kerncache/${clst_kname}/${clst_kname}-${clst_version_stamp}.CONFIG
@@ -144,63 +144,58 @@ fi
 # USE variables (and thus different patches enabled/disabled.) Also, there's no
 # real benefit in using the pkgcache for kernel source ebuilds.
 
-USE_MATCH=0
-if [ -e /tmp/kerncache/${clst_kname}/${clst_kname}-${clst_version_stamp}.USE ]
+
+# Check if we have a match in kerncach
+
+if [ -n "${clst_KERNCACHE}" ]
 then
-	STR1=$(for i in `cat /tmp/kerncache/${clst_kname}/${clst_kname}-${clst_version_stamp}.USE`; do echo $i; done|sort)
-	STR2=$(for i in ${clst_kernel_use}; do echo $i; done|sort)
-	if [ "${STR1}" = "${STR2}" ]
+
+	USE_MATCH=0
+	if [ -e /tmp/kerncache/${clst_kname}/${clst_kname}-${clst_version_stamp}.USE ]
 	then
-		#echo "USE Flags match"
-		USE_MATCH=1
-	else
-		if [ -n "${clst_KERNCACHE}" ]
+		STR1=$(for i in `cat /tmp/kerncache/${clst_kname}/${clst_kname}-${clst_version_stamp}.USE`; do echo $i; done|sort)
+		STR2=$(for i in ${clst_kernel_use}; do echo $i; done|sort)
+		if [ "${STR1}" = "${STR2}" ]
 		then
-		[ -d /tmp/kerncache/${clst_kname}/ebuilds ] && \
-			rm -r /tmp/kerncache/${clst_kname}/ebuilds
-		[ -e /tmp/kerncache/${clst_kname}/usr/src/linux/.config ] && \
-			rm /tmp/kerncache/${clst_kname}/usr/src/linux/.config
+			#echo "USE Flags match"
+			USE_MATCH=1
+		else
+			[ -d /tmp/kerncache/${clst_kname}/ebuilds ] && \
+				rm -r /tmp/kerncache/${clst_kname}/ebuilds
+			[ -e /tmp/kerncache/${clst_kname}/usr/src/linux/.config ] && \
+				rm /tmp/kerncache/${clst_kname}/usr/src/linux/.config
 		fi
 	fi
-fi
 
-EXTRAVERSION_MATCH=0
-if [ -e /tmp/kerncache/${clst_kname}/${clst_kname}-${clst_version_stamp}.EXTRAVERSION ]
-then
-	STR1=`cat /tmp/kerncache/${clst_kname}/${clst_kname}-${clst_version_stamp}.EXTRAVERSION`
-	STR2=${clst_kextraversion}
-	if [ "${STR1}" = "${STR2}" ]
+	EXTRAVERSION_MATCH=0
+	if [ -e /tmp/kerncache/${clst_kname}/${clst_kname}-${clst_version_stamp}.EXTRAVERSION ]
 	then
-		if [ -n "${clst_KERNCACHE}" ]
+		STR1=`cat /tmp/kerncache/${clst_kname}/${clst_kname}-${clst_version_stamp}.EXTRAVERSION`
+		STR2=${clst_kextraversion}
+		if [ "${STR1}" = "${STR2}" ]
 		then
 			#echo "EXTRAVERSION match"
 			EXTRAVERSION_MATCH=1
 		fi
 	fi
-fi
 
-CONFIG_MATCH=0
-if [ -n "${clst_KERNCACHE}" -a \
-     -e /tmp/kerncache/${clst_kname}/${clst_kname}-${clst_version_stamp}.CONFIG ]
-then
-	if [ ! -e /var/tmp/${clst_kname}.config ]
+	CONFIG_MATCH=0
+	if [ -e /tmp/kerncache/${clst_kname}/${clst_kname}-${clst_version_stamp}.CONFIG ]
 	then
-		CONFIG_MATCH=1
-	else
-		STR1=`cat /tmp/kerncache/${clst_kname}/${clst_kname}-${clst_version_stamp}.CONFIG`
-		STR2=`md5sum /var/tmp/${clst_kname}.config|awk '{print $1}'`
-		if [ "${STR1}" = "${STR2}" ]
+		if [ ! -e /var/tmp/${clst_kname}.config ]
 		then
 			CONFIG_MATCH=1
+		else
+			STR1=`cat /tmp/kerncache/${clst_kname}/${clst_kname}-${clst_version_stamp}.CONFIG`
+			STR2=`md5sum /var/tmp/${clst_kname}.config|awk '{print $1}'`
+			if [ "${STR1}" = "${STR2}" ]
+			then
+				CONFIG_MATCH=1
+			fi
 		fi
 	fi
-fi
 
-[ -e /etc/portage/make.conf ] && \
-	echo "USE=\"\${USE} ${clst_kernel_use} \"" >> /etc/portage/make.conf
-
-if [ -n "${clst_KERNCACHE}" ]
-then
+	# Create the kerncache directory if it doesn't exists
 	mkdir -p /tmp/kerncache/${clst_kname}
 	clst_root_path=/tmp/kerncache/${clst_kname} PKGDIR=${PKGDIR} clst_myemergeopts="--quiet --update --newuse" run_merge "${clst_ksource}" || exit 1
 	KERNELVERSION=`portageq best_visible / "${clst_ksource}"`
@@ -216,25 +211,38 @@ then
 	fi
 	[ -L /usr/src/linux ] && rm -f /usr/src/linux
 	ln -s /tmp/kerncache/${clst_kname}/usr/src/linux /usr/src/linux
+
+	# If catalyst has set to a empty string, extraversion wasn't specified so we
+	# skip this part
+	if [ "${EXTRAVERSION_MATCH}" = "0" ]
+	then
+		if [ ! "${clst_kextraversion}" = "" ]
+		then
+			echo "Setting extraversion to ${clst_kextraversion}"
+			sed -i -e "s:EXTRAVERSION \(=.*\):EXTRAVERSION \1-${clst_kextraversion}:" /usr/src/linux/Makefile
+			echo ${clst_kextraversion} > /tmp/kerncache/${clst_kname}/${clst_kname}-${clst_version_stamp}.EXTRAVERSION
+		else
+			touch /tmp/kerncache/${clst_kname}/${clst_kname}-${clst_version_stamp}.EXTRAVERSION
+		fi
+	fi
+
 else
 	[ -L /usr/src/linux ] && rm -f /usr/src/linux
 	run_merge "${clst_ksource}" || exit 1
-fi
-make_destpath
-
-# If catalyst has set to a empty string, extraversion wasn't specified so we
-# skip this part
-if [ "${EXTRAVERSION_MATCH}" = "0" ]
-then
 	if [ ! "${clst_kextraversion}" = "" ]
 	then
 		echo "Setting extraversion to ${clst_kextraversion}"
 		sed -i -e "s:EXTRAVERSION \(=.*\):EXTRAVERSION \1-${clst_kextraversion}:" /usr/src/linux/Makefile
-		echo ${clst_kextraversion} > /tmp/kerncache/${clst_kname}/${clst_kname}-${clst_version_stamp}.EXTRAVERSION
-	else
-		touch /tmp/kerncache/${clst_kname}/${clst_kname}-${clst_version_stamp}.EXTRAVERSION
 	fi
 fi
+
+
+# Update USE flag in make.conf
+[ -e ${clst_make_conf} ] && \
+	echo "USE=\"\${USE} ${clst_kernel_use} build\"" >> ${clst_make_conf}
+
+make_destpath
+
 
 build_kernel
 sed -i "/USE=\"\${USE} ${clst_kernel_use} \"/d" /etc/portage/make.conf
@@ -246,12 +254,13 @@ EXV=`grep ^EXTRAVERSION\ \= /usr/src/linux/Makefile | sed -e "s/EXTRAVERSION =//
 clst_fudgeuname=${VER}.${PAT}.${SUB}${EXV}
 
 unset USE
-echo ${clst_kernel_use} > /tmp/kerncache/${clst_kname}/${clst_kname}-${clst_version_stamp}.USE
 
 
 if [ -n "${clst_KERNCACHE}" ]
 then
-	if [ -e /etc/portage/profile/package.provided ]
+	echo ${clst_kernel_use} > /tmp/kerncache/${clst_kname}/${clst_kname}-${clst_version_stamp}.USE
+
+	if [ -e ${clst_port_conf}/profile/package.provided ]
 	then
 		sed -i "/^$(echo "${KERNELVERSION}" | sed -e 's|/|\\/|g')\$/d" /etc/portage/profile/package.provided
 	fi

commit 5af4e00582761330325abdef14f507e8aaa1dc99

targets: Remove wrong make.conf munging
With pkgcache fixed for kernel sources in a prior commit, I noticed that
if a livecd-stage2 built three kernels, the first would emerge normally
and build a binpkg, the second would emerge and fail to use the binpkg
produced earlier, and the third would use a previously produced binpkg.
I discovered the reason is that the second and third emerges are run
after USE="... build" has been added to make.conf. We intentionally
removed this in commit c0fcbf02 ("kmerge.sh: Don't set USE=build for
emerging kernels.") years ago, so I was surprised to find this code.

The commit mentioned below inexplicably (and wrongly) readded USE=build
to make.conf, failed to update the sed command to remove it, and readded
the addition in entirely the wrong spot so it didn't even affect the
kernel merge.

genkernel doesn't source the system's make.conf, so this code would be
entirely useless if it wasn't harmful.

Fixes: 0343a18e ("Fix merging kernel without kerncache enabled.")
Signed-off-by: Matt Turner <mattst88@gentoo.org>

targets/support/kmerge.sh
@@ -238,13 +238,7 @@ $(portageq contents / $(portageq best_visible / "${ksource}" 2>/dev/null) 2>/dev
 	fi
 fi
 
-# Update USE flag in make.conf
-[ -e ${clst_make_conf} ] && \
-	echo "USE=\"\${USE} ${kernel_use} build\"" >> ${clst_make_conf}
-
 genkernel_compile
-
-sed -i "/USE=\"\${USE} ${kernel_use} \"/d" ${clst_make_conf}
 
 if [ -n "${clst_KERNCACHE}" ]
 then