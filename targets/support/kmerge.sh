gentoo/catalyst

commit 388def77c5bc066c4230c443b84e2477da1ebff2

kmerge.sh blows away the /usr/src/linux symlink but nothing puts it back in place (nessesarily) add code to properly setup the symlink to the location the freshly installed kernel sources

targets/support/kmerge.sh
@@ -253,8 +253,12 @@ then
 	fi
 
 else
-	[ -L /usr/src/linux ] && rm -f /usr/src/linux
 	run_merge "${clst_ksource}" || exit 1
+	#ensure that there is a /usr/src/linux symlink and it points to the sources we just installed
+	echo "Adjusting /usr/src/linux to point to \
+$(portageq contents / $(portageq best_visible / "${clst_ksource}" 2>/dev/null) 2>/dev/null | grep --color=never '/usr/src/' | head -n1 2>/dev/null)"
+	ln -snf $(portageq contents / $(portageq best_visible / "${clst_ksource}" 2>/dev/null) 2>/dev/null | grep --color=never '/usr/src/' | head -n1 2>/dev/null) \
+		/usr/src/linux
 	if [ ! "${clst_kextraversion}" = "" ]
 	then
 		echo "Setting extraversion to ${clst_kextraversion}"

commit ffe1b6669ba37b166ba968c0cc99267f1b1f0aab

targets: Default to sys-kernel/gentoo-sources directly
Using virtual/linux-sources was a nice idea, but leads to multiple
failures in practice. For example, we use 'portageq contents' later, and
the virtual provides no files. We could handle the indirection with
'portageq expand_virtual' in some cases but that requires the virtual to
be installed on the system.

Fixes: 388def77 ("kmerge.sh blows away the /usr/src/linux symlink ...")
Signed-off-by: Matt Turner <mattst88@gentoo.org>

targets/support/kmerge.sh
@@ -118,11 +118,7 @@ eval "kernel_merge=\$clst_boot_kernel_${kname}_packages"
 eval "kernel_use=\$clst_boot_kernel_${kname}_use"
 eval eval kernel_gk_kernargs=( \$clst_boot_kernel_${kname}_gk_kernargs )
 eval "ksource=\$clst_boot_kernel_${kname}_sources"
-
-if [ -z "${ksource}" ]
-then
-	ksource="virtual/linux-sources"
-fi
+[[ -z ${ksource} ]] && ksource="sys-kernel/gentoo-sources"
 
 # Check if we have a match in kerncach
 