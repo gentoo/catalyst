gentoo/catalyst

commit 8fb885ff174f7299f0edddd987a357d45709b764

Added an additional make_destpath call to kmerge.sh so it'll export ROOT=/ prior to running genkernel.  This is catalyst 2.0.6_pre11 for testing.
git-svn-id: svn+ssh://svn.gentoo.org/var/svnroot/catalyst/trunk@1352 d1e1f19c-881f-0410-ab34-b69fee027534

targets/support/kmerge.sh
@@ -63,7 +63,6 @@ setup_gk_args() {
     then
         GK_ARGS="${GK_ARGS} --initramfs-overlay=\"${clst_merge_path}\""
     fi
-        
 }
 
 genkernel_compile(){
@@ -101,7 +100,8 @@ genkernel_compile(){
 	else
 		genkernel ${GK_ARGS} || exit 1
 	fi
-	md5sum /var/tmp/${clst_kname}.config|awk '{print $1}' > /tmp/kerncache/${clst_kname}/${clst_kname}-${clst_version_stamp}.CONFIG
+	md5sum /var/tmp/${clst_kname}.config | awk '{print $1}' > \
+		/tmp/kerncache/${clst_kname}/${clst_kname}-${clst_version_stamp}.CONFIG
 }
 
 build_kernel() {
@@ -196,6 +196,7 @@ then
 else
 	run_merge "${clst_ksource}" || exit 1
 fi
+make_destpath
 sed -i "/USE=\"\${USE} ${clst_kernel_use} symlink build\"/d" /etc/make.conf
 
 # If catalyst has set to a empty string, extraversion wasn't specified so we

catalyst
@@ -8,7 +8,7 @@ import os,sys,imp,string,getopt
 import pdb
 
 __maintainer__="Chris Gianelloni <wolf31o2@gentoo.org>"
-__version__="2.0.6_pre10"
+__version__="2.0.6_pre11"
 
 conf_values={}
 

commit 852b486b445cf9c49800c6052142bde972706c41

targets: Remove bizarre make_destpath() call
The commit that added it says it did so in order to "export ROOT=/ prior
to running genkernel". Why that was necessary is unclear. I can find no
evidence that genkernel needs (or needed) ROOT to be set, and why it
would need ROOT=/ seems even more doubtful.

Fixes: 8fb885ff ("Added an additional make_destpath call to kmerge.sh ...")
Signed-off-by: Matt Turner <mattst88@gentoo.org>

targets/support/kmerge.sh
@@ -242,8 +242,6 @@ fi
 [ -e ${clst_make_conf} ] && \
 	echo "USE=\"\${USE} ${kernel_use} build\"" >> ${clst_make_conf}
 
-make_destpath
-
 genkernel_compile
 
 sed -i "/USE=\"\${USE} ${kernel_use} \"/d" ${clst_make_conf}