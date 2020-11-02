gentoo/catalyst

commit 7e1ea3ac0107c349333d1ec887917eb3627af7a4

Build ISO with Joliet directory information
Previous syslinux based CDs had this information.
Necessary for direct kernel booting with virt-install --location.

Fixes: 0a27a7a39a "targets: Use GRUB for BIOS boot"
Signed-off-by: Daniel Cordero <catalyst@0xdc.io>
Signed-off-by: Matt Turner <mattst88@gentoo.org>

targets/support/create-iso.sh
@@ -191,12 +191,13 @@ case ${clst_hostarch} in
 	amd64|ia64|ppc*|powerpc*|sparc*|x86)
 		isoroot_checksum
 
+		extra_opts=("-joliet")
 		case ${clst_hostarch} in
-		sparc*) extra_opts="--sparc-boot" ;;
+		sparc*) extra_opts+=("--sparc-boot") ;;
 		esac
 
 		echo ">> Running grub-mkrescue to create iso image...."
-		grub-mkrescue ${extra_opts} -o "${1}" "${clst_target_path}"
+		grub-mkrescue "${extra_opts[*]}" -o "${1}" "${clst_target_path}"
 	;;
 esac
 exit  $?

commit eb5509b929cf162ecb1bea35f7aa4e3d150edf6d

targets: Expand extra_opts into separate words
I introduced this mistaken when reworking the patch referenced below.

Fixes: 7e1ea3ac ("Build ISO with Joliet directory information")
Signed-off-by: Matt Turner <mattst88@gentoo.org>

targets/support/create-iso.sh
@@ -197,7 +197,7 @@ case ${clst_hostarch} in
 		esac
 
 		echo ">> Running grub-mkrescue to create iso image...."
-		grub-mkrescue "${extra_opts[*]}" -o "${1}" "${clst_target_path}"
+		grub-mkrescue "${extra_opts[@]}" -o "${1}" "${clst_target_path}"
 	;;
 esac
 exit  $?