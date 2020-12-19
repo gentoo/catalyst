ee1fb86d76f47a38e0c3991f897b4c1896637557
Matt Turner <mattst88@gentoo.org>
2020-12-18 19:04:23 -0500

    targets: Remove EBEEP_IGNORE / EPAUSE_IGNORE
    Signed-off-by: Matt Turner <mattst88@gentoo.org>

--- targets/support/chroot-functions.sh
+++ targets/support/chroot-functions.sh
@@ -260,8 +260,6 @@ run_merge() {
 
 	export EMERGE_WARNING_DELAY=0
 	export CLEAN_DELAY=0
-	export EBEEP_IGNORE=0
-	export EPAUSE_IGNORE=0
 	[[ $CONFIG_PROTECT != "-*"* ]] && export CONFIG_PROTECT="-*"
 
 	if [ -n "${clst_VERBOSE}" ]
