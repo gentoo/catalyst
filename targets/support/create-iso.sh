gentoo/catalyst

commit 0a27a7a39a7d6944618009f8027fb09a22244c34

targets: Use GRUB for BIOS boot
grub-mkrescue produces an ISO that is bootable from EFI as well as from
the BIOS, so isolinux isn't necessary to accomplish that. The benefits
are that (1) we reduce the number of dependencies required to make the
ISO, (2) the same GRUB set up works for EFI and BIOS systems, and (3)
grub-mkrescue replaces a whole lot of code that we no longer need to
maintain.

This patch has the side effect of removing the F[2-7] help texts and the
memtest86+ option, since those were done via isolinux. I feel okay
removing those since isolinux was only used on BIOS systems and not on
EFI, so none of those things were available to the vast majority of our
users.

It also has the side-effect of removing Fedora's signed UEFI shim
(sys-boot/shim) which could be used to add a hash for the live CD's grub
binary so that you could install under Secure Boot. That doesn't offer
any protection, and the author of the code indicated to me that the
value in offering this feature is that it allows installation on systems
with broken firmware under which important hardware (like the NVMe
drive) isn't exposed unless using Secure Boot. After quite a bit of
searching the internet, I can't find any mentions of this being a
problem.

Signed-off-by: Matt Turner <mattst88@gentoo.org>

targets/support/create-iso.sh
@@ -184,7 +184,7 @@ case ${clst_hostarch} in
 		# o=	output image (burnable to CD; readable by fdisk)
 		/usr/bin/sgibootcd c=${cfg} o=${clst_iso}
 	;;
-	ia64|ppc*|powerpc*|sparc*)
+	amd64|ia64|ppc*|powerpc*|sparc*|x86)
 		isoroot_checksum
 
 		case ${clst_hostarch} in
@@ -193,72 +193,6 @@ case ${clst_hostarch} in
 
 		echo ">> Running grub-mkrescue to create iso image...."
 		grub-mkrescue ${extra_opts} -o "${1}" "${clst_target_path}"
-	;;
-	x86|amd64)
-		# detect if an EFI bootloader is desired
-		if 	[ -d "${clst_target_path}/boot/efi" ] || \
-			[ -d "${clst_target_path}/boot/EFI" ] || \
-			[ -e "${clst_target_path}/gentoo.efimg" ]
-		then
-			if [ -e "${clst_target_path}/gentoo.efimg" ]
-			then
-				echo "Found prepared EFI boot image at \
-					${clst_target_path}/gentoo.efimg"
-			else
-				echo "Preparing EFI boot image"
-				if [ -d "${clst_target_path}/boot/efi" ] && [ ! -d "${clst_target_path}/boot/EFI" ]; then
-					echo "Moving /boot/efi to /boot/EFI"
-					mv "${clst_target_path}/boot/efi" "${clst_target_path}/boot/EFI"
-				fi
-				# prepare gentoo.efimg from clst_target_path /boot/EFI dir
-				iaSizeTemp=$(du -sk --apparent-size "${clst_target_path}/boot/EFI" 2>/dev/null)
-				iaSizeB=$(echo ${iaSizeTemp} | cut '-d ' -f1)
-				iaSize=$((${iaSizeB}+64)) # add slack, tested near minimum for overhead
-				echo "Creating loopback file of size ${iaSize}kB"
-				dd if=/dev/zero of="${clst_target_path}/gentoo.efimg" bs=1k \
-					count=${iaSize}
-				echo "Formatting loopback file with FAT16 FS"
-				mkfs.vfat -F 16 -n GENTOOLIVE "${clst_target_path}/gentoo.efimg"
-
-				mkdir "${clst_target_path}/gentoo.efimg.mountPoint"
-				echo "Mounting FAT16 loopback file"
-				mount -t vfat -o loop "${clst_target_path}/gentoo.efimg" \
-					"${clst_target_path}/gentoo.efimg.mountPoint" || die "Failed to mount EFI image file"
-
-				echo "Populating EFI image file from ${clst_target_path}/boot/EFI"
-				cp -rv "${clst_target_path}"/boot/EFI/ \
-					"${clst_target_path}/gentoo.efimg.mountPoint" || die "Failed to populate EFI image file"
-
-				umount "${clst_target_path}/gentoo.efimg.mountPoint"
-				rmdir "${clst_target_path}/gentoo.efimg.mountPoint"
-
-				echo "Copying /boot/EFI to /EFI for rufus compatability"
-				cp -rv "${clst_target_path}"/boot/EFI/ "${clst_target_path}"
-			fi
-		fi
-
-		if [ -e "${clst_target_path}/isolinux/isolinux.bin" ]; then
-			echo '** Found ISOLINUX bootloader'
-			if [ -e "${clst_target_path}/gentoo.efimg" ]; then
-			  # have BIOS isolinux, plus an EFI loader image
-			  echo '** Found GRUB2 EFI bootloader'
-				echo 'Creating ISO using both ISOLINUX and EFI bootloader'
-				run_mkisofs -J -R -l -V "${clst_iso_volume_id}" -o "${1}" -b isolinux/isolinux.bin -c isolinux/boot.cat -no-emul-boot -boot-load-size 4 -boot-info-table -eltorito-alt-boot -eltorito-platform efi -b gentoo.efimg -no-emul-boot -z "${clst_target_path}"/
-				isohybrid --uefi "${1}"
-		  else
-			  echo 'Creating ISO using ISOLINUX bootloader'
-			  run_mkisofs -J -R -l -V "${clst_iso_volume_id}" -o "${1}" -b isolinux/isolinux.bin -c isolinux/boot.cat -no-emul-boot -boot-load-size 4 -boot-info-table "${clst_target_path}"/
-			  isohybrid "${1}"
-		  fi
-		elif [ -e "${clst_target_path}/gentoo.efimg" ]; then
-			echo '** Found GRUB2 EFI bootloader'
-			echo 'Creating ISO using EFI bootloader'
-			run_mkisofs -J -R -l -V "${clst_iso_volume_id}" -o "${1}" -b gentoo.efimg -c boot.cat -no-emul-boot "${clst_target_path}"/
-		else
-			echo '** Found no known bootloader'
-			echo 'Creating ISO with fingers crossed that you know what you are doing...'
-			run_mkisofs -J -R -l -V "${clst_iso_volume_id}" -o "${1}" "${clst_target_path}"/
-		fi
 	;;
 esac
 exit  $?

targets/support/functions.sh
@@ -43,75 +43,6 @@ extract_cdtar() {
 	tar -I lbzip2 -xpf ${clst_cdtar} -C $1 || die "Couldn't extract cdtar ${cdtar}"
 }
 
-create_bootloader() {
-	# For amd64 and x86 we attempt to copy boot loader files from the live system and configure it right
-	# this prevents (among other issues) needing to keep a cdtar up to date.  All files are thrown into $clst_target_path
-	# Future improvement may make bootloaders optional, but for now there is only one option
-  if [ -x "/usr/bin/grub2-mkstandalone" ]; then
-    grubmkstndaln="/usr/bin/grub2-mkstandalone"
-  elif [ -x "/usr/bin/grub-mkstandalone" ]; then
-    grubmkstndaln="/usr/bin/grub-mkstandalone"
-  else
-    die "Unable to find grub-mkstandalone"
-  fi
-
-  pushd "${1}" || die "Failed to enter livecd dir ${1}"
-
-  # while $1/grub is unused here, it triggers grub config building in bootloader-setup.sh
-  mkdir -p boot/EFI/BOOT isolinux
-  #create boot.msg for isolinux
-	echo "Gentoo Linux Installation LiveCD                         http://www.gentoo.org/" > isolinux/boot.msg
-	echo "Enter to boot; F1 for kernels  F2 for options." >> isolinux/boot.msg
-	echo "Press any key in the next 15 seconds or we'll try to boot from disk." >> isolinux/boot.msg
-  #install isolinux files
-  if [ -f /usr/share/syslinux/isolinux.bin ]; then
-    cp /usr/share/syslinux/isolinux.bin isolinux/
-    #isolinux support files
-    for i in libcom32.c32 libutil.c32 ldlinux.c32 reboot.c32 vesamenu.c32; do
-      if [ -f "/usr/share/syslinux/${i}" ]; then
-        cp "/usr/share/syslinux/${i}" isolinux/
-      fi
-    done
-    #isolinux hardware detection toolkit, useful for system info and debugging
-    if [ -f "/usr/share/syslinux/hdt.c32" ]; then
-      cp /usr/share/syslinux/hdt.c32 isolinux/
-      if [ -f "/usr/share/misc/pci.ids" ]; then
-        cp /usr/share/misc/pci.ids isolinux/
-      fi
-    fi
-    #memtest goes under isolinux since it doesn't work for uefi right now
-    if [ -f /usr/share/memtest86+/memtest ]; then
-      cp /usr/share/memtest86+/memtest.bin isolinux/memtest86
-    else
-      echo "Missing /usr/share/memtest86+/memtest.bin, this livecd will not have memtest86+ support.  Enable USE=system-bootloader on catalyst to pull in the correct deps"
-    fi
-  else
-    echo "Missing /usr/share/syslinux/isolinux.bin, this livecd will not bios boot.  Enable USE=system-bootloader on catalyst to pull in the correct deps"
-  fi
-
-  #create grub-stub.cfg for embedding in grub-mkstandalone
-  echo "insmod part_gpt" > grub-stub.cfg
-  echo "insmod part_msdos" >> grub-stub.cfg
-  echo "search --no-floppy --set=root --file /livecd" >> grub-stub.cfg
-  echo "configfile /boot/grub/grub.cfg" >> grub-stub.cfg
-
-  # some 64 bit machines have 32 bit UEFI, and you might want to boot 32 bit on a 64 bit machine, so we take the safest path and include both
-  # set up 32 bit uefi
-  ${grubmkstndaln} /boot/grub/grub.cfg=./grub-stub.cfg --compress=xz -O i386-efi -o ./boot/EFI/BOOT/grubia32.efi --themes= || die "Failed to make grubia32.efi"
-  #secure boot shim
-  cp /usr/share/shim/BOOTIA32.EFI boot/EFI/BOOT/
-  cp /usr/share/shim/mmia32.efi boot/EFI/BOOT/
-
-  #set up 64 bit uefi
-  ${grubmkstndaln} /boot/grub/grub.cfg=./grub-stub.cfg --compress=xz -O x86_64-efi -o ./boot/EFI/BOOT/grubx64.efi --themes= || die "Failed to make grubx64.efi"
-  #secure boot shim
-  cp /usr/share/shim/BOOTX64.EFI boot/EFI/BOOT/
-  cp /usr/share/shim/mmx64.efi boot/EFI/BOOT/
-
-  rm grub-stub.cfg || echo "Failed to remove grub-stub.cfg, but this hurts nothing"
-  popd || die "Failed to leave livecd dir"
-}
-
 extract_kernels() {
 	# extract multiple kernels
 	# $1 = Destination

examples/livecd-stage2_template.spec
@@ -94,10 +94,9 @@ livecd/fstype:
 livecd/fsops:
 
 # The cdtar is essentially the bootloader for the CD.  It also holds the main
-# configuration for the bootloader.  On x86/amd64, it also can include a small
-# memory testing application, called memtest86+.
+# configuration for the bootloader.
 # example:
-# livecd/cdtar: /usr/share/catalyst/livecd/cdtar/isolinux-2.13-memtest86+-cdtar.tar.bz2
+# livecd/cdtar: /usr/share/catalyst/livecd/cdtar/[...].cdtar.tar.bz2
 livecd/cdtar:
 
 # This is the full path and filename to the ISO image that the livecd-stage2

livecd/files/x86-F2.msg
@@ -1,22 +1,0 @@
-Gentoo Linux LiveCD boot options - [F1 to display available kernels]
-
-Please hit F1 to see the available kernels on this livecd.  Please note that
-the -nofb counterparts to each kernel disable the framebuffer
-and splash images. Additionally, the memtest86 boot option is available
-to test local RAM for errors. To use memtest86, just type 'memtest86'.
-
-This lists the possible command line options that can be used to tweak the boot
-process of this CD.  This lists the Gentoo-specific options, along with a few
-options that are built-in to the kernel, but that have been proven very useful
-to our users.  Also, all options that start with "do" have a "no" inverse, that
-does the opposite.  For example, "doscsi" enables SCSI support in the initial
-ramdisk boot, while "noscsi" disables it.
-
-To list the options, please press keys from F3 through F7.
-
-F3: Hardware (Page 1)
-F4: Hardware (Page 2)
-F5: Hardware (Page 3)
-F6: Volume Management
-F7: Misc.
-

livecd/files/x86-F3.msg
@@ -1,22 +1,0 @@
-Hardware options (Page 1):
-acpi=on         This loads support for ACPI and also causes the acpid daemon to
-                be started by the CD on boot.  This is only needed if your
-                system requires ACPI to function properly.  This is not
-                required for Hyperthreading support.
-acpi=off        Completely disables ACPI.  This is useful on some older systems
-                and is also a requirement for using APM.  This will disable any
-                Hyperthreading support of your processor.
-console=X       This sets up serial console access for the CD.  The first
-                option is the device, usually ttyS0 on x86, followed by any
-                connection options, which are comma separated.  The default
-                options are 9600,8,n,1.
-dmraid=X        This allows for passing options to the device-mapper RAID
-                subsystem.  Options should be encapsulated in quotes.
-doapm           This loads APM driver support.  This requires you to also use
-                acpi=off.
-dopcmcia        This loads support for PCMCIA and Cardbus hardware and also
-                causes the pcmcia cardmgr to be started by the CD on boot.
-                This is only required when booting from PCMCIA/Cardbus devices.
-doscsi          This loads support for most SCSI controllers.  This is also a
-                requirement for booting most USB devices, as they use the SCSI
-                subsystem of the kernel.

livecd/files/x86-F4.msg
@@ -1,20 +1,0 @@
-Hardware options (Page 2):
-hda=stroke      This allows you to partition the whole hard disk even when your
-                BIOS is unable to handle large disks.  This option is only used
-                on machines with an older BIOS.  Replace hda with the device
-                that is requiring this option.
-ide=nodma       This forces the disabling of DMA in the kernel and is required
-                by some IDE chipsets and also by some CDROM drives.  If your
-                system is having trouble reading from your IDE CDROM, try this
-                option.  This also disables the default hdparm settings from
-                being executed.
-noapic          This disables the Advanced Programmable Interrupt Controller
-                that is present on newer motherboards.  It has been known to
-                cause some problems on older hardware.
-nodetect        This disables all of the autodetection done by the CD,
-                including device autodetection and DHCP probing.  This is
-                useful for doing debugging of a failing CD or driver.
-nodhcp          This disables DHCP probing on detected network cards.  This is
-                useful on networks with only static addresses.
-nodmraid        Disables support for device-mapper RAID, such as that used for
-                on-board IDE/SATA RAID controllers.

livecd/files/x86-F5.msg
@@ -1,22 +1,0 @@
-Hardware options (Page 3):
-nofirewire      This disables the loading of Firewire modules.  This should
-                only be necessary if your Firewire hardware is causing
-                a problem with booting the CD.
-nogpm           This diables gpm console mouse support.
-nohotplug       This disables the loading of the hotplug and coldplug init
-                scripts at boot.  This is useful for doing debugging of a
-                failing CD or driver.
-nokeymap        This disables the keymap selection used to select non-US
-                keyboard layouts.
-nolapic         This disables the local APIC on Uniprocessor kernels.
-nosata          This disables the loading of Serial ATA modules.  This is used
-                if your system is having problems with the SATA subsystem.
-nosmp           This disables SMP, or Symmetric Multiprocessing, on SMP-enabled
-                kernels.  This is useful for debugging SMP-related issues with
-                certain drivers and motherboards.
-nosound         This disables sound support and volume setting.  This is useful
-                for systems where sound support causes problems.
-nousb           This disables the autoloading of USB modules.  This is useful
-                for debugging USB issues.
-slowusb         This adds some extra pauses into the boot process for slow
-                USB CDROMs, like in the IBM BladeCenter.

livecd/files/x86-F6.msg
@@ -1,14 +1,0 @@
-Volume/Device Management:
-doevms          This enables support for IBM's pluggable EVMS, or Enterprise
-                Volume Management System.  This is not safe to use with lvm2.
-dolvm           This enables support for Linux's Logical Volume Management.
-                This is not safe to use with evms2.
-Screen reader access:
-speakup.synth=synth  starts speakup using a given synthesizer.
-                     supported synths are acntpc, acntsa, apollo, audptr, bns,
-                     decext, dectlk, dtlk, keypc, ltlk, spkout and txprt.
-                     Also, soft is supported for software speech and dummy is
-                     supported for testing.
-speakup.quiet=1      sets the synthesizer not to speak until a key is pressed.
-speakup_SYNTH.port=n sets the port for internal synthesizers.
-speakup_SYNTH.ser=n  sets the serial port for external synthesizers.

commit 90e1cbf32bf1de2854e8ed21dbd644b10b4e22ad

targets: Remove some ISOLINUX leftovers
We don't use ISOLINUX since the commit listed below.

Fixes: 0a27a7a3 ("targets: Use GRUB for BIOS boot")
Signed-off-by: Matt Turner <mattst88@gentoo.org>

targets/support/create-iso.sh
@@ -102,7 +102,7 @@ isoroot_checksum() {
 	echo ">> Creating checksums for all files included in the ISO"
 
 	pushd "${clst_target_path}"
-	find -type f ! -name 'isolinux.bin' -exec b2sum {} + > /tmp/isoroot_b2sums
+	find -type f -exec b2sum {} + > /tmp/isoroot_b2sums
 	popd
 
 	mv /tmp/isoroot_b2sums "${clst_target_path}"/

targets/support/functions.sh
@@ -49,7 +49,7 @@ extract_kernels() {
 	# ${clst_target_path}/kernel is often a good choice for ${1}
 
 	# Takes the relative desination dir for the kernel as an arguement
-	# i.e boot or isolinux
+	# i.e boot
 	[ -z "$clst_boot_kernel" ] && \
 		die "Required key boot/kernel not defined, exiting"
 	# install the kernels built in kmerge.sh