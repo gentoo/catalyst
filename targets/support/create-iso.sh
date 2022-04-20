gentoo/catalyst

commit 0b56dbe4d36c88b0b051d24451e15cd6b64c819d

livecd: Use ISO level 3
Signed-off-by: Andreas K. Hüttel <dilfridge@gentoo.org>

targets/support/create-iso.sh
@@ -194,7 +194,7 @@ case ${clst_hostarch} in
 	amd64|arm64|ia64|ppc*|powerpc*|sparc*|x86)
 		isoroot_checksum
 
-		extra_opts=("-joliet")
+		extra_opts=("-joliet" "-iso-level 3")
 		case ${clst_hostarch} in
 		sparc*) extra_opts+=("--sparc-boot") ;;
 		esac

commit 05b1f53a03e6e4ca9373cbd280c4e22ed234ac4f

create-iso.sh: split iso-level arguments
When enclosed in quotes and expanded with "${variable[@]}", the word is expanded
to be a single argument, rather than two separate arguments.

i.e. argv[2] = "-iso-level 3"
instead of
  argv[2] = "-iso-level"
  argv[3] = "3"

This lead to the failure:
  xorriso : FAILURE : -as mkisofs: Unrecognized option '-iso-level 3'
  xorriso : aborting : -abort_on 'FAILURE' encountered 'FAILURE'
  grub-mkrescue: error: `xorriso` invocation failed

Fixes: 0b56dbe4 ("livecd: Use ISO level 3")
Signed-off-by: Matt Turner <mattst88@gentoo.org>

targets/support/create-iso.sh
@@ -194,7 +194,7 @@ case ${clst_hostarch} in
 	amd64|arm64|ia64|ppc*|powerpc*|sparc*|x86)
 		isoroot_checksum
 
-		extra_opts=("-joliet" "-iso-level 3")
+		extra_opts=("-joliet" "-iso-level" "3")
 		case ${clst_hostarch} in
 		sparc*) extra_opts+=("--sparc-boot") ;;
 		esac