gentoo/catalyst

commit cd58b37bcd07433e93b80b4a93c9e79ec9395e26

catalyst: Access setarch data correctly
This prevented the setarch data in the TOML files from being applied
correctly.

Fixes: b368f6aa5e29 (catalyst: Use arch data from TOML, not python
                     modules)
Signed-off-by: Matt Turner <mattst88@gentoo.org>

catalyst/base/stagebase.py
@@ -95,7 +95,7 @@ class StageBase(TargetBase, ClearBase, GenBase):
                 # Search for a subarchitecture in each arch in the arch_config
                 for arch in [x for x in arch_config if x.startswith(name) and host in arch_config[x]]:
                     self.settings.update(arch_config[arch][host])
-                    setarch = arch_config.get('setarch', {})
+                    setarch = arch_config.get('setarch', {}).get(host, {})
                     break
                 else:
                     # Didn't find a matching subarchitecture, keep searching

commit aa52470e86ebd0578c379813a9144611c97431a8

catalyst: Use correct arch setting for setarch testing
Previously we were testing against the 'host' or subarchitecture (e.g.
i486) instead of the architecture (e.g. x86). This manifests on x86
builds on x86_64; on ppc on ppc64 and sparc on sparc64 the arch and
subarch values happen to line up.

Fixes: cd58b37b ("catalyst: Access setarch data correctly")
Signed-off-by: Matt Turner <mattst88@gentoo.org>

catalyst/base/stagebase.py
@@ -129,7 +129,7 @@ class StageBase(TargetBase, ClearBase, GenBase):
                 # Search for a subarchitecture in each arch in the arch_config
                 for arch in [x for x in arch_config if x.startswith(name) and host in arch_config[x]]:
                     self.settings.update(arch_config[arch][host])
-                    setarch = arch_config.get('setarch', {}).get(host, {})
+                    setarch = arch_config.get('setarch', {}).get(arch, {})
                     break
                 else:
                     # Didn't find a matching subarchitecture, keep searching