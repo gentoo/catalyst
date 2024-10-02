8717fcfb083fa9bbf8367af28cb3459243688611
Andreas K. Hüttel <dilfridge@gentoo.org>
2024-10-02 23:47:47 +0200

    more
    Signed-off-by: Andreas K. Hüttel <dilfridge@gentoo.org>

--- targets/support/create-qcow2.sh
+++ targets/support/create-qcow2.sh
@@ -15,6 +15,10 @@ source ${clst_shdir}/support/functions.sh
 #  - clst_qcow2_efisize   size of the EFI boot partition (GByte, default 0.5)
 #  - clst_qcow2_roottype  type of the root partition (default xfs)
 #
+export clst_qcow2_size=20
+export clst_qcow2_swapsize=0
+export clst_qcow2_efisize=0.5
+export clst_qcow2_roottype=xfs
 
 #
 # We only support one set of tools, based on
@@ -27,65 +31,59 @@ source ${clst_shdir}/support/functions.sh
 # Let's assume these are deps of catalyst and thus present.
 #
 
-
 # create a new qcow2 disk image file
 qemu-img create -f qcow2 ${1} ${clst_qcow2_size}G || die "Cannot create qcow2 file"
 
 # connect the qcow2 file to a network block device
 # TODO: find next free device
 modprobe -q nbd
-qemu-nbd -c /dev/nbd0 -f qcow2 ${1} || die "Cannot connect qcow2 file to nbd"
+qemu-nbd -c /dev/nbd0 -f qcow2 ${1} || die "Cannot connect qcow2 file to nbd0"
 
 # create a GPT disklabel
-parted /dev/nbd0 mklabel gpt
+parted -s /dev/nbd0 mklabel gpt || die "Cannot create disklabel"
 
 # create an EFI boot partition
+parted -s /dev/nbd0 -- mkpart efi fat32 1M {clst_qcow2_efisize}GiB || die "Cannot create EFI partition"
 
 # if requested, create a swap partition
 
 # fill the rest of the image with the root partition
+parted -s /dev/nbd0 -- mkpart root ${clst_qcow2_roottype} ${clst_qcow2_efisize}GiB -1s || die "Cannot create root partition"
 
 # re-read the partition table
-partprobe /dev/nbd0
+partprobe /dev/nbd0 || die "Probing partition table failed"
 
 # make a vfat filesystem in p1
-# /dev/nbd0p1
+mkfs.fat -F 32 /dev/nbd0p1 || die "Formatting EFI partition failed"
 
 # make a swap signature in p2
 
-# make a xxx filesystem in p3 (or p2 if no swap)
+# make a xfs filesystem in p3 (or p2 if no swap)
+mkfs.xfs /dev/nbd0p2 || die "Formatting root partition failed"
 
 # mount things
+# we need a mount point- how do we get one?
+my_mountpoint=/tmp/bla
+mkdir -p "${my_mountpoint}" || die "Could not create mount point"
+mount /dev/nbd0p2 "${my_mountpoint}" || die "Could not mount root partition"
+mount /dev/nbd0p1 "${my_mountpoint}/boot" || die "Could not mount boot partition"
 
 # copy contents in
+cp -a "${clst_target_path}"/* "${my_mountpoint}/"
 
 # at this point we have a rudimentary working system
-# now we need to follow the handbook and do basic configuration
-# (locale, timezone, ssh)
+# now we need to follow the handbook and do some basic configuration
+# (locale, timezone, ssh) - though most of it will be done by cloud-init
 
 
+# now let's install an efi loader
+bootctl --no-variables --graceful install
 
+# unmount things
+umount "${my_mountpoint}/boot" || die "Could not unmount boot partition"
+umount "${my_mountpoint}" || die "Could not unmount root partition"
 
+# disconnect the nbd
+qemu-nbd -d /dev/nbd0 || die "Could not disconnect nbd0"
 
-
-
-
-# old stuff
-
-
-
-# Here we actually create the ISO images for each architecture
-case ${clst_hostarch} in
-	amd64|arm64|ia64|ppc*|powerpc*|sparc*|x86|i?86)
-		isoroot_checksum
-
-		extra_opts=("-joliet" "-iso-level" "3")
-		case ${clst_hostarch} in
-		sparc*) extra_opts+=("--sparc-boot") ;;
-		esac
-
-		echo ">> Running grub-mkrescue to create iso image...."
-		grub-mkrescue --mbr-force-bootable -volid "${clst_iso_volume_id}" "${extra_opts[@]}" -o "${1}" "${clst_target_path}"
-	;;
-esac
-exit  $?
+# Finished...
