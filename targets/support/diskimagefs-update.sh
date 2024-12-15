999202313d76a1dd61687f7847758311baf51b1c
Andreas K. Hüttel <dilfridge@gentoo.org>
2024-12-15 22:28:18 +0100

    cloud-init needs no service enabling, it has a generator
    Signed-off-by: Andreas K. Hüttel <dilfridge@gentoo.org>

--- targets/support/diskimagefs-update.sh
+++ targets/support/diskimagefs-update.sh
@@ -80,8 +80,6 @@ case ${clst_diskimage_type} in
 		echo "Setting up cloud-init image"
 		echo "Running systemd-firstboot"
 		systemd-firstboot --timezone=UTC || die "Failed running systemd-firstboot"
-		echo "Adding cloud-init service"
-		systemctl enable cloud-init || die "Failed to enable cloud-init service"
 		;;
 	*)
 		die "As yet unsupported image type"
