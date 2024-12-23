1151a6ae1a11a3fc1288598b50a70d231a5cda4c
Andreas K. Hüttel <dilfridge@gentoo.org>
2024-12-23 22:25:14 +0100

    Allow qcow2 grub-install on arm64
    Signed-off-by: Andreas K. Hüttel <dilfridge@gentoo.org>

--- targets/support/qcow2-grub-install.sh
+++ targets/support/qcow2-grub-install.sh
@@ -2,8 +2,16 @@
 
 source /tmp/chroot-functions.sh
 
-echo "Installing grub with target x86_64-efi"
-grub-install --no-floppy --efi-directory=/boot --removable --skip-fs-probe --no-nvram --no-bootsector --target=x86_64-efi
+case ${clst_hostarch} in
+	amd64)
+		echo "Installing grub with target x86_64-efi"
+		grub-install --no-floppy --efi-directory=/boot --removable --skip-fs-probe --no-nvram --no-bootsector --target=x86_64-efi
+		;;
+	arm64)
+		echo "Installing grub with target arm64-efi"
+		grub-install --no-floppy --efi-directory=/boot --removable --skip-fs-probe --no-nvram --no-bootsector --target=arm64-efi
+		;;
+esac
 
 echo "Creating grub configuration"
 grub-mkconfig -o /boot/grub/grub.cfg
