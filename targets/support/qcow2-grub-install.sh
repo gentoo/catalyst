41f1cb7c0d435953df7dd5bbd029201f3112ad24
Andreas K. Hüttel <dilfridge@gentoo.org>
2024-12-27 15:22:21 +0100

    Set up disk images for serial console
    Signed-off-by: Andreas K. Hüttel <dilfridge@gentoo.org>

--- targets/support/qcow2-grub-install.sh
+++ targets/support/qcow2-grub-install.sh
@@ -2,6 +2,18 @@
 
 source /tmp/chroot-functions.sh
 
+echo "Setting up grub for also serial console"
+cat >> /etc/default/grub <<THISISIT
+
+# Added by catalyst
+GRUB_TERMINAL='serial console'
+GRUB_SERIAL_COMMAND='serial --speed 115200 --unit=0 --word=8 --parity=no --stop=1'
+GRUB_CMDLINE_LINUX="console=ttyS0 console=tty0"
+THISISIT
+
+echo "Creating grub configuration"
+grub-mkconfig -o /boot/grub/grub.cfg
+
 case ${clst_hostarch} in
 	amd64)
 		echo "Installing grub with target x86_64-efi"
@@ -12,6 +24,3 @@ case ${clst_hostarch} in
 		grub-install --no-floppy --efi-directory=/boot --removable --skip-fs-probe --no-nvram --no-bootsector --target=arm64-efi
 		;;
 esac
-
-echo "Creating grub configuration"
-grub-mkconfig -o /boot/grub/grub.cfg
