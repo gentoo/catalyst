fca4dbb1f83dd8d46a568d639272fdbf75f9bfc7
Nowa Ammerlaan <nowa@gentoo.org>
2025-05-09 13:17:13 +0200

    controller.sh: pass on arguments when calling livecdfs-update.sh
    Signed-off-by: Andreas K. Hüttel <dilfridge@gentoo.org>
    Signed-off-by: Nowa Ammerlaan <nowa@gentoo.org>

--- targets/embedded/controller.sh
+++ targets/embedded/controller.sh
@@ -46,8 +46,9 @@ case ${1} in
 
 	;;
 	livecd-update)
+		shift
 		# Now, finalize and tweak the livecd fs (inside of the chroot)
-		exec_in_chroot  ${clst_shdir}/support/livecdfs-update.sh
+		exec_in_chroot  ${clst_shdir}/support/livecdfs-update.sh ${@}
 	;;
 
 	bootloader)
