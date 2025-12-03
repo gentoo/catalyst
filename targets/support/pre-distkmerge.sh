80087c60c3fe8c2ab5228bfd00aaa099f8e641b4
Ian Jordan <immoloism@gmail.com>
2025-12-03 15:20:46 +0000

    Disable installkernel chroot check
    >sys-kernel/installkernel now has a chroot detection script.
    This will cause any kernel above 6.16 to error out during
    the kernel phasee.
    
    This change will disable that test and allow us to use
    installkernel as we always have.
    
    Testewd on riscv64 diskimage abd amd64 livegui
    
    Signed-off-by: Ian Jordan <immoloism@gmail.com>
    Signed-off-by: Andreas K. Hüttel <dilfridge@gentoo.org>

--- targets/support/pre-distkmerge.sh
+++ targets/support/pre-distkmerge.sh
@@ -4,4 +4,7 @@ RUN_DEFAULT_FUNCS="yes"
 
 source /tmp/chroot-functions.sh
 
+# Disable chroot check in installkernel
+touch /etc/kernel/install.d/05-check-chroot.install
+
 run_merge --oneshot sys-kernel/dracut
