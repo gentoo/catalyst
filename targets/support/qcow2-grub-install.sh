5b0d60a398427ac96f1ad4888c968a879c90b92f
Andreas K. Hüttel <dilfridge@gentoo.org>
2025-12-28 16:16:59 +0100

    Select correct hostarch for riscv64-efi grub
    Signed-off-by: Andreas K. Hüttel <dilfridge@gentoo.org>

--- targets/support/qcow2-grub-install.sh
+++ targets/support/qcow2-grub-install.sh
@@ -14,6 +14,7 @@ THISISIT
 echo "Creating grub configuration"
 grub-mkconfig -o /boot/grub/grub.cfg
 
+echo "Catalyst hostarch is ${clst_hostarch}"
 case ${clst_hostarch} in
 	amd64)
 		echo "Installing grub with target x86_64-efi"
@@ -23,8 +24,11 @@ case ${clst_hostarch} in
 		echo "Installing grub with target arm64-efi"
 		grub-install --no-floppy --efi-directory=/boot --removable --skip-fs-probe --no-nvram --no-bootsector --target=arm64-efi
 		;;
-	riscv64)
+	rv64_lp64d)
 		echo "Installing grub with target riscv64-efi"
 		grub-install --no-floppy --efi-directory=/boot --removable --skip-fs-probe --no-nvram --no-bootsector --target=riscv64-efi
+		;;
+	*)
+		echo "ERROR: unknown hostarch ${clst_hostarch}, grub not installed"
 		;;
 esac
